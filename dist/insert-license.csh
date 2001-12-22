745382f6a05a460659d48e3dfbba3561fb69e046
jsquyres <jsquyres@18f9d06c-6ae2-0310-8438-938a554feff9>
2001-12-22 01:03:43 +0000

    Take II of the packaging system; now insert copyright notices in all files that have $COPYRIGHT$ in them.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@137 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 6d0562c34066c0ed6d48711d0b492b54a8a47a7e [formerly 260b32e2359933e5a2e5a8315f34b4096d6b4504]
    Former-commit-id: 650c46396115a0384401f963939da97484d5f3e2

---
+++ dist/insert-license.csh
@@ -0,0 +1,140 @@
+#! /bin/csh -f
+#
+# $COPYRIGHT$
+#
+
+#
+# This is goofy.  We have to be sure to skip running sed on this
+# script itself because that will remove the +x Unix perms on this
+# file, which we can't very well change in the middle of the find
+# statement.
+#
+# Also must mv -f because the perms may very well be 444
+#
+
+# First, make all the license headers from the main copy:
+# - Insert the current year
+# - Put the comment marks in each file
+
+set filelist=$1
+set tempdir=/tmp/license-oscar.$$
+mkdir -p $tempdir
+set clicense=$tempdir/license.hdr.c
+set cpplicense=$tempdir/license.hdr.cpp
+set shelllicense=$tempdir/license.hdr.shell
+set textlicense=$tempdir/license.hdr.text
+set texlicense=$tempdir/license.hdr.tex
+set year=`date +%Y`
+
+# Text comment file
+set dir="`dirname $0`"
+sed -e s/CURRENTYEAR/$year/ $dir/license.hdr > $textlicense
+
+# LaTeX comment file
+sed -e 's/^/% /' $textlicense > $texlicense
+
+# C comment file
+sed -e 's/^/ * /' $textlicense > $clicense
+
+# C++ comment file
+sed -e 's/^/\/\/ /' $textlicense > $cpplicense
+
+# Shell comment file
+sed -e 's/^/# /' $textlicense > $shelllicense
+
+
+# Now make a sed script to substitute them in
+
+set sedscript=$tempdir/license.sed
+cat > $sedscript <<EOF
+# C++ with // style comments
+/\/\/.*\\\$COPYRIGHT\\\$/ {
+	r $cpplicense
+	d
+}
+
+# C with /* */ style comments
+/\*.*\\\$COPYRIGHT\\\$/ {
+	r $clicense
+	d
+}
+
+# shell script with # style comments
+/#.*\\\$COPYRIGHT\\\$/ {
+	r $shelllicense
+	d
+}
+
+# LaTeX script with % style comments
+/%.*\\\$COPYRIGHT\\\$/ {
+	r $texlicense
+	d
+}
+
+# Text with no comments (i.e., everything else)
+/\\\$COPYRIGHT\\\$/ {
+	r $textlicense
+	d
+}
+EOF
+
+# Now substitue them in.  Can only do 25 files at a time due to
+# limitations in csh's foreach (you can overload it with too many
+# words)
+
+set num=25
+set num1="`expr $num + 1`"
+set cur="`head -$num $filelist`"
+tail +$num1 $filelist > $filelist.2
+mv -f $filelist.2 $filelist
+
+while ("$cur" != "")
+    foreach arg ($cur)
+	echo checking file $arg
+	if (-f $arg && "$arg" != "dist/insertlic.csh" && \
+	    "$arg" != "./dist/insertlic.csh" && \
+	    "$arg" != "insertlic.csh") then
+	    sed -f $tempdir/license.sed $arg > $arg.new 
+	    diff $arg $arg.new > /dev/null
+	    if ("$status" != 0) then
+	    
+		# Have to take a few pains to ensure that the original perms
+		# are preserved, but ensure that all have read perms
+	    
+		set wantx=0
+		set wantw=0
+		if (-x $arg) then
+		    set wantx=1
+		endif
+		if (-w $arg) then
+		    set wantw=1
+		endif
+		
+		cp -f $arg.new $arg
+		rm $arg.new
+		chmod a+r $arg
+		
+		if ("$wantx" == "1") then
+		    chmod a+x $arg
+		endif
+		if ("$wantw" == "1") then
+		    chmod u+w $arg
+		endif
+	    else
+		rm -f $arg.new
+	    endif
+	endif
+    end
+
+    # Get the next $num files
+
+    set cur="`head -$num $filelist`"
+    tail +$num1 $filelist > $filelist.2
+    mv -f $filelist.2 $filelist
+end
+
+# Now remove the temporary files
+
+rm -rf $tempdir
+
+exit 0
--- dist/make-dist.sh
+++ dist/make-dist.sh
@@ -1,26 +1,59 @@
 #!/bin/sh
 #
-# $Id: make-dist.sh,v 1.2 2001/12/19 02:36:54 jsquyres Exp $
+# $Id: make-dist.sh,v 1.3 2001/12/22 01:03:43 jsquyres Exp $
 #
 # $COPYRIGHT$
 #
 
-distdir="`pwd`/$1"
+srcdir="`pwd`"
+distdir="$srcdir/$1"
 
-echo "*** Customizing OSCAR distribution"
+OSCAR_VERSION="`sh dist/get-oscar-version.sh $srcdir --full`"
+OSCAR_MAJOR_VERSION="`sh dist/get-oscar-version.sh $srcdir --major`"
+OSCAR_MINOR_VERSION="`sh dist/get-oscar-version.sh $srcdir --minor`"
+OSCAR_RELEASE_VERSION="`sh dist/get-oscar-version.sh $srcdir --release`"
+OSCAR_ALPHA_VERSION="`sh dist/get-oscar-version.sh $srcdir --alpha`"
+OSCAR_BETA_VERSION="`sh dist/get-oscar-version.sh $srcdir --beta`"
+
+if test "$distdir" = ""; then
+    echo "Must supply distdir as argv[1] -- aborting"
+    exit 1
+fi
+
+start=`date`
+cat <<EOF
+
+============================================================================== 
+Creating OSCAR distribution version $OSCAR_VERSION
+In directory: `pwd`
+Started: $start
+============================================================================== 
+ 
+EOF
 
 #
-# Make the docs
+# STILL IN THE SOURCE TREE...
+#
+# We need to build the docs here in the source tree because we can run
+# autogen.sh and configure here; we can't do that in the disttree.
+#
+
+#
+# Run autogen.sh and configure if necessary
 #
 
 if ! test -f configure; then
-    echo " - running GNU auto tools"
-    sh ./autogen.sh
+    echo " - Running GNU auto* tools"
+    ./autogen.sh
 fi
 if ! test -f Makefile; then
-    echo " - running configure"
+    echo " - Running configure"
     ./configure
 fi
+
+#
+# Build the docs
+#
 
 echo " - building installation docs"
 cd doc/installation
@@ -36,9 +69,68 @@ make mostlyclean pdf
 make mostlyclean
 cp intro.ps intro.pdf $distdir/doc/introduction
 
+#########################################################
+# VERY IMPORTANT: Now go into the new distribution tree #
+#########################################################
+
+cd $distdir
+umask 022
+
+#
+# Put in those headers
+#
+ 
+echo "*** Inserting license headers..."
+
+cd $distdir
+filelist=/tmp/oscar-license-filelist.$$
+rm -f $filelist
+cat > $filelist <<EOF
+README
+README.ia64
+EOF
+find dist -type f -print >> $filelist
+find doc -type f -print | egrep -v '.png$' >> $filelist
+find images -type f -print | egrep -v '.gif$' >> $filelist
+find lib -type f -print >> $filelist
+find oscarsamples -type f -print >> $filelist
+find packages -type f -print | egrep -v '.rpm$' >> $filelist
+find scripts -type f -print >> $filelist
+find testing -type f -print >> $filelist
+
+#
+# If this is a beta, prepend the beta notice to the license.  
+#
+
+if test "$OSCAR_BETA_VERSION" != "" -o "$OSCAR_ALPHA_VERSION" != ""; then
+    echo " - This is a BETA version"
+    file=/tmp/oscar-license.$$
+    rm -f $file
+    cat dist/beta-notice.txt dist/license.hdr > $file
+    mv -f $file dist/license.hdr
+    echo " - Ammended license notice ready"
+fi
+
+csh -f ./dist/insert-license.csh $filelist
+rm -rf $filelist
+
+rm -f dist/insertlic.*
+rm -f dist/license.hdr
+rm -f dist/beta-notice.txt
+
 #
 # All done
 #
 
-echo "*** All done!"
+cat <<EOF
+
+============================================================================== 
+OSCAR version $ver distribution created
+ 
+Started: $start
+Ended:   `date`
+============================================================================== 
+ 
+EOF
+
 exit 0
