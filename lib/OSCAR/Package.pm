dea7739d1ba2cf0664f91e9776496eee7fd26b60
sdague <sdague@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-02-18 02:19:54 +0000

    added new Package.pm which provides some library calls for getting information from packages
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@267 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: df1cc111462d8a50983658bef836fd3c0bf81649 [formerly bc0a5b4b5e20f7575153d3ae6566474f274fff4a]
    Former-commit-id: 1da26d3b350e63d50a346ed3fcd9d0fb2f38e443

---
+++ lib/OSCAR/Package.pm
@@ -0,0 +1,132 @@
+package OSCAR::Package;
+
+#   $Id: Package.pm,v 1.1 2002/02/18 02:19:54 sdague Exp $
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+ 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+ 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#   Copyright 2001-2002 International Business Machines
+#                       Sean Dague <japh@us.ibm.com>
+
+use strict;
+use vars qw(@EXPORT $VERSION $RPM_TABLE $RPM_POOL @COREPKGS %PHASES);
+use base qw(Exporter);
+use Carp;
+
+$RPM_TABLE = {};
+
+# Trying to figure out the best way to set this.
+$RPM_POOL = $ENV{OSCAR_RPMPOOL} || '/tftpboot/rpm';
+
+$VERSION = sprintf("%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/);
+
+# This defines which packages are core packages (i.e. MUST be installed before
+# the wizard comes up)
+@COREPKGS = qw(c3 sis);
+
+# The list of phases that are valid for package install.  For more info, please
+# see the developement doc
+
+%PHASES = (
+           post_server_install => 'post_server_install',
+           post_rpm_install => 'post_rpm_install',
+           post_clients => 'post_clients',
+           post_install => 'post_install',
+          );
+
+
+#
+# list_pkg - this returns a list of packages.
+#          you may specify "core" or "noncore" as the first
+#          argument to get a list of core or noncore packages instead of all packages
+#
+#          Basic theory, if you want core, return the core list.  If you want 
+#          something else, read the package directory, if 'noncore' through away
+#          core packages as you hit them.
+#
+
+sub list_pkg {
+    my $type = shift;
+    my @pkgs = ();
+    if($type eq "core") {
+        @pkgs = @COREPKGS;
+    } else {
+        opendir(PKGDIR,"<$ENV{OSCAR_HOME}/packages") or (carp("Couldn't open $ENV{OSCAR_HOME}/packages for reading"), return undef);
+        while(<PKGDIR>) {
+            unless (/^(\.|CVS)/) {
+                my $pkg = $_;
+                chomp($pkg);
+                unless($type eq "noncore" and _is_core($pkg)) {
+                    push @pkgs, $pkg;
+                }
+            }
+        }
+        close(PKGDIR);
+    }
+    return @pkgs;
+}
+
+sub _is_core {
+    my $pkg = shift;
+    for (@COREPKGS) {
+        if($pkg eq $_) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+#
+# run_pkg_script - runs the package script for a specific package
+#
+
+
+sub run_pkg_script {
+    my ($pkg, $phase) = @_;
+    my $scriptname = $->{$phase};
+    if(!$scriptname) {
+        carp("No such phase '$phase' in OSCAR package API");
+        return undef;
+    }
+    my $script = $ENV{OSCAR_HOME} . "/packages/" . $pkg . "/scripts/" . $scriptname;
+    if(-e $script) {
+        my $rc = system($script);
+        if($rc) {
+            my $realrc = $rc >> 8;
+            carp("Script $script exitted badly with exit code '$realrc'");
+            return 0;
+        }
+        return 1;
+    }
+    return 1;
+}
+
+sub rpmlist {
+    my ($pkg, $type) = @_;
+    my $listfile = ($type eq "client") ? "client.rpmlist" : "server.rpmlist";
+    my $file = "$ENV{OSCAR_HOME}/packages/$pkg/$listfile";
+    my @rpms = ();
+    open(IN,"<$file") or carp("Couldn't open package list file $file for reading!");
+    while(<IN>) {
+        # get rid of comments
+        s/\#.*//;
+        if(/(\S+)/) {
+            push @rpms, $1;
+        }
+    }
+    close(IN);
+    return @rpms;
+}
+
+1;
