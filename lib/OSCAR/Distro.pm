67a3b742fcab4b33c4652b3894f6bd8fcae03d8a
sdague <sdague@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-02-21 19:06:45 +0000

    added small library to figure out what distro version things are
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@308 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 8de96bfe959e7f93bd146109e00b6a9665def4e9 [formerly 2f13076ef2a0c505558ca1a30a767c39d854be47]
    Former-commit-id: 2981216e29f915e3132adebcfdfac07546dd74a1

---
+++ lib/OSCAR/Distro.pm
@@ -0,0 +1,50 @@
+package OSCAR::Distro;
+
+#   $Id: Distro.pm,v 1.1 2002/02/21 19:06:45 sdague Exp $
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+ 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+ 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#   Copyright 2002 International Business Machines
+#                  Sean Dague <japh@us.ibm.com>
+
+use strict;
+use vars qw($VERSION @EXPORT);
+use Carp;
+use base qw(Exporter);
+@EXPORT = qw(which_distro);
+
+$VERSION = sprintf("%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/);
+
+my $DISTROFILES = {
+                   'mandrake-release' => 'Mandrake',
+                   'redhat-release' => 'RedHat',
+                  };
+
+sub which_distro {
+    my $directory = shift;
+    my $version = "0.0";
+    my $name = "UnknownLinux";
+    foreach my $file (keys %$DISTROFILES) {
+        my $output = `rpm -q --qf '\%VERSION' $directory/$file*`;
+        if($output =~ /^([\w\.]+)/) {
+            $version = $1;
+            $name = $DISTROFILES->{$file};
+            last;
+        }
+    }
+    return ($name, $version);
+}
+
+1;
