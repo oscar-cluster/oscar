46fcf2312367bf75e0146a3f56845ebdb5dd2852
mchasal <mchasal@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-05-17 20:56:43 +0000

    Add/Del node panels.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@533 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: def3ef8fe9f2d52134e5bf08fe147549b52387b0 [formerly 486df7802038e238a12bfa300b547eb971c9eb13]
    Former-commit-id: 404302904feb7bf2b4db11add03262beb7e0334a

---
+++ lib/OSCAR/AddNode.pm
@@ -0,0 +1,59 @@
+package OSCAR::AddNode;
+
+#   $Id: AddNode.pm,v 1.1 2002/05/17 20:56:43 mchasal Exp $
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+ 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+ 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#   Copyright 2001-2002 International Business Machines
+#                       Sean Dague <japh@us.ibm.com>
+
+use strict;
+use vars qw($VERSION @EXPORT);
+use Tk;
+use Carp;
+use SystemInstaller::Tk::Common;
+use base qw(Exporter);
+use SIS::Image;
+@EXPORT = qw(addnode_window);
+
+$VERSION = sprintf("%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/);
+
+sub addnode_window {
+    my ($parent, $vars) = @_;
+
+    my $window = $parent->Toplevel;
+    $window->title("Add Oscar Nodes");
+    my $inst=$window->Label (-text=>"Perform the following steps to add nodes to your OSCAR cluster",-relief=>"groove");
+    $inst->grid("-","-",-sticky=>"nsew");
+    &main::oscar_button($window, "Step 1:", "Define OSCAR Clients", [\&main::build_oscar_clients, $window, $main::interface], 'addclients');
+    &main::oscar_button($window, "Step 2:", "Setup Networking", [\&main::mac_window, $window, {interface => $main::interface}], 'netboot');
+    my $boot=$window->Label (-text=>"Before continuing, network boot all of your nodes. 
+    Once they have completed installation, reboot them from 
+    the hard drive. Once all the machines and their ethernet
+    adaptors are up, move on to the next step.",-relief=>"groove");
+    $boot->grid("-","-",-sticky=>"nsew");
+
+    &main::oscar_button($window, "Step 3:", "Complete Cluster Setup", [\&main::run_post_install, $window], 'post_install');
+
+    my $exitbutton = $window->Button(
+                                     -text => "Close",
+                                     -command => sub {$window->destroy},
+                                    );
+
+    $exitbutton->grid("-",-sticky => "ew");
+
+}
+
+1;
---
+++ lib/OSCAR/DelNode.pm
@@ -0,0 +1,118 @@
+package OSCAR::DelNode;
+
+#   $Id: DelNode.pm,v 1.1 2002/05/17 20:56:43 mchasal Exp $
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+ 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+ 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#   Copyright 2001-2002 International Business Machines
+#                       Sean Dague <japh@us.ibm.com>
+
+use strict;
+use Net::Netmask;
+use vars qw($VERSION @EXPORT);
+use Tk;
+use Tk::Tree;
+use Carp;
+use SystemInstaller::Tk::Common;
+use File::Copy;
+use base qw(Exporter);
+use SIS::Client;
+@EXPORT = qw(delnode_window);
+
+$VERSION = sprintf("%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/);
+
+sub delnode_window {
+    my ($parent, $vars) = @_;
+
+    my $window = $parent->Toplevel;
+    $window->title("Delete Oscar Nodes");
+
+    my $listbox = $window->ScrlListbox(
+                                       -selectmode => 'multiple',
+                                       -background => "white",
+                                      );
+
+    $listbox->grid("-");
+    fill_listbox($listbox);
+
+    
+    my $deletebutton = $window->Button(
+                                      -text => "Delete clients",
+                                      -command => [\&delnodes, $window, $listbox],
+                                     );
+    my $exitbutton = $window->Button(
+                                     -text => "Close",
+                                     -command => sub {$window->destroy},
+                                    );
+
+    $deletebutton->grid($exitbutton,-sticky => "ew");
+}
+
+sub fill_listbox {
+        my $listbox=shift;
+        my @elements;
+        my @clients = clientList();
+        foreach my $client (@clients) {
+                push (@elements,$client->{NAME});
+        }
+        $listbox->delete(0,'end');
+        $listbox->insert(0,@elements);
+        $listbox->update;
+        return 1;
+}
+
+sub delnodes {
+        my $window=shift;
+        my $listbox=shift;
+        my @clients;
+        my @elements=$listbox->curselection;
+        foreach my $index (@elements) {
+                push @clients,$listbox->get($index);
+        }
+        my $clientstring=join(",",@clients);
+        if (system("mksimachine --Delete --name $clientstring")) {
+                carp("Failed to delete machines $clientstring");
+                fill_listbox($listbox);
+                error_window($window,"Failed to delete requested clients.");
+                return 0;
+        } else {
+                my $fail=0;
+                print "Executing post_clients phase\n";
+                if (system("./post_clients")) {
+                        carp("post_clients phase failed.");
+                        $fail++;
+                }
+                print "Executing post_install phase\n";
+                if (system("./post_install")) {
+                        carp("post_install phase failed.");
+                        $fail++;
+                }
+                fill_listbox($listbox);
+                if ($fail) {
+                        error_window($window,"Clients deleted, but reconfiguration failed.");
+                        return 0;
+                } else {
+                        done_window($window,"Clients deleted.");
+                        return 1;
+                }
+        }
+
+}
+
+
+
+
+
+1;
