2b18f4e2b8977d372b140dccc7e2e77a9c1875a5
jsquyres <jsquyres@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-08-17 04:39:21 +0000

    Addition of two OSCAR functions: - oscar_log_section - oscar_log_subsection These are now used throughout the OSCAR installer scripts.  They prettyprint logging messages so that a) the user has nice, easy things to read, and b) when users submit oscarinstall.log files, we developers can tell much easier exactly what happened (or not) during their install.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@971 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 0529c8b98d8b580ff440597c9f8ae4f732b317ed [formerly ca054654a9762f08a6bbc613e4bc4f4e327f9098] [formerly d35c1520dd1e3e435163615ead6f9a2bfc2bea4c [formerly 3addb6c94122f71e29180d522d4caab5fc04cd76]]
    Former-commit-id: bfbe4011732d28e6a7889fb27167168e445c5790 [formerly 14f37d57aeb1722a8fce124ca20e65073dc7aca6]
    Former-commit-id: f5293480496cbb9641c8d169cf2145728625bc1e

---
+++ lib/OSCAR/Logger.pm
@@ -0,0 +1,61 @@
+package OSCAR::Logger;
+
+#   $Id: Logger.pm,v 1.1 2002/08/17 04:39:21 jsquyres Exp $
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+ 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+ 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#   Copyright 2002 Jeffrey M. Squyres <jsquyres@lam-mpi.org>
+
+use strict;
+use vars qw($VERSION @EXPORT);
+use base qw(Exporter);
+
+@EXPORT = qw(oscar_log_section oscar_log_subsection);
+
+$VERSION = sprintf("%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/);
+
+
+#############################################################################
+#
+# Simple routine to output a "section" title to stdout
+#
+#############################################################################
+
+sub oscar_log_section {
+    my $title = shift;
+
+    print "
+=============================================================================
+== $title
+=============================================================================
+
+";
+}
+
+
+#############################################################################
+#
+# Simple routine to output a "subsection" title to stdout
+#
+#############################################################################
+
+sub oscar_log_subsection {
+    my $title = shift;
+
+    print "--> $title\n"
+}
+
+
+1;
