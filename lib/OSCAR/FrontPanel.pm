3f135af0fa22bd6069ba9e2b78381ae3f3c8447c
jsquyres <jsquyres@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-08-17 20:13:07 +0000

    Lots of changes: - when step 1 is active, the main OSCAR wizard window becomes "busy" (icon   goes to a stopwatch, and you can't click in the window) - when you click on "prepare server" in the step 1 window, that window   also goes "busy" - when the "done" window appears indicating success (or failure), clicking   on "ok" will destroy the step 1 window, and make the main window unbusy - if you try to run step 1 again, you'll get a window saying "You have   already run this step.  Are you sure you want to run it again?" with yes   and no buttons in it, and the main OSCAR wizard window is set to busy.   If you click yes, you'll get the step 1 window, and it's the same as   above.  If you click no, you'll go back to the main window, which has   been made unbusy.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@979 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: dbd1d80dff3c075ae7f877e5b12059b8dc3155a0 [formerly 52a571b78551fa1d3caf226f0f28571577eed19e] [formerly f7b3379e8453b5513c2b5e2967dfcbf41f7805e4 [formerly e5c160f293672b9b03b02a6e771ba0cf317ae654]]
    Former-commit-id: 8b1ff835868d935b46f395b0fcb7038489ed9a2b [formerly 992ee42728bd63bdd54c4350c76571e7050f5ee4]
    Former-commit-id: 2ce4425114d68517124f368a7d5a8d1f6158676f

--- lib/OSCAR/FrontPanel.pm
+++ lib/OSCAR/FrontPanel.pm
@@ -1,6 +1,6 @@
 package OSCAR::FrontPanel;
 
-#   $Id: FrontPanel.pm,v 1.10 2002/08/17 04:50:55 jsquyres Exp $
+#   $Id: FrontPanel.pm,v 1.11 2002/08/17 20:13:07 jsquyres Exp $
 
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -26,95 +26,209 @@ use vars qw($VERSION @EXPORT);
 use Tk;
 use Tk::Tree;
 use Carp;
-use SystemInstaller::Tk::Common;
 use File::Copy;
 use base qw(Exporter);
 use OSCAR::Logger;
+use OSCAR::Tk;
 @EXPORT = qw(frontpanel_window);
 
-$VERSION = sprintf("%d.%02d", q$Revision: 1.10 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.11 $ =~ /(\d+)\.(\d+)/);
 
 my %MAC = (); # mac will be -1 for unknown, machine name for known
 my $COLLECT = 0;
 my $PINGPID = undef;
 
+# Text and values for the two MPI's that are currently included in
+# OSCAR.  These will someday change/be moved elsewhere when we have
+# proper MPI package detection.
+
+my $lam_text = "LAM/MPI 6.5.6";
+my $lam_value = "lam-6.5.6";
+my $mpich_text = "MPICH 1.2.4";
+my $mpich_value = "mpich-1.2.4";
+
+# Globals to the module
+
+my ($parent_window, $fp_window);
+my ($setupbutton, $exitbutton);
 my ($lambutton, $mpichbutton, $mpivalue);
+my $have_run_already = 0;
+my $vars;
+
+#
+# Initial front panel window code.  Set the parent window to busy and
+# sanity check to see if we have already run step 1.
+#
 
 sub frontpanel_window {
-    my ($parent, $vars) = @_;
+    $parent_window = shift;
+    $vars = shift;
 
     oscar_log_section("Running step 1 of the OSCAR wizard");
 
-    my $window = $parent->Toplevel;
-    $window->title("OSCAR Server Prepartation");
+    # Make the top-level OSCAR wizard window busy so that the user
+    # can't click in another step while this one is running.
+
+    $parent_window->Busy(-recurse => 1);
+
+    # Before we do anything else, see if we have already run this step
+
+    if ($have_run_already) {
+	oscar_log_subsection("Warning: have already run this step");
+	yesno_window($parent_window,
+		     "Re-run step?",
+		     "WARNING: You have already run this step.  " .
+		     "Do you really want to run it again?",
+		     \&fp_yes, \&fp_no);
+	return 1;
+    } else {
+	return do_fp_work();
+    }
+}
+
+
+#
+# Entry point for where we have already run step 1, yet the user
+# selects to run it again.
+#
+
+sub fp_yes {
+    do_fp_work();
+    return 1;
+}
+
+
+#
+# Entry point for where we have already run step 1, and the user
+# selects to *not* run it again.
+#
+
+sub fp_no {
+    $parent_window->Unbusy();
+    return 1;
+}
+
+
+#
+# Entry point to create the actual frontpanel window
+#
+
+sub do_fp_work {
+    # Make a new window for this step
+
+    $fp_window = $parent_window->Toplevel;
+    $fp_window->title("OSCAR Server Prepartation");
     
-    my $instructions = $window->Message(-aspect => 500, -text => "Welcome to the OSCAR Cluster Installer.  Before we get started we need to ask you a couple of questions.\n");
+    my $instructions = 
+	$fp_window->Message(-aspect => 500, 
+			    -text => "Welcome to the OSCAR Cluster Installer.  Before we get started we need to ask you a couple of questions.\n");
 
-    my $mpitext = $window->Message(-aspect => 400, -text => "Which MPI Implementation do you wish to use by default? If you don't know, the default value should be appropriate.");
+    my $mpitext = 
+	$fp_window->Message(-aspect => 400, 
+			    -text => "Which MPI Implementation do you wish to use by default? If you don't know, the default value should be appropriate.");
 
-    $lambutton = $window->Radiobutton(-text => "LAM/MPI 6.5.6", -value => "lam-6.5.6",
-                                      -variable => \$mpivalue, -command => \&set_mpi);
-    $mpichbutton = $window->Radiobutton(-text => "MPICH 1.2.4", -value => "mpich-1.2.4",
-                                      -variable => \$mpivalue, -command => \&set_mpi);
+    $lambutton = $fp_window->Radiobutton(-text => $lam_text, 
+					 -value => $lam_value,
+					 -variable => \$mpivalue, 
+					 -command => \&set_mpi);
+    $mpichbutton = $fp_window->Radiobutton(-text => $mpich_text, 
+					   -value => $mpich_value,
+					   -variable => \$mpivalue, 
+					   -command => \&set_mpi);
 
-    my $setupbutton = $window->Button(
-                                      -text => "Prepare Server for OSCAR",
-                                      -command => [\&server_prep, $vars, $window],
-                                     );
-    my $exitbutton = $window->Button(
-                                     -text => "Close",
-                                     -command => sub {$window->destroy},
-                                    );
+    $exitbutton = 
+	$fp_window->Button(-text => "Close",
+			   -command => \&fp_window_close);
+    $setupbutton = 
+	$fp_window->Button(-text => "Prepare Server for OSCAR",
+			   -command => [\&server_prep]);
 
     $instructions->grid("-",-sticky => "ew");
     $mpitext->grid("-");
     $lambutton->grid($mpichbutton);
     $setupbutton->grid("-",-sticky => "ew");
     $exitbutton->grid("-",-sticky => "ew");
-    # set lam as the default
-    $mpivalue = "lam-6.5.6";
+
+    # Set LAM as the default
+
+    $mpivalue = $lam_value;
 }
+
+
+#
+# Entry point for when one of the MPI buttons is set.  Really only for
+# the purposes of printing out.
+#
 
 sub set_mpi {
     my ($button) = @_;
-    print $mpivalue,"\n";
-    
+    oscar_log_subsection("Step 1: MPI selected: $mpivalue\n");
 }
 
+
+#
+# Do the actual work of step 1.
+#
+
 sub server_prep {
-    my $vars = shift;
-    my $window = shift;
-    $window->Busy(-recurse => 1);
+    # Make this window busy so that the user can't click on anything
+    # while this step is running.
+
+    $fp_window->Busy(-recurse => 1);
+
+    # Mark it so that we know that we have already run this step
+
+    $have_run_already = 1;
 
     my $cmd = "./server_prep $$vars{interface}";
     oscar_log_subsection("Step 1: Running: $cmd");
     open(OUTPUT,"$cmd |") or (carp("Couldn't run command $cmd"), 
-                              error_window($window,"Couldn't run command $cmd"),
-                              $window->Unbusy(), return undef);
+                              error_window($fp_window,
+					   "Couldn't run command $cmd"),
+			      fp_window_close(),
+			      return undef);
 
     while(<OUTPUT>) {
         print $_;
-        $window->update();
+        $fp_window->update();
     }
 
-
     close(OUTPUT) or (carp("Couldn't run command $cmd"), 
-                      error_window($window,"Couldn't run command $cmd"),
-                      $window->Unbusy(), return undef);
+                      error_window($fp_window,"Couldn't run command $cmd"),
+		      fp_window_close(),
+		      return undef);
     oscar_log_subsection("Step 1: Successfully ran command");
 
     $cmd = "bash -c 'switcher mpi --add-attr default $mpivalue --force --system'";
     oscar_log_subsection("Step 1: Running: $cmd");
     !system("$cmd") or
         (carp("Couldn't run command $cmd"),
-         error_window($window,"Couldn't run command $cmd"),
-         $window->Unbusy(), return undef);
+         error_window($fp_window,"Couldn't run command $cmd"),
+	 fp_window_close(),
+	 return undef);
     oscar_log_subsection("Step 1: Successfully ran command");
     oscar_log_subsection("Step 1: Completed successfully");
 
-    done_window($window,"Successfully prepared server for OSCAR installation"),
-    $window->Unbusy();
+    done_window($fp_window,
+		"Successfully prepared server for OSCAR installation",
+		\&fp_window_close);
+
     return 1;
 }
+
+
+#
+# When we close the frontpanel window, do some cleanup
+#
+
+sub fp_window_close {
+
+    # Destroy this step's window, and make the top-level OSCAR wizard
+    # window unbusy.
+
+    $fp_window->destroy();
+    $parent_window->Unbusy();
+}
+
 
 1;
---
+++ lib/OSCAR/Tk.pm
@@ -0,0 +1,132 @@
+package OSCAR::Tk;
+
+#   $Id: Tk.pm,v 1.1 2002/08/17 20:13:07 jsquyres Exp $
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+ 
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+ 
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#   Copyright 2001-2002 International Business Machines
+#                       Sean Dague <japh@us.ibm.com>
+
+use strict;
+use vars qw($VERSION @EXPORT);
+use Tk;
+use Carp;
+use base qw(Exporter);
+use OSCAR::Logger;
+use OSCAR::Tk;
+@EXPORT = qw(yesno_window done_window);
+
+$VERSION = sprintf("%d.%02d", q$Revision: 1.1 $ =~ /(\d+)\.(\d+)/);
+
+# Module-specific variables
+
+my $parent_window;
+my $yn_window;
+my ($on_yes, $on_no);
+
+
+sub yesno_window {
+    $parent_window = shift;
+    my $title = shift;
+    my $label = shift;
+    $on_yes = shift;
+    $on_no = shift;
+    my @args = @_;
+
+    # Make the parent window busy
+    
+    $parent_window->Busy(recurse => 1);
+
+    # Make the new yes/no window
+
+    $yn_window = $parent_window->Toplevel;
+
+    # Fill in the widgets
+
+    if (!$title) {
+	$title = "Are you sure?";
+    }
+    if (!$label) {
+	$label = $title;
+    }
+    $yn_window->title($title);
+    my $label_widget = $yn_window->Message(-aspect => 500,
+					   -text => $label,
+					   -foreground => "blue");
+    my $yes_button = $yn_window->Button(-text => "Yes",
+					-command => [\&yesno_close, 1, @args]);
+    my $no_button = $yn_window->Button(-text => "No",
+				       -command => [\&yesno_close, 0, @args]);
+
+    $label_widget->grid("-", -sticky => "ew");
+    $yes_button->grid($no_button);
+}
+
+
+sub yesno_close {
+    my $value = shift;
+    my @args = @_;
+
+    $yn_window->destroy;
+    $parent_window->Unbusy();
+
+    if ($value == 0 && ref($on_no) eq "CODE") {
+	&$on_no(@args);
+    } elsif ($value == 1 && ref($on_yes) eq "CODE") {
+	&$on_yes(@args);
+    }
+
+    1;
+}
+
+#
+# These two subs stolen from SystemImager/Tk/Common.pm because we need
+# the ordering of close_after to be different -- destroy the "done"
+# window, and *then* invoke the callbacks.
+#
+
+sub done_window {
+    my ($window, $message, $onclose, @args) = @_;
+    my $done = $window->Toplevel();
+    $done->title("Done!");
+
+    my $label = $done->Message(-text => $message, 
+                               -foreground => "blue");
+    $label->grid();
+
+    my $button = $done->Button(-text=>"Close",
+                               -command=> [\&done_window_close, $done,
+					   $onclose, @args],
+                               -pady => 8,
+                               -padx => 8);
+    $button->grid();
+
+    1;
+}
+
+
+sub done_window_close {
+    my ($window, $onclose, @args) = @_;
+    $window->destroy;
+
+    if(ref($onclose) eq "CODE") {
+        &$onclose(@args);
+    }
+
+    1;
+}
+
+
+1;
