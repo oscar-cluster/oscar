cfd94e45922d1ab2db2ff3386b98b02b1e3b0b45
sad <sad@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-08-22 09:01:02 +0000

    added PyXML license info and added software info & license verbage to top of shell scripts I just checked in
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@1019 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: b2fa6e54921fb3d028fab1bc59e0490f155ae95a [formerly 18407fb2e1c6a1f5e4871d2ceb2ff1790adcfab9] [formerly e29724748a05b9029b66d777b737a452edacccb6 [formerly 4d62d0eb63a4c79f62aa948dd5db5dd71f9f3173]]
    Former-commit-id: f48a03092633e6add94177ce9920c9ea4f0d154b [formerly 9189e75e1689f4eb60c1a9a319467b14e970e88c]
    Former-commit-id: 639b1b587e41abba3b7624df14dd927247d10687

--- testing/OSCAR_test/ganglia/ganglia_test.sh
+++ testing/OSCAR_test/ganglia/ganglia_test.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# $Id: ganglia_test.sh,v 1.2 2002/08/22 09:01:02 sad Exp $
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+#   ganglia Server test script
+#   Steven A. DuChene  <linux-clusters@mindspring.com>
+
 gang=/usr/sbin/ganglia
 
  if [ -x $gang ]; then
