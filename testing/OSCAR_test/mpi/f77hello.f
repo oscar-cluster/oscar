253150e572ac9505d11039f8ec2ff3eb08dbb1c9
jsquyres <jsquyres@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-08-28 12:50:06 +0000

    Including 'mpif.h' would be helpful.  Duh!
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@1063 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 00effd56d0af23284b915e4eca530efda1635541 [formerly c2665312de48e1ec4da40a759c82fa33c84e3928] [formerly 7fdadb7db0a16f6d32b28972e76c3a712240312d [formerly 159fe3b16f2751597f3f974d325e49d7abd25f5d]]
    Former-commit-id: cd6b631b8dcd71e4726fa7601a586e0a404e61a2 [formerly 4b7ec0c8313297d7239d6e1abd88a1dd8b8e39ff]
    Former-commit-id: 8a6887e078c223a79e601dba9b38d0d670cc37eb

--- testing/OSCAR_test/mpi/f77hello.f
+++ testing/OSCAR_test/mpi/f77hello.f
@@ -1,11 +1,12 @@
 c
-c	$Id: f77hello.f,v 1.1 2002/08/27 23:57:58 jsquyres Exp $
+c	$Id: f77hello.f,v 1.2 2002/08/28 12:50:06 jsquyres Exp $
 c
 c	$COPYRIGHT$
 c
 	program hello
-	integer		rank
-	integer		size
+	include 'mpif.h'
+	integer rank
+	integer size
 
 	call MPI_INIT(ierror)
 	call MPI_COMM_SIZE(MPI_COMM_WORLD, size, ierror)
