63786bab8f00d4e09e9608aac62905a4d4997ce2
jsquyres <jsquyres@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-09-04 05:55:12 +0000

    Fix for SF bug 599339 -- only close the parent window after we close the error window, by making the action of the error window be an unnamed sub that closes the parent window (in the local scope).
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@1095 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 21834129acb8653175e7704a45e10b63e7f8c38f [formerly eaf102924f641e2401b7f6447ecbb6e32f463252] [formerly e3fc6bb183a750b515379c3885e7c3418f751f74 [formerly 58ad2096bc3e415916980737372a635e2c5664fc]]
    Former-commit-id: a236d2efbbd9ca65742687e9fa82052d40dd5e12 [formerly bc51b3df77c0c13bf258aff3de4945568ad8bb18]
    Former-commit-id: 05596719e356d014e693a79bdf7b1c20b9d837b3

--- lib/OSCAR/FrontPanel.pm
+++ lib/OSCAR/FrontPanel.pm
@@ -1,6 +1,6 @@
 package OSCAR::FrontPanel;
 
-#   $Id: FrontPanel.pm,v 1.11 2002/08/17 20:13:07 jsquyres Exp $
+#   $Id: FrontPanel.pm,v 1.12 2002/09/04 05:55:12 jsquyres Exp $
 
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -32,7 +32,7 @@ use OSCAR::Logger;
 use OSCAR::Tk;
 @EXPORT = qw(frontpanel_window);
 
-$VERSION = sprintf("%d.%02d", q$Revision: 1.11 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.12 $ =~ /(\d+)\.(\d+)/);
 
 my %MAC = (); # mac will be -1 for unknown, machine name for known
 my $COLLECT = 0;
@@ -184,8 +184,8 @@ sub server_prep {
     oscar_log_subsection("Step 1: Running: $cmd");
     open(OUTPUT,"$cmd |") or (carp("Couldn't run command $cmd"), 
                               error_window($fp_window,
-					   "Couldn't run command $cmd"),
-			      fp_window_close(),
+					   "Couldn't run command $cmd",
+					   sub { fp_window_close() }),
 			      return undef);
 
     while(<OUTPUT>) {
@@ -194,18 +194,21 @@ sub server_prep {
     }
 
     close(OUTPUT) or (carp("Couldn't run command $cmd"), 
-                      error_window($fp_window,"Couldn't run command $cmd"),
-		      fp_window_close(),
+                      error_window($fp_window,
+				   "Couldn't run command $cmd",
+				   sub { fp_window_close() }),
 		      return undef);
+
     oscar_log_subsection("Step 1: Successfully ran command");
 
     $cmd = "bash -c 'switcher mpi --add-attr default $mpivalue --force --system'";
     oscar_log_subsection("Step 1: Running: $cmd");
     !system("$cmd") or
         (carp("Couldn't run command $cmd"),
-         error_window($fp_window,"Couldn't run command $cmd"),
-	 fp_window_close(),
+         error_window($fp_window, "Couldn't run command $cmd", 
+		      sub { fp_window_close() }), 
 	 return undef);
+
     oscar_log_subsection("Step 1: Successfully ran command");
     oscar_log_subsection("Step 1: Completed successfully");
 
