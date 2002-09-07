d4cc8e51b810076993efad1c17a7c8236b4d6d2f
jsquyres <jsquyres@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-09-07 14:43:51 +0000

    Silly Jeff -- *test* before committing... must make window unbusy instead of closing it
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@1114 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 91787f6ffa4073183afbbe8cc71cc586b4f35e32 [formerly 98a84f10b0a083e9bb31647fcfc814b5da191711] [formerly 2550ba23c25f2fa39f52c650a9c9e0eaed8a4756 [formerly ffad2eb003b974baece9624f6e5d4fee2c5362ab]]
    Former-commit-id: 7ea50de98961070ef698155f8d805c2a8d700ec6 [formerly dc6d31794dab8f12337702d2c5c090b0b7cbd9e7]
    Former-commit-id: 2caca0b0110ebee9ba9473a3997a060c2dbdf6d0

--- lib/OSCAR/FrontPanel.pm
+++ lib/OSCAR/FrontPanel.pm
@@ -1,6 +1,6 @@
 package OSCAR::FrontPanel;
 
-#   $Id: FrontPanel.pm,v 1.13 2002/09/07 14:33:25 jsquyres Exp $
+#   $Id: FrontPanel.pm,v 1.14 2002/09/07 14:43:51 jsquyres Exp $
 
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -32,7 +32,7 @@ use OSCAR::Logger;
 use OSCAR::Tk;
 @EXPORT = qw(frontpanel_window);
 
-$VERSION = sprintf("%d.%02d", q$Revision: 1.13 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.14 $ =~ /(\d+)\.(\d+)/);
 
 my %MAC = (); # mac will be -1 for unknown, machine name for known
 my $COLLECT = 0;
@@ -184,7 +184,8 @@ sub server_prep {
     oscar_log_subsection("Step 1: Running: $cmd");
     open(OUTPUT,"$cmd |") or (carp("Couldn't run command $cmd"), 
                               error_window($fp_window,
-					   "Couldn't run command $cmd"),
+					   "Couldn't run command $cmd",
+					   sub { $fp_window->Unbusy() }),
 			      return undef);
 
     while(<OUTPUT>) {
@@ -194,7 +195,8 @@ sub server_prep {
 
     close(OUTPUT) or (carp("Couldn't run command $cmd"), 
                       error_window($fp_window,
-				   "Couldn't run command $cmd"),
+				   "Couldn't run command $cmd",
+				   sub { $fp_window->Unbusy() }),
 		      return undef);
 
     oscar_log_subsection("Step 1: Successfully ran command");
@@ -203,7 +205,8 @@ sub server_prep {
     oscar_log_subsection("Step 1: Running: $cmd");
     !system("$cmd") or
         (carp("Couldn't run command $cmd"),
-         error_window($fp_window, "Couldn't run command $cmd"),
+         error_window($fp_window, "Couldn't run command $cmd", 
+		      sub { $fp_window->Unbusy() }),
 	 return undef);
 
     oscar_log_subsection("Step 1: Successfully ran command");
