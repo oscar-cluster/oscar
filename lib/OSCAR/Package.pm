bb4906c16313475ed2c3ccb8bd8704bcc2be69af
jsquyres <jsquyres@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-10-25 12:47:37 +0000

    - Added new public function read_all_pkg_config_xml() - Changed guts of list_pkg() and rpmlist() functions to look at XML - Removed _is_core function -- no longer necessary - Minor formatting changes
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@1216 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: fe08c8ead2f27b4ba6e33f6130be7bcaba937e5e [formerly 1ffa91fbe49a37196269c7f23c3a8a4d318f66dd] [formerly fa09d227fc64b6b957cafd911bdd2861c362ebbd [formerly 49bff15627107eb55806e746eaad8ae71e729289]]
    Former-commit-id: b117336101dd7ac0ab17bc13826447403143fa3e [formerly a6827e650ecb4e5cb88f831f7a7fb687218c3bbf]
    Former-commit-id: d60c919fd216b53edb7660460cc1148d9fd2828e

--- lib/OSCAR/Package.pm
+++ lib/OSCAR/Package.pm
@@ -5,7 +5,7 @@ package OSCAR::Package;
 # Copyright (c) 2002 The Trustees of Indiana University.  
 #                    All rights reserved.
 # 
-#   $Id: Package.pm,v 1.19 2002/10/23 15:04:31 jsquyres Exp $
+#   $Id: Package.pm,v 1.20 2002/10/25 12:47:37 jsquyres Exp $
 
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -29,77 +29,88 @@ use OSCAR::PackageBest;
 use OSCAR::Logger;
 use File::Basename;
 use File::Copy;
+use XML::Simple;
 use Carp;
 
-@EXPORT = qw(list_pkg run_pkg_script run_pkg_script_chroot rpmlist distro_rpmlist install_rpms);
+@EXPORT = qw(list_pkg run_pkg_script run_pkg_script_chroot rpmlist distro_rpmlist install_rpms read_all_pkg_config_xml);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.20 $ =~ /(\d+)\.(\d+)/);
 
 # Trying to figure out the best way to set this.
+
 $RPM_POOL = $ENV{OSCAR_RPMPOOL} || '/tftpboot/rpm';
 
-$VERSION = sprintf("%d.%02d", q$Revision: 1.19 $ =~ /(\d+)\.(\d+)/);
+# XML data from all the packages.
 
-# This defines which packages are core packages (i.e. MUST be installed before
-# the wizard comes up)
+my $PACKAGE_CACHE = undef;
+my $xs = new XML::Simple(keyattr => {}, forcearray => 
+			 [ "site", "uri", 
+			   "rpm",
+			   "requires", "conflicts", "provides" ]);
+
+# This defines which packages are core packages (i.e. MUST be
+# installed before the wizard comes up).  
+
 @COREPKGS = qw(c3 sis switcher);
 
-# The list of phases that are valid for package install.  For more info, please
-# see the developement doc
+# The list of phases that are valid for package install.  For more
+# info, please see the developement doc
 
 %PHASES = (
 	   setup => ['setup'],
-           post_server_install => ['post_server_install','post_server_rpm_install'],
-           post_rpm_install => ['post_client_rpm_install','post_rpm_install'],
+           post_server_install => ['post_server_install',
+				   'post_server_rpm_install'],
+           post_rpm_install => ['post_client_rpm_install',
+				'post_rpm_install'],
            post_clients => ['post_clients'],
-           post_install => ['post_install'],
-          );
-
+           post_install => ['post_install']
+	   );
 
 #
 # list_pkg - this returns a list of packages.
-#          you may specify "core" or "noncore" as the first
-#          argument to get a list of core or noncore packages instead of all packages
 #
-#          Basic theory, if you want core, return the core list.  If you want 
-#          something else, read the package directory, if 'noncore' through away
-#          core packages as you hit them.
+# you may specify "core" or "noncore" as the first argument to get a
+# list of core or noncore packages instead of all packages.  Basic
+# theory, if you want core, return the core list.  If you want
+# something else, read the package directory, if 'noncore' through
+# away core packages as you hit them.
 #
 
 sub list_pkg {
     my $type = shift;
-    my @pkgs = ();
-    if($type eq "core") {
-        @pkgs = @COREPKGS;
-    } else {
-	opendir(PKGDIR,"$ENV{OSCAR_HOME}/packages") or (carp("Couldn't open $ENV{OSCAR_HOME}/packages for reading"), return undef);
-        while($_ = readdir(PKGDIR)) {
-	    my $rel = "$_";
-	    my $abs = "$ENV{OSCAR_HOME}/packages/$rel";
-	    if (-d $abs && $rel ne "CVS" && $rel ne "." && $rel ne ".." && 
-		! -f "$abs/.oscar_ignore") {
-                my $pkg = $rel;
-                chomp($pkg);
-                unless($type eq "noncore" and _is_core($pkg)) {
-                    push @pkgs, $pkg;
-                }
-            }
-        }
-        close(PKGDIR);
-    }
-    return @pkgs;
-}
+    my @packages_to_return = ();
 
-#
-#  _is_core - pretty simple, is the pacakge a core package
-#
+    # If we haven't read in all the package config.xml files, do so.
 
-sub _is_core {
-    my $pkg = shift;
-    for (@COREPKGS) {
-        if($pkg eq $_) {
-            return 1;
-        }
+    read_all_pkg_config_xml() if (!$PACKAGE_CACHE);
+
+    # Now do the work
+
+    my @packages = keys %{$PACKAGE_CACHE};
+    foreach my $pkg (@packages) {
+	
+	# First, check if it's a valid package: not ".", not "..", not
+	# "CVS", doesn't contain a .oscar_ignore file, and has its
+	# installable attribute set to 1.
+	
+	my $dir = "$ENV{OSCAR_HOME}/packages/$pkg";
+	next if (! -d $dir || $pkg eq "." || $pkg eq ".." || $pkg eq "CVS" ||
+		 -f "$dir/.oscar_ignore" ||
+		 $PACKAGE_CACHE->{$pkg}->{installable} ne 1);
+	
+	# If it's a valid package, see if it's the right kind or not
+
+	if ($type eq "core") {
+	    if ($PACKAGE_CACHE->{$pkg}->{class} eq "core") {
+		push @packages_to_return, $pkg;
+	    }
+	} else {
+	    if ($PACKAGE_CACHE->{$pkg}->{class} ne "core") {
+		push @packages_to_return, $pkg;
+	    }
+	}
     }
-    return 0;
+
+    return @packages_to_return;
 }
 
 #
@@ -109,7 +120,7 @@ sub _is_core {
 sub run_pkg_script {
     my ($pkg, $phase, $verbose) = @_;
     my $scripts = $PHASES{$phase};
-    if(!$scripts) {
+    if (!$scripts) {
         carp("No such phase '$phase' in OSCAR package API");
         return undef;
     }
@@ -117,10 +128,10 @@ sub run_pkg_script {
     foreach my $scriptname (@$scripts) {
         my $script = $ENV{OSCAR_HOME} . "/packages/" . $pkg . "/scripts/" . 
 	    $scriptname;
-        if(-e $script) {
+        if (-e $script) {
             oscar_log_subsection("About to run $script for $pkg");
             my $rc = system($script);
-            if($rc) {
+            if ($rc) {
                 my $realrc = $rc >> 8;
                 carp("Script $script exitted badly with exit code '$realrc'");
                 return 0;
@@ -133,14 +144,14 @@ sub run_pkg_script {
 sub run_pkg_script_chroot {
     my ($pkg, $dir) = @_;
     my $scripts = $PHASES{post_rpm_install};
-    if(!$scripts) {
+    if (!$scripts) {
         carp("No such phase 'post_rpm_install' in OSCAR package API");
         return undef;
     }
     foreach my $scriptname (@$scripts) {
         my $script = $ENV{OSCAR_HOME} . "/packages/" . $pkg . "/scripts/" . 
 	    $scriptname;
-        if(-e $script) {
+        if (-e $script) {
             oscar_log_subsection("About to run $script for $pkg");
             run_in_chroot($dir,$script) or (carp "Script $script failed", 
 					    return undef);
@@ -163,9 +174,13 @@ sub run_in_chroot {
 }
 
 #
-# This returns the type of rpm list for a package file.  If neither
-# rpmlist file exists, return all the RPM names that exist in the RPMS
-# directory (unique by package, of course).
+# This returns the type of rpm list for a package file.  Use this
+# order of precedence in looking for the RPM list:
+#
+# 1. If XML RPM lists for client, server, or all exist, use them.
+# 2. If client.rpmlist or server.rpmlist exists, use them.
+# 3. Otherwise, get a listing of all the RPMs in package/[name]/RPMS,
+#    and use those.
 #
 
 sub rpmlist {
@@ -174,12 +189,52 @@ sub rpmlist {
     my $cfile = "$prefix/client.rpmlist";
     my $sfile = "$prefix/server.rpmlist";
     my $listfile = ($type eq "client") ? $cfile : $sfile;
-    my @rpms = ();
+    my @rpms_to_return = ();
     
-    # If both files do not exist, then return all unique RPM names in
-    # the RPMS directory.
-    
-    if (! -f $cfile && ! -f $sfile) {
+    # If we haven't read in all the package config.xml files, do so.
+
+    read_all_pkg_config_xml() if (!$PACKAGE_CACHE);
+
+    # Double check to ensure that the package's "installable" XML
+    # attribute is 1.  Return an empty RPM list if it's not.
+
+    # Look for XML first
+
+    if ($PACKAGE_CACHE->{$pkg}->{rpmlist}->{server} ||
+	$PACKAGE_CACHE->{$pkg}->{rpmlist}->{client} ||
+	$PACKAGE_CACHE->{$pkg}->{rpmlist}->{all}) {
+	
+	foreach my $i (0 .. 
+		       $#{$PACKAGE_CACHE->{$pkg}->{rpmlist}->{all}->{rpm}}) {
+	    my $rpm = $PACKAGE_CACHE->{$pkg}->{rpmlist}->{all}->{rpm}[$i];
+	    push @rpms_to_return, $rpm;
+	}
+
+	foreach my $i (0 .. 
+		       $#{$PACKAGE_CACHE->{$pkg}->{rpmlist}->{$type}->{rpm}}) {
+	    my $rpm = $PACKAGE_CACHE->{$pkg}->{rpmlist}->{$type}->{rpm}[$i];
+	    push @rpms_to_return, $rpm;
+	}
+    }
+
+    # Next, look for client.rpmlist and/or server.rpmlist
+
+    elsif (-f $cfile || -f $sfile) {
+	if (open(IN,"<$listfile")) {
+	    while(<IN>) {
+		# get rid of comments
+		s/\#.*//;
+		if(/(\S+)/) {
+		    push @rpms_to_return, $1;
+		}
+	    }
+	    close(IN);
+	}
+    } 
+
+    # Otherwise, get a list of files in packages/[name]/RPMS.
+
+    else {
 	if (-d "$prefix/RPMS") {
 	    my @parts;
 	    my %found;
@@ -205,31 +260,21 @@ sub rpmlist {
 		$found{$base} = 1;
 	    }
 	    closedir(PKGDIR);
-	    @rpms = keys(%found);
+	    @rpms_to_return = keys(%found);
 	}
-    }
-    
-    # Otherwise, if either file exists, then read in the desired file
-    # and return it.
-    
-    elsif (open(IN,"<$listfile")) {
-	while(<IN>) {
-	    # get rid of comments
-	    s/\#.*//;
-	    if(/(\S+)/) {
-		push @rpms, $1;
-	    }
-	}
-	close(IN);
     }
 
-    oscar_log_subsection("Returning RPMs for $pkg: " . join(' ', @rpms));
-    return @rpms;
+    # That's all she wrote
+
+    oscar_log_subsection("Returning $type RPMs for $pkg: " . 
+			 join(' ', @rpms_to_return));
+
+    @rpms_to_return;
 }
 
 #
-#  distro_rpmlist - returns the rpms needed for a specific distro on the server
-#                   could be modified for client as well.
+# distro_rpmlist - returns the rpms needed for a specific distro on
+# the server could be modified for client as well.
 #
 
 sub distro_rpmlist {
@@ -237,7 +282,8 @@ sub distro_rpmlist {
     my $listfile = "$distro-$version-$arch.rpmlist";
     my $file = "$ENV{OSCAR_HOME}/share/serverlists/$listfile";
     my @rpms = ();
-    open(IN,"<$file") or carp("Couldn't open package list file $file for reading!");
+    open(IN,"<$file") 
+	or carp("Couldn't open package list file $file for reading!");
     while(<IN>) {
         # get rid of comments
         s/\#.*//;
@@ -250,8 +296,8 @@ sub distro_rpmlist {
 }
 
 #
-#  This is a routine to install the best rpms on the server, only if they don't already
-#  exist at a high enough version
+# This is a routine to install the best rpms on the server, only if
+# they don't already exist at a high enough version
 #
 
 sub install_rpms {
@@ -320,6 +366,76 @@ sub server_version_goodenough {
         # the version in file is better
         return 0;
     }
+}
+
+###########################################################################
+
+# Return an new, empty XML structure with most fields blank.  However,
+# give it a name of the package name, put it in the third-party class,
+# and mark it as installable.
+
+sub make_empty_xml {
+    my ($package_name) = @_;
+
+    return {
+	name => $package_name,
+	version => {
+	    major => 0,
+	    minor => 0,
+	    subversion => 0,
+	    release => 0,
+	    epoch => 0
+	    },
+	class => "third-party",
+	installable => 1,
+	summary => "Not provided",
+	license => "",
+	group => "",
+	url => "",
+	maintainer => {
+	    name => "",
+	    email => ""
+	},
+	packager => {
+	    name => "",
+	    email => ""
+	},
+	description => "Not provided",
+	download => {
+	    uri => [ "" ],
+	    size => 0,
+	    md5sum => ""
+	},
+    };
+}
+
+#
+# Returns a hash of *all* packages' config.xml files (or empty hashes
+# for the ones that don't have config.xml files).
+#
+
+sub read_all_pkg_config_xml {
+    opendir(PKGDIR,"$ENV{OSCAR_HOME}/packages") 
+	or (carp("Couldn't open $ENV{OSCAR_HOME}/packages for reading"), 
+	    return undef);
+
+    while (my $pkg = readdir(PKGDIR)) {
+	chomp($pkg);
+	my $dir = "$ENV{OSCAR_HOME}/packages/$pkg";
+	my $config = "$dir/config.xml";
+	if (-d $dir && $pkg ne "." && $pkg ne ".." && $pkg ne "CVS") {
+	    if (-f $config) {
+		oscar_log_subsection("Reading $config");
+		$PACKAGE_CACHE->{$pkg} = $xs->XMLin($config);
+	    } else {
+		oscar_log_subsection("Got empty XML config for $pkg");
+		$PACKAGE_CACHE->{$pkg} = make_empty_xml($pkg);
+	    }
+	}
+    }
+    close(PKGDIR);
+
+    $PACKAGE_CACHE;
 }
 
 1;
