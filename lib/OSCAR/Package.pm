fa2178edb7d143fe0265587189329809e38a6f5a
ngorsuch <ngorsuch@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-10-30 17:38:58 +0000

    Added forced xml list field "shortcut" for oda database.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@1334 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 331792fb0e3398b658a411e320b331586cae479a [formerly 7acdbd47b5e4421568ae4808626fef3c3bb42415] [formerly 0ed4e7f6329daef87211a71a045cd9d0c2edb6c7 [formerly 51b9775fe8620fb258bf4c04db9cfcb4db576e7d]]
    Former-commit-id: b97f090bdfc48ae4482f2308396bc69ff17e8abb [formerly 2be69106fc497e69a5e47dc32a8e810db5af8423]
    Former-commit-id: ce12716c97e1f4aacd59957c22e6b20dd6e98eaa

--- lib/OSCAR/Package.pm
+++ lib/OSCAR/Package.pm
@@ -5,7 +5,7 @@ package OSCAR::Package;
 # Copyright (c) 2002 The Trustees of Indiana University.  
 #                    All rights reserved.
 # 
-#   $Id: Package.pm,v 1.30 2002/10/29 05:28:02 jsquyres Exp $
+#   $Id: Package.pm,v 1.31 2002/10/30 17:38:58 ngorsuch Exp $
 
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -36,7 +36,7 @@ use Carp;
              run_pkg_script_chroot rpmlist distro_rpmlist install_rpms
              pkg_config_xml list_install_pkg getSelectionHash
              isPackageSelectedForInstallation getConfigurationValues);
-$VERSION = sprintf("%d.%02d", q$Revision: 1.30 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.31 $ =~ /(\d+)\.(\d+)/);
 
 # Trying to figure out the best way to set this.
 
@@ -48,6 +48,7 @@ my $PACKAGE_CACHE = undef;
 my $xs = new XML::Simple(keyattr => {}, forcearray => 
 			 [ "site", "uri", 
 			   "rpm",
+			   "shortcut", 
 			   "requires", "conflicts", "provides" ]);
 
 # The list of phases that are valid for package install.  For more
