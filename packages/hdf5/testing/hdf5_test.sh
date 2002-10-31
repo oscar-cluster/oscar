647e655501df1d59e9d3115d39c3ced4a13e5b31
mchasal <mchasal@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-10-31 00:06:54 +0000

    Initial test
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@1353 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: c6a43c601d60eaab7a91ca473c40c27f14fe1129 [formerly f0c6bba0bff322ce850c4860e069cae9b8083a27] [formerly e004cc44ccfa9be0daf3ce3ab6ec2840029b34c7 [formerly 76526dea75aedbfe0ae1f31983f291ebc69ee776]]
    Former-commit-id: a114dcd8dfaff740246c07320f1968558269a0fb [formerly 6f56b3fbd30a7488c13bdc1f51cd50790f4c1813]
    Former-commit-id: e990b9bbd836b94d1f6ab0a3397958fa9c8542f9

---
+++ packages/hdf5/testing/hdf5_test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+HDF5_TMP=/tmp/hdf5_test
+
+# The HDF5 test itself
+# Test that we can compile two HDF5 example programs and run them.
+
+echo "Running HDF5 test on $HOSTNAME"
+mkdir -p $HDF5_TMP
+cd $HDF5_TMP
+cp /opt/hdf5-oscar-1.4.4-post2/examples/h5_read.c .
+cp /opt/hdf5-oscar-1.4.4-post2/examples/h5_write.c .
+
+( h5cc h5_write.c -o h5_write && \
+h5cc h5_read.c -o h5_read && \
+./h5_write && ./h5_read && \
+echo && echo "HDF5 SUCCESS") || echo "HDF5 FAILED"
+
+# Remove temporary directory
+rm -rf $HDF5_TMP
+
+echo
+echo "HDF5 test complete"
+echo
+
