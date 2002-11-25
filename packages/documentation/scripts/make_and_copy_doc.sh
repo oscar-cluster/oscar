46f06aa56140eee23ba6c546bfbe5900dcfd7108
bligneri <bligneri@18f9d06c-6ae2-0310-8438-938a554feff9>
2002-11-25 04:07:21 +0000

    First "working" version of the HTML documentation generated by latex2html.
    For the moment, the packages/documentation/scripts/make_and_copy_doc.sh script
    generate the html doc and install it in the master node /oscar/ location.
    
    All options for all the documentation generated by latex2html is controlled by the
    LATEX2HTML_OPTION in configure.in
    
    
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@1491 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: d9dfb27340c37e2271dfef1fca4fb940c79d76b7 [formerly 71586dab859de86521dd4119c079682bb464f53f] [formerly 198da6fdd8dd2427e3de38ff68502266e5fe44c4 [formerly a04f0cda902859b1589b93098ac386edcd56d3c2]]
    Former-commit-id: 81548b9e59e7920e5a7f05f1e55e07b95a0fd3eb [formerly 94fe9039b1609e352da9b1dbfcf5325d787a43be]
    Former-commit-id: c75c395e54e903c71e39cf35382a795ff5f8df7e

---
+++ packages/documentation/scripts/make_and_copy_doc.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+SRC="architecture \
+by-laws \
+development \
+installation \
+introduction \
+user";
+
+DOC="architecture/architecture \
+by-laws/by-laws \
+development/development \
+installation/install \
+installation/quick_install \
+introduction/introduction \
+user/user";
+
+# Let's generate the doc
+for dir in $SRC; do \
+    echo "Generating $dir";\
+    pushd ../../../doc/$dir;\
+    make html;\
+    popd;\
+done
+
+# Let's copy the relevant directories
+for dir in $DOC; do \
+    echo "Generating $dir";\
+    cp -rf ../../../doc/$dir ../html;\
+    popd;\
+done
+
+# Delete and copy the html directory in /var/www/html/oscar
+
+rm -rf /var/www/html/oscar
+cp -rf ../html /var/www/html/oscar
