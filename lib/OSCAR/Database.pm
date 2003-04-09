7f72a4c9d2305e02c6061b5a294658f836423b1a
ngorsuch <ngorsuch@18f9d06c-6ae2-0310-8438-938a554feff9>
2003-04-09 16:28:17 +0000

    Added read_table_fields function to lib/OSCAR/Database.pm. Added optional flags to Database.pm functions to block outputting error messages. Documented input/output paramaters for Database.pm functions better.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@1749 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 0d34cc7c69cd2d31ce1cc0b40c12a9f4c6bcbb0b [formerly 5ecbeef7cb9bb72f35df033ed2b7739504ebca12] [formerly 78b6b3e2b05a7d44950ddc20ea9629975ba94079 [formerly 2f2e0b5da88af7a9d24f0788cc32927f4928064a]]
    Former-commit-id: 56165cd4ba7ad7dd082e052c97fc2c4daef02018 [formerly 7ade18474f35438dde5b6fd78e5a3fe429fb6641]
    Former-commit-id: cdd3a2d589c3f0ed2f01a354962685b8bf3e2758

--- lib/OSCAR/Database.pm
+++ lib/OSCAR/Database.pm
@@ -1,22 +1,21 @@
 package OSCAR::Database;
 
-# Copyright 2003 NCSA
-#       Neil Gorsuch <ngorsuch@ncsa.uiuc.edu>
-# 
-
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation; either version 2 of the License, or
 #   (at your option) any later version.
- 
+
 #   This program is distributed in the hope that it will be useful,
 #   but WITHOUT ANY WARRANTY; without even the implied warranty of
 #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #   GNU General Public License for more details.
- 
+
 #   You should have received a copy of the GNU General Public License
 #   along with this program; if not, write to the Free Software
 #   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# Copyright 2003 NCSA
+#                Neil Gorsuch <ngorsuch@ncsa.uiuc.edu>
 
 use strict;
 use vars qw(@EXPORT $VERSION @PKG_SOURCE_LOCATIONS);
@@ -25,9 +24,14 @@ use base qw(Exporter);
 # oda may or may not be installed and initialized
 eval " use oda; ";
 my $oda_available = ! $@;
+my %oda_options = ();
 my $database_available = 0;
 
-@EXPORT = qw( database_connect database_disconnect database_execute_command );
+@EXPORT = qw( database_connect 
+	      database_disconnect 
+	      database_execute_command 
+	      database_read_table_fields 
+	      database_calling_traceback );
 
 #
 # Connect to the oscar database if the oda package has been
@@ -38,8 +42,13 @@ my $database_available = 0;
 # function at the start of your program and leave the database
 # connected throughout the execution of your program.
 #
+# inputs:   no_error_output_flag   if defined and non-zero,
+#                                  don't output error messages
+#
+# outputs:  status                 non-zero if success
 
 sub database_connect {
+    my ( $no_errors_flag ) = @_;
 
     # if the database is already available and connected, success
     return 1 if $database_available;
@@ -55,7 +64,9 @@ sub database_connect {
     return 0 if ! $oda_available;
 
     # try to connect to the database
-    if ( oda::connect( undef, undef ) ) {
+    my @error_strings = ();
+    if ( oda::connect( \%oda_options,
+		       \@error_strings ) ) {
 
 	# then try to execute an oscar shortcut command
 	$database_available = 
@@ -65,7 +76,9 @@ sub database_connect {
         oda::disconnect( undef, undef )
 	    if ! $database_available;
     }
-
+    @error_strings = ()
+	if defined $no_errors_flag && $no_errors_flag;
+    warn shift @error_strings while @error_strings;
     return $database_available;
 }
 
@@ -80,30 +93,202 @@ sub database_disconnect {
     return 1 if ! $database_available;
 
     # disconnect from the database
-    oda::disconnect( undef, undef );
+    oda::disconnect( \%oda_options, undef );
     $database_available = 0;
 
     return 1;
 }
 
 #
-# call oda::execute command to execute a database command or shortcut,
-# this is only needed to avoid having calling code have to do the
-# conditional "use OSCAR::oda" in case they are executing at a point
-# when the oda has not been installed yet
+# This function executes an oda database command, parsing
+# the command from one or more string arguments, expanding
+# any database shortcuts if needed. It calls oda::execute_command
+# to execute a database command or shortcut, this is only needed
+# to avoid having calling code have to do the conditional 
+# "use OSCAR::oda" in case they are executing at a point when
+# the oda has not been installed yet.
 #
+# inputs: command_args  either a single scalar string that
+#                       includes the command/shortcut and any
+#                       arguments, or a reference to a list
+#                       strings that include the command/shortcut
+#                       and any arguments.
+#         results_ref   reference to a variable for the results,
+#                       commands that return one or more strings
+#                       will place a reference to the list of 
+#                       result strings in results_ref, commands
+#                       that do not return any result strings 
+#                       will place the integer number of records 
+#                       affected or modified in results_ref
+#        no_errors_flag if defined and non-zero, don't output 
+#                       any error messages
+#
+# outputs: status       non-zero for success, note that success
+#                       just means that there are no database
+#                       query modification errors, the intended 
+#                       result could still be in error without
+#                       any database errors, the results pointed
+#                       to by the results_ref variable should
+#                       be checked for values, or for the correct
+#                       number of records being affected
 
 sub database_execute_command {
 
-    my ( $options_ref,
-         $command_args_ref,
+    my ( $command_args_ref,
          $results_ref,
-         $error_strings_ref ) = @_;
+	 $no_errors_flag ) = @_;
 
-    return oda::execute_command( $options_ref,
-				 $command_args_ref,
-				 $results_ref,
-				 $error_strings_ref );
+    my @error_strings = ();
+    my $success = oda::execute_command( \%oda_options,
+					$command_args_ref,
+					$results_ref,
+					\@error_strings );
+    @error_strings = ()
+	if defined $no_errors_flag && $no_errors_flag;
+    warn shift @error_strings while @error_strings;
+    return $success;
 }
+
+#
+# reads specified fields from all the records in a specified database
+# table into a double deep hash with the first level of keys being
+# taken fromt the "name" field for each record, with data in the first
+# level of keys being pointers to hashes one per database table record,
+# with the second level hashes being the requested fields with their
+# data being the values in the database fields.
+#
+# paramaters are:   table     database table name
+#                   fields    pointer to requested field nanes list
+#                             (if undef or empty returns all fields)
+#                   wheres    pointer to where expressions
+#                             (if undef all records returned)
+
+sub read_table_fields {
+    
+    my ( $table,
+	 $requested_fields_ref,
+	 $wheres_ref ) = @_;
+
+    # since we are going to do a number of database operations, we'll
+    # try to be more effecient by connecting to the database first if
+    # we weren't connected when called, then perform the operations, 
+    # then disconnect if we weren't connected when we were called.
+
+    ( my $was_connected_flag = $database_available ) ||
+	OSCAR::Database::database_connect() ||
+	    return undef;
+
+    # get a list of field names for this database table
+
+    my %fields_in_table = ();
+    my @error_strings = ();
+    if ( ! oda::list_fields( \%oda_options,
+			     $table,
+			     \%fields_in_table,
+			     \@error_strings ) ) {
+	warn shift @error_strings while @error_strings;
+	warn "cannot read the field names for database table <$table> from the ODA database";
+	OSCAR::Database::database_disconnect() if $was_connected_flag;
+	return undef;
+    }
+
+    # if there isn't a "name" field in this database table, we
+    # have a serious problem
+
+    if ( ! exists $fields_in_table{name} ) {
+	warn "there is no <name> field in database table <$table>";
+	warn "Database\:\:read_table_fields cannot supply the data as requested";
+	OSCAR::Database::database_disconnect() if $was_connected_flag;
+	return undef;
+    }
+	
+    # if the caller supplied an undef or empty fields list,
+    # we'll supply all fields for this database. Also, make
+    # sure that the field <name> is included.
+
+    my @fields = ();
+    if ( defined($requested_fields_ref) && @$requested_fields_ref ) {
+	@fields = sort keys %fields_in_table;
+    } else {
+	@fields = @$requested_fields_ref;
+	push @fields, "name"
+	    if ! grep( /^name$/, @fields );
+    }
+
+    # now read all the records from the packages database table,
+    # this will return an array of pointers to hashes, one hash
+    # for each package record with the keys being the field names
+    # and the data being the field contents
+
+    my @table_fields = ( $table );
+    foreach my $field ( @fields ) {
+	push @table_fields, "$table.$field";
+    }
+    my @records = ();
+    if ( ! oda::read_records( \%oda_options,
+			      \@table_fields,
+			      $wheres_ref,,
+			      \@records,
+			      1,
+			      \@error_strings ) ) {
+	warn shift @error_strings while @error_strings;
+	OSCAR::Database::database_disconnect() if $was_connected_flag;
+	return undef;
+    }
+
+    # convert the array of hash pointers that read_records returned
+    # into the hash of hashes format that the callers expect
+
+    my %result = ();
+    foreach my $record_ref ( @records ) {
+	my %duplicated_name_values = ();
+	my $missing_name_fields = 0;
+	if ( exists $$record_ref{name} && $$record_ref{name} ne "" ) {
+	    if ( exists $$record_ref{name} ) {
+		$duplicated_name_values{$$record_ref{name}} = 1;
+	    } else {
+		delete $$record_ref{name};
+		$result{$$record_ref{name}} = $record_ref;
+	    }
+	} else {
+	    $missing_name_fields++;
+	}
+	foreach my $name ( sort keys %duplicated_name_values ) {
+	    warn "There are duplicated records in database table <$table> that";
+	    warn "has the same <name> field value of <$name>.";
+	}
+	warn "Database\:\:read_table_fields will only return the first of each."
+	    if %duplicated_name_values;
+	if ( $missing_name_fields ) {
+	    warn "$missing_name_fields records from the database table <$table> are missing the <name>";
+	    warn "field and are not being returned by Database\:\:read_table_fields.";
+	}
+    }
+
+    # if we weren't connected to the database when called, disconnect
+
+    OSCAR::Database::database_disconnect() if $was_connected_flag;
+
+oda::print_hash("", "Database\:\:read_table_fields returning", \%result);
+    return \%result;
+}
+
+#
+# prints a traceback of the call stack
+#
+
+sub calling_traceback {
+    my $level = 1;
+    my ( $package, $filename, $line, $subroutine, $hasargs, 
+    $wantarray, $evaltext, $is_require, $hints, $bitmask );
+    do {
+	( $package, $filename, $line, $subroutine, $hasargs, $wantarray,
+	  $evaltext, $is_require, $hints, $bitmask ) = caller ( $level );
+	print "called from $filename\:$line $subroutine\(\)\n"
+	    if defined $filename;
+	$level++;;
+    } while defined $filename;
+}
+
 
 1;
