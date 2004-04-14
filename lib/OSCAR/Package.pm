65acf247b684fadeac4cc23435f8b881bf6c3b0d
tuelusr <tuelusr@18f9d06c-6ae2-0310-8438-938a554feff9>
2004-04-14 05:05:39 +0000

    DepMan::query_required_by, not DepMan::required_by. I'm forgetting my own API now.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@2454 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 82fa0aa609bf91e52cb58225a0bf2b7fa933b579 [formerly 839f00e1590b4a142394e52fd001c6fcf5bb343b] [formerly 3c7c1af86acc4b834077c49821f768298d397b99 [formerly a61e28ae3bcf913daca2b352a09304a2de898b6f]]
    Former-commit-id: 8c544cdd9a67bf0e1a10190ecf9d42bb01bb7aaa [formerly 18f20490c71781a272982261e642ca8cd1f051bb]
    Former-commit-id: 17226445e9700648ce69bd04974c45d942197e4e

--- lib/OSCAR/Package.pm
+++ lib/OSCAR/Package.pm
@@ -7,7 +7,7 @@ package OSCAR::Package;
 # Copyright (c) 2002-2004 The Trustees of Indiana University.  
 #                         All rights reserved.
 # 
-#   $Id: Package.pm,v 1.62 2004/04/14 01:53:04 tuelusr Exp $
+#   $Id: Package.pm,v 1.63 2004/04/14 05:05:39 tuelusr Exp $
 
 #   This program is free software; you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -44,7 +44,7 @@ use Carp;
              run_pkg_script_chroot rpmlist install_packages copy_pkgs 
              pkg_config_xml list_selected_packages getSelectionHash
              isPackageSelectedForInstallation getConfigurationValues);
-$VERSION = sprintf("%d.%02d", q$Revision: 1.62 $ =~ /(\d+)\.(\d+)/);
+$VERSION = sprintf("%d.%02d", q$Revision: 1.63 $ =~ /(\d+)\.(\d+)/);
 
 # Trying to figure out the best way to set this.
 my ($distro_name, $distro_version) = which_distro_server();
@@ -431,8 +431,8 @@ sub install_packages {
     my $pm = PackMan->new;
     my $dm = DepMan->new;
 
-    if (! $pm->install ($dm->required_by (@_))) {
-        carp('$pm->install ($dm->required_by ()) failed');
+    if (! $pm->install ($dm->query_required_by (@_))) {
+        carp('$pm->install ($dm->query_required_by ()) failed');
         return 0;
     }
 
