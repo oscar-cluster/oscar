291bdd4712c3c63e76f3101871f3305d0edc22cf
jsquyres <jsquyres@18f9d06c-6ae2-0310-8438-938a554feff9>
2005-05-18 11:33:24 +0000

    Add component infrastructure to trunk, plus a first component for the OS_Detect framework (for OS X, because that's what we were testing on).  This framework still needs to be *used* throughout the OSCAR code base -- hence, this is currently unreferenced code and is safe to put on the trunk.
    To-do:
    - write plugins for all the OS/distros that we support
    - integrate the use of $OS_Detect->{query}() throughout the tree
    
    
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@3174 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 52d38fb3446616f9d91e79d09335b0f609a3bdbc [formerly 88a83c9defe006722a8c16c5c8ba76ec00ce85d1] [formerly 6c41c83e0664aea71cf8411c307335e26920c4cd [formerly 6515db6465e4ab8699ba72e663caedf6af0a5048]]
    Former-commit-id: 0e5846a99a8365d82daa4a26f8b3523d3b0430a2 [formerly 4c13b0119bea52c29b4791e0babb3144a518fcce]
    Former-commit-id: 39b4be97c1024421e331551e991ae0de578a52a4

---
+++ lib/OSCAR/OCA.pm
@@ -0,0 +1,84 @@
+#
+# Copyright (c) 2005 The Trustees of Indiana University.  
+#                    All rights reserved.
+# 
+# This file is part of the OSCAR software package.  For license
+# information, see the COPYING file in the top level directory of the
+# OSCAR source distribution.
+#
+# $HEADER$
+#
+
+package OSCAR::OCA;
+
+use strict;
+
+#
+# Subroutine to find components in a given framework.
+#
+sub find_components {
+    my ($framework, $basedir) = @_;
+
+    # If basedir is not provided, use the default
+
+    if (! $basedir) {
+        if ($ENV{OSCAR_HOME}) {
+            $basedir = $ENV{OSCAR_HOME};
+        } else {
+            $basedir = "/opt/oscar";
+        }
+    }
+
+    # Append on the framework subdirectory that we're looking in
+
+    $basedir .= "/lib/OSCAR/OCA/$framework";
+
+    # Return on bozo error
+
+    return undef if (! -d $basedir);
+
+    # Scan the framework directory for .pm and .pmc files of the form:
+    # $basedir/$comp.pm[c].
+
+    opendir(DIR, $basedir);
+    my @comps = 
+        grep { ( /\.pm$/ || /\.pmc$/) && -f "$basedir/$_" } readdir(DIR);
+    closedir(DIR);
+
+    # We need to augment @INC here so that we can do a simple
+    # "require", so save the current @INC and then augment it.
+
+    my @INC_save = @INC;
+    push(@INC, "$basedir");
+
+    # Try opening all the .pm[c] files that we found above
+
+    my @opened;
+    foreach my $comp (@comps) {
+
+        # Do a little trickiness: remove any trailing .pm or .pmc so
+        # that when "require" rolls around, all we do is "require
+        # <component_name>".
+
+        $comp =~ s/.pm[c]{0,1}$//;
+
+        # Do the require inside an eval so that if it fails, we won't
+        # get a nasty error and/or abort.  The success status of the
+        # eval'ed require will be in $@.
+
+        eval "require $comp";
+        if (! $@) {
+            push(@opened, $comp);
+        }
+    }
+
+    # Restore @INC
+
+    @INC = @INC_save;
+
+    # Return the names of the opened components
+
+    \@opened;
+}
+
+1;
---
+++ lib/OSCAR/OCA/OS_Detect.pm
@@ -0,0 +1,59 @@
+#
+# Copyright (c) 2005 The Trustees of Indiana University.  
+#                    All rights reserved.
+# 
+# This file is part of the OSCAR software package.  For license
+# information, see the COPYING file in the top level directory of the
+# OSCAR source distribution.
+#
+# $HEADER$
+#
+
+package OSCAR::OCA::OS_Detect;
+
+use strict;
+use vars qw(@EXPORT);
+use base qw(Exporter);
+use OSCAR::OCA;
+
+#
+# Exports
+#
+
+@EXPORT = qw($OS_Detect);
+
+#
+# Globals
+#
+
+our $OS_Detect;
+
+#
+# Subroutine to open the OS_Detect framework
+#
+
+sub open {
+    my $comps = OSCAR::OCA::find_components("OS_Detect");
+
+    # Did we find one and only one?
+
+    if (!$comps) {
+        print "Could not find an os-detect component for this system!\n";
+        die "Cannot continue";
+    } elsif (scalar(@$comps) > 1) {
+        print "Found more than one os-detect component for this system!\n";
+        die" Cannot continue";
+    }
+
+    # Yes, we only found one.  Suck its function pointers into the
+    # global $OS_Detect hash reference.
+
+    my $str = "\$OS_Detect->{query} = \\&OCA::OS_Detect::@$comps[0]::query";
+    eval $str;
+
+    # Happiness -- tell the caller that all went well.
+
+    1;
+}
+
+1;
