ab17467a986b9219debdc4dd837e84e5d702df59
efocht <efocht@18f9d06c-6ae2-0310-8438-938a554feff9>
2005-09-06 15:08:31 +0000

    Fix for bug #1282422. Added filter for selected packages, such that only their switcher tags can be chosen. Rewrote ODA access using the OSCAR::Database routines.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@3682 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: d8f859bb730896bb4fbafda460929f55d095ccec [formerly 5c60a13fccf53b8cdecccea9a1df4a781d51a428] [formerly c8f40337951fa6ae07cfeaca79089f9b79ba762a [formerly 5401e2bfc00f6a97d3182e0ba20c7552516903ef]] [formerly a0ed87cd40a35de9f2baa86610e780ac4436800d [formerly 729ca28d36952a51e091b44df3b742cb104e7713] [formerly ff706fca6e35e8aff282a508f941f4c70075c74f [formerly d0a290d9c17d4b031d2fb47a8c628c8718b0c6f3]]]
    Former-commit-id: ecafb25a7cededa93622736c2efe4e4c893c3659 [formerly 664ddace6d4a65c7bc58c1a9e3af37ca705b3a6d] [formerly 4e51a449c2bae417e0c7b88641a844726f18effb [formerly 38584b14287221de2f2ec5ff8099f7dc0774a09e]]
    Former-commit-id: 68a9704c53b2a888ceb1017b70a18a77ca07d400 [formerly d5cff1da448244c0974d4757493b5b6335ca50c2]
    Former-commit-id: 16edac3b95f205e1d4fb71274be73e72ee925467

--- packages/switcher/scripts/package_config.pm
+++ packages/switcher/scripts/package_config.pm
@@ -11,43 +11,38 @@ package scripts::package_config;
 #
 
 use strict;
+use lib "$ENV{OSCAR_HOME}/lib";
 use vars qw(@EXPORT $VERSION);
 use base qw(Exporter);
 use Data::Dumper;
+use OSCAR::Database;
 
 @EXPORT = qw(get);
 $VERSION = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
 
-
-#
-# This will be re-written when Neil introduces the ODA interface where
-# I can search for this information in the database.  For the moment,
-# I'm going to hard-code it...
-#
 sub get {
 
     # Query ODA to get the <switcher> blocks from other packages
-
-    open(ODA, "oda switcher_list_packages_tags_names|") || 
-	die("packages/switcher/scripts/package_config.pm could not run ODA");
     my $result = undef;
-    while (<ODA>) {
-	chomp($_);
-	my ($package, $tag, $name) = split(/ /, $_);
-	$result->{$package} = {
-	    tag => $tag,
-	    name => $name,
-	};
+    my @sel_pkgs = database_return_list("packages_in_selected_package_set",1);
+    if (!defined(@sel_pkgs)) {
+	die("packages/switcher/scripts/package_config.pm failed to access the OSCAR database!");
     }
-    close(ODA);
+    my @tmp = database_return_list("switcher_list_packages_tags_names",1);
+    if (!@tmp) {
+	print "packages/switcher/scripts/package_config.pm: switcher tag list is empty";
+    }
+    while (@tmp) {
+	my ($package, $tag, $name) = splice(@tmp, 0, 3);
+	next if (!grep(/^$package$/,@sel_pkgs));
+	$result->{$package} = { tag => $tag, name => $name, };
+    }
 
     # Traverse the data returned and construct a data mapping tags to
     # names/packages.
-
     my %tags;
     foreach my $k (sort(keys(%$result))) {
 	my $tag = $result->{$k}->{tag};
-	
 	if (defined($tags{$tag})) {
 	    ++$tags{$tag}->{count};
 	} else {
@@ -64,7 +59,6 @@ sub get {
     }
     
     # Return a reference to the hash
-
     \%tags;
 }
 
