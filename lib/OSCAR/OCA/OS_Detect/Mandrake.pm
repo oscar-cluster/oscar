551bc02e529db4747dccab6858eb3ef9de90c7df
laudares <laudares@18f9d06c-6ae2-0310-8438-938a554feff9>
2005-10-28 20:52:50 +0000

    Adding suppor for Mandriva and Mandrake distros in the new OS_Detect system
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@3878 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: c6c57af12fafd60f5a11fa6915a4c59a78e81855 [formerly 5165a3f9c0c596574af1598812fd47c073cd795a] [formerly f4421c2eae89a951092c0409679e46e02bdd7ffb [formerly e3ff54d3b1fa7160934d6e32687fb18dc0ab21d3]] [formerly 1762ed289a002d625e2b15071c6324416fbe215c [formerly ac5efeedc313c2b5e0b075d902a2df2c5d9dba4e] [formerly f6a2d60473b0023e48269898c6fdc12b1e2c1c07 [formerly 147e1365fab054a419685d5ac380eca05905d4f7]]]
    Former-commit-id: 242298cbc8e8730ed5707abbdf5276d14e29f44c [formerly 5d22b85b5d1cf0063381aff1c7a6024757d5b1aa] [formerly d9d64531670345c5743a1f52440fe38f36166232 [formerly 218d5133c80e7cf35b103abe169380930183c8b2]]
    Former-commit-id: 14b0eac73da39a1df901febe7f935f091f44df32 [formerly 8d5b9e6ce12e1be100457277ccaa5cdb3eba90b5]
    Former-commit-id: 5c6a8d40e1c7c54d932b552ea71273c8e25cee9e

---
+++ lib/OSCAR/OCA/OS_Detect/Mandrake.pm
@@ -0,0 +1,76 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) 2005 The Trustees of Indiana University.  
+#                    All rights reserved.
+# Copyright (c) 2005 Bernard Li <bli@bcgsc.ca>
+#                    All rights reserved.
+# 
+# This file is part of the OSCAR software package.  For license
+# information, see the COPYING file in the top level directory of the
+# OSCAR source distribution.
+#
+# $HEADER$
+#
+
+package OCA::OS_Detect::Mandrake;
+
+use strict;
+use POSIX;
+use Config;
+
+# This is the logic that determines whether this component can be
+# loaded or not -- i.e., whether we're on a Mandrake machine or not.
+
+# Check uname
+
+my ($sysname, $nodename, $release, $version, $machine) = POSIX::uname();
+
+# We only support Linux -- if we're not Linux, then quit
+
+return 0 if ("Linux" ne $sysname);
+
+my $mandrake_release;
+
+# If /etc/mandrake-release exists, continue, otherwise, quit.
+
+if (-e "/etc/mandrake-release")  {
+	$mandrake_release = `cat /etc/mandrake-release`;
+} else {
+	return 0;
+}
+
+# We only support Mandrake 10.0 and 10.1 -- otherwise quit.
+
+if ($mandrake_release =~ '10.0') {
+	$mandrake_release = 10.0;
+} elsif ($mandrake_release =~ '10.1') {
+	$mandrake_release = 10.1;
+} else {
+	return 0;
+}
+
+# First set of data
+
+our $id = {
+    os => "linux",
+    arch => $machine,
+    os_release => $release,
+    linux_distro => "mandrake",
+    linux_distro_version => $mandrake_release
+};
+
+# Make final string
+
+$id->{ident} = "$id->{os}-$id->{arch}-$id->{os_release}-$id->{linux_distro}-$id->{linux_distro_version}";
+
+# Once all this has been setup, whenever someone invokes the "query"
+# method on this component, we just return the pre-setup data.
+
+sub query {
+    our $id;
+    return $id;
+}
+
+# If we got here, we're happy
+
+1;
