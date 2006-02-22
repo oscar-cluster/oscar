258132ba500aa3f3df600d03f2e5cce8296a3b40
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-02-22 19:15:48 +0000

    Add a debugging mechanism for OCA
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@4413 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 2e5dfb224ed879d712b9cface7a50a1045b11567 [formerly b4dca8d70fdddb0ccb4ae40d0ea6e2b2c2351dd6] [formerly 61b1d0980fcdf174e7b64c90bccaf074027345b1 [formerly e8a9c16d9f42a7f7aa841142021b9c912e101213]] [formerly 8c08ff470fd04883c551db45c935dfa8d56b05ce [formerly 521821453464af73a107acd0e15f099c59a31029] [formerly 5ef634990656db1f55d2cb2f84a03170ad6dfc8a [formerly 7c2c98c91120c9b8fa0a016dca1095d8683919e8]]]
    Former-commit-id: d677a15f47ef2b8cb97f344fe4619694a849ebf6 [formerly a567760399f0852fcee8052e49a53cb99512980f] [formerly 54a9ad299d877fb628779d3870ca8d669b86ee00 [formerly 33b556ac2f90ed7b3ef9ced431673fa5536b1347]]
    Former-commit-id: 32b51c99790e8997504592bb265b336ef3119c7f [formerly 432e482abeeffda65691190abadacd5020b98409]
    Former-commit-id: 7598f2f7378bd2bf3528b4e705ee65a876c5b5b9

---
+++ lib/OSCAR/OCA/Debugger.pm
@@ -0,0 +1,44 @@
+package OSCAR::OCA::Debugger;
+
+#
+# Copyright (c) 2006 Oak Ridge National Laboratory 
+#                    Geoffroy Vallee <valleegr.ornl.gov>
+#                    All rights reserved.
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+
+use strict;
+use OSCAR::Logger;
+use vars qw(@EXPORT);
+use base qw(Exporter);
+
+@EXPORT = qw(oca_debug_section oca_debug_subsection);
+
+
+sub oca_debug_section {
+  my $debug_text = shift;
+  if ( $ENV{OCA_DEBUG} eq "y" ) {
+    oscar_log_section ($debug_text);
+  }
+}
+
+
+sub oca_debug_subsection {
+  my $debug_text = shift;
+  if ( $ENV{OCA_DEBUG} eq "y" ) {
+    oscar_log_subsection ($debug_text);
+  }
+}
