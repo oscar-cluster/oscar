433f179785e48f7227e1bf0ca05b64957b10df4b
naughtont <naughtont@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-04-19 21:52:15 +0000

    + updated this to call the frameworks that we have in OCA,   some are not complete or appear to do little to nothing but...
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@4611 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: a57103bc6faaf37cbb92e4232076b62a427e539f [formerly a7d636365c3b0a45604b63c24302dd423a760c6f] [formerly c2422ce64438222ad5b3f319629ef55ebcd8bf82 [formerly 6c9d23f000288dce92dd75466861a4d3f1e1d311]] [formerly 0b42194bbad18634d501aecabcb3ffcb48a78263 [formerly d518a66764ebf3cf72543b620b5dac9d5aa0ff5b] [formerly e43c0ff963ab47d105292d4f79d737f06fab1929 [formerly 8c5b4c8f22136de4a2f8167248406a9bc039d505]]]
    Former-commit-id: e9475e3d3043f354b53ff5392c0960d0e20ee42c [formerly 2f961161bcedffff94cedad510e9a0d296ae5415] [formerly 74679dc8a1631596f821b8546902e1d8ec043392 [formerly ea98d5f442cbed540b16d546472786e99b43cda1]]
    Former-commit-id: 36852493caf1433536e3c3c9aadcc9a8ba4877fb [formerly b6c4a15c0514db91ce4249303a67685ac7b5aece]
    Former-commit-id: 91ade7e29b4ffd005ce40e8f24538311f084d957

--- scripts/OCA-driver.pl
+++ scripts/OCA-driver.pl
@@ -1,17 +1,98 @@
 #!/usr/bin/perl
 #
-# Dummy driver program for OSCAR Component Architecture (OCA).
+# Dummy driver program
 #
-# Related EnvVars:
+# Relevant EnvVars:
 #   OSCAR_HOME
 #   DEBUG_OCA_OS_DETECT
-
+#
 
 use lib "$ENV{OSCAR_HOME}/lib";
-use OSCAR::OCA::OS_Detect;
+use OSCAR::Logger;
 use Data::Dumper;
+use Carp;
 
-die "Cannot continue\n" if( ! OSCAR::OCA::OS_Detect::open() );
 
-my $ident = $OS_Detect->{query}();
-print Dumper($ident);
+ ############################################################
+ #
+ # (Check#1)
+ # Framework: OS_Detect 
+ #
+ ############################################################
+oscar_log_section("OS_Detect");
+
+use OSCAR::OCA::OS_Detect;
+my $os = OSCAR::OCA::OS_Detect::open();
+
+if( not defined($os) ) {
+	carp "Error: OS_Detect open failed."; 
+} else {
+	oscar_log_subsection( " Dump...\n" . Dumper($os) );
+	oscar_log_subsection( "distro_flavor=(" . $os->{distro_flavor} . ")" );
+	oscar_log_subsection( "distro_version=(" . $os->{distro_version} . ")" );
+	oscar_log_subsection( "arch=(" . $os->{arch} . ")" );
+}
+
+
+ ############################################################
+ #
+ # (Check#2)
+ # Framework: RM_Detect 
+ #
+ ############################################################
+oscar_log_section("RM_Detect");
+
+use OSCAR::OCA::RM_Detect;
+my $rmgr = OSCAR::OCA::RM_Detect::open();
+
+if( not defined($rmgr) ) {
+	carp "Error: RM_Detect open failed."; 
+} else {
+	oscar_log_subsection( " Dump...\n" . Dumper($rmgr) );
+}
+
+
+#
+# TJN: (4/19/06) The Sanity_Check framework is not complete
+#    as it exists in SVN so I'm commenting this section for now.
+#
+#  ############################################################
+#  #
+#  # (Check#3)
+#  # Framework: Sanity_Check
+#  #
+#  ############################################################
+# oscar_log_section("Sanity_Check");
+#
+# use OSCAR::OCA::Sanity_Check;
+# my $sanity = OSCAR::OCA::Sanity_Check::open();
+# 
+# if( not defined($sanity) ) {
+# 	"Error: Sanity_Check open failed.";
+# } else {
+# 	oscar_log_subsection( " Dump...\n" . Dumper($sanity) );
+# }
+# 
+
+# 
+# TJN: (4/19/06) Until I commit the Cluster_Ops framework, I'm
+#    going to comment this section out too.
+#
+#  ############################################################
+#  #
+#  # (Check#4)
+#  # Framework: Cluster_Ops
+#  #
+#  ############################################################
+# oscar_log_section("Cluster_Ops");
+# 
+# use OSCAR::OCA::Cluster_Ops;
+# my $cops = OSCAR::OCA::Cluster_Ops::open();
+# 
+# if( not defined($cops) ) {
+# 	carp "Error: Cluster_Ops open failed."; 
+# } else {
+# 	oscar_log_subsection( " Dump...\n" . Dumper($cops) );
+# }
+# 
+# 
