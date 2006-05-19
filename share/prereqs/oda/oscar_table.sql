3955bac5ed1ad0bdde87c0557348a8a0529e97d9
dikim <dikim@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-05-19 01:02:33 +0000

    Oops, I accidentally deleted the copyrights and some comments. Sorry. :-(
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@4792 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: eee318253540f15a2d79751af33d544e4cfd047c [formerly 5560e5a9e3bd2783215eee4ed485810d188a67af] [formerly 901cc98a5f45f3d267b489aee5ed6a306b0b5a50 [formerly 15e8411b380defd25e068053dba1fd0fad093f70]] [formerly f1dfdf326ed61bc52d267d645398798d10f78dc1 [formerly 5a6f593455d59bd9af59f7be53940c04264d7e0a] [formerly c7fe0087420778c51dcc83fef50079e6e8dabf4f [formerly d5d59b98a51d734973619a02a4dd65574fca2698]]]
    Former-commit-id: 2fc9c6925344503cf9f3aa5ae893aa94392e7b79 [formerly f267b7175cfb7392c965f650c1fd871a7eefcf67] [formerly d27326470ff5ed3381c625c3902a73802a9b7b77 [formerly cb1a34a40a5d21d217ef523617808f355ec93598]]
    Former-commit-id: 77e2d8c50bbd012bcb622dacc1c6de2cdc0e6c7c [formerly 7ce1e35fbad0488d4aab5a04ccbf86192d3506a1]
    Former-commit-id: c27c4f453d6cd242ed2cff603ce8f0a2f1f0b280

--- share/prereqs/oda/oscar_table.sql
+++ share/prereqs/oda/oscar_table.sql
@@ -1,4 +1,32 @@
 -- Clusters
+--  This program is free software; you can redistribute it and/or modify
+--  it under the terms of the GNU General Public License as published by
+--  the Free Software Foundation; either version 2 of the License, or
+--  (at your option) any later version.
+
+--  This program is distributed in the hope that it will be useful,
+--  but WITHOUT ANY WARRANTY; without even the implied warranty of
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--  GNU General Public License for more details.
+
+--  You should have received a copy of the GNU General Public License
+--  along with this program; if not, write to the Free Software
+--  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+--  USA
+
+--
+-- Copyright (c) 2006 The Trustees of Indiana University.
+--                    All rights reserved.
+--
+-- $Id$
+--
+
+--
+-- All the OSCAR database tables are defined here and any changes on
+-- this file will directly affect the OSCAR database implementation.
+--
+
+-- Clusters
 CREATE TABLE IF NOT EXISTS Clusters(
     headnode_interface VARCHAR(100),
     id  integer   auto_increment not null unique,
