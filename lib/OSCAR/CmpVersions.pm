29d0041a6e3efa9a600ff7e201d721501188ea8b
efocht <efocht@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-06-02 11:37:24 +0000

    Moved version comparison routines to a library. This is good because it will be used in several places.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@4951 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: a4bb1b9f6e75504f8076cf1174e366e07febbce0 [formerly b1b5dc114e5fc2f61f4adbc41ed58881264f152a] [formerly 12eebc45a2bd006ceec5502eecad69ea91a66795 [formerly 315bbbc0b72219cda3632db223f33df4cdf48b41]] [formerly 0e39f08531f9aa3ac5026a856fcab54f83fedfc0 [formerly acb9d95158e28749fb6fc91c1e0c7def6c5a04d1] [formerly 1bc0b8bd02bdfc4c4cc2f887af5f9440d4b2eb6c [formerly 676f9540d2a54180e7c6ab773cd5b24f275833ad]]]
    Former-commit-id: d4b9a15481ac6e1bcf44ebe297db92b383695aef [formerly 456515d046ba5bab57f1967d2cf2c9dea01ada23] [formerly 5922245cd4d32f4851070b8a882589c51e0ba474 [formerly 6d7e0613f56fc8c1b4aa1049d63e42a9518ab4bc]]
    Former-commit-id: 91ece9b81bbf8666fdbfbcc793568a2bce608a7f [formerly c8fe61929dfaf2fe868f002fc727f72515d0c192]
    Former-commit-id: 68ae865637b8ac2e101080b839881195bbf8d56c

---
+++ lib/OSCAR/CmpVersions.pm
@@ -0,0 +1,116 @@
+package OSCAR::CmpVersions;
+#
+# Copyright (c) 2006, Erich Focht <efocht@hpce.nec.com>
+#                     All rights reserved.
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#
+# Generic routines for comparing version strings. Used in multiple places
+# inside OSCAR.
+#
+# Comparison routines taken from David Lombard's update-rpms package.
+# (C)opyright by David Lombard, distributed under GPL 2.
+#
+# $Id$
+
+use strict;
+use vars qw($verbose @EXPORT);
+use POSIX;
+use base qw(Exporter);
+
+@EXPORT = qw(cmp_version_strings cmp_version_strings_num);
+
+#
+# Compare two version strings.
+#
+sub cmp_version_strings($$) {
+    my ($a, $b) = @_;
+
+    return 0 unless defined $a && defined $b && $a ne "" && $b ne "";
+    #
+    #       Split a & b into runs of numeric and non-numeric strings.
+    #         2.4.0 => "2.4.0"
+    #         2.0p15 => "2.0", "p", "15"
+    #
+    #       EF:
+    #         22.EL  vs. 22.0.1.EF fails
+    #
+    my @a;
+    # ($a =~ /([\d\.]+)([^\d\.]*)/gc) ## FAILS FOR SOME CIRCUMSTANCES
+    push @a, $1, ($2||"") while ($a =~ /([\d\.]+)([^0-9\.]*)/gc);
+    my @aa = @a;
+
+    my @b;
+    # ($b =~ /([\d\.]+)([^\d\.]*)/gc) ## FAILS FOR SOME CIRCUMSTANCES
+    push @b, $1, ($2||"") while ($b =~ /([\d\.]+)([^0-9\.]*)/gc);
+    my @bb = @b;
+    #
+    #       Compare the strings.
+    #
+    my $ans;
+    while (@a && @b) {
+	last if $ans = cmp_version_strings_num( shift( @a ), shift( @b ) );
+	last if $ans = shift( @a ) cmp shift( @b );
+    }
+    unless( $ans ) {
+	my $ta = shift( @a ) || -1;
+	my $tb = shift( @b ) || -1;
+	$ans = cmp_version_strings_num( $ta, $tb );
+    }
+    if( $verbose > 7 ) {
+	my $word = $ans < 0 ? "<" : $ans ? ">" : "==";
+	print join( " ", @aa ), " $word ", join( " ", @bb ), "\n";
+    }
+    return $ans;
+}
+
+#
+# Compare two version numbers.
+#
+sub cmp_version_strings_num($$) {
+    my ($a, $b) = @_;
+
+    # EF: version problems when trailing dot in version number
+    # like "2.0.1." compared to "2.0."
+    # If both strings end with a dot, strip it off
+    if (($a =~ /\d\.$/) && ($b =~ /\d\.$/)) {
+	$a =~ s/\.$//;
+	$b =~ s/\.$//;
+    }
+    #
+    #       Split a & b into runs of numbers.
+    #         21.42.08 => "21", "42", "08"
+    #         2 => "2"
+    #
+    my @a;
+    push @a, $1, ($2||"") while ($a =~ /(\d+)(\.*)/gc);
+
+    my @b;
+    push @b, $1, ($2||"") while ($b =~ /(\d+)(\.*)/gc);
+    #
+    #       Compare the runs.
+    #
+    my $ans;
+    while (@a && @b) {
+	last if $ans = shift( @a ) <=> shift( @b );
+	last if $ans = shift( @a ) cmp shift( @b );
+    }
+    unless( $ans ) {
+	my $ta = shift( @a ) || -1;
+	my $tb = shift( @b ) || -1;
+	$ans = $ta <=> $tb;
+    }
+    return $ans;
+}
