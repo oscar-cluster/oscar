972f38ec3ee6f2763a4b37c2582bc37a0c88c7f5
bli <bli@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-07-04 07:19:28 +0000

    For "Manage Mode", moved "Monitor Cluster Deployment" button to the "Add OSCAR Clients" submenu.  I also changed the behaviour slightly such that if the "Add OSCAR Clients" widget or the main OSCAR Wizard (in "Install Mode") is closed while si_monitortk is still running it will be killed.  This closes #187.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@5097 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: e4b47c22cdbf9c9a913592f552ef42e960ceb251 [formerly 6fdd9cb24168f145e4a9f343626338d4b70edf46] [formerly 93f388d3554aeb51c346dfa748815a48be22be57 [formerly 834097b4ebea1a4b06de3ce302efb409184b0564]] [formerly b905c8ffa0319270c21ef0216882e27a4db89ecb [formerly 33ea5cd82cddf98b81ead589d5850f3f596cb174] [formerly cc6720b0118be7b552975a2056a456862b72f871 [formerly b91a48418ef50777d95e2e5054e4e048d8f68fc6]]]
    Former-commit-id: a9f9d92969483fadff942dba9ebb4c20c0edd5be [formerly 85d81fb6acd042e4ba83c085fa02dc47410852d8] [formerly eb075f7c0eff313b51766944d6e4c0e9380d6f4a [formerly 4f45d5968c02a58b7bedc7e6ee64688b43817198]]
    Former-commit-id: 2c1cea87e4c3b4efede7a0dd2fde5f9361ebe164 [formerly 04f2b54bf3a4fcac2ce1dc907cd948d3509ad1f4]
    Former-commit-id: de30745ec6655b4c7facc4c73e0297fb0e6bd24d

--- lib/OSCAR/AddNode.pm
+++ lib/OSCAR/AddNode.pm
@@ -30,12 +30,15 @@ use strict;
 use vars qw($VERSION @EXPORT);
 use Tk;
 use Carp;
+use POSIX;
 use SystemInstaller::Tk::Common;
 use base qw(Exporter);
 use SIS::Image;
 @EXPORT = qw(addnode_window);
 
 $VERSION = sprintf("r%d", q$Revision$ =~ /(\d+)/);
+
+our $pid;
 
 sub addnode_window {
     my ($parent, $interface) = @_;
@@ -54,6 +57,11 @@ sub addnode_window {
     &main::oscar_button($window, "Step $step_number:", "Setup Networking...",
 			[\&main::mac_window, $window, $step_number,
 			 {interface => $interface}], 'netboot');
+
+    # Monitor Cluster Deployment
+    our $monitor_button = &main::oscar_button($window, "", "Monitor Cluster Deployment",
+             [\&monitor_deployment, $window], 'monitor_deployment');
+
     my $boot=$window->Label (-text=>"Before continuing, network boot all of your nodes. 
     Once they have completed installation, reboot them from 
     the hard drive. Once all the machines and their ethernet
@@ -68,12 +76,40 @@ sub addnode_window {
 
     my $exitbutton = $window->Button(
                                      -text => "Close",
-                                     -command => sub {$window->destroy},
+                                     -command => sub {kill "HUP" => $pid if $pid; $window->destroy},
                                     );
 
     $exitbutton->grid("-","-",-sticky => "ew");
     center_window( $window );
 
+}
+
+# Pop up si_monitortk from SystemImager
+sub monitor_deployment {
+    my ($window) = @_;
+    our $pid;
+    our $monitor_button;
+
+    my $cmd = "/usr/sbin/si_monitortk --progress";
+    $monitor_button->configure(-state => 'disabled');
+
+    my $pipe = new FileHandle;
+    $pid = $pipe->open("$cmd |") or die "Cannot fork: $!";
+
+    $window->fileevent($pipe, 'readable', [\&stop_monitor_deployment, $window, $pipe]);
+    return 1;
+}
+
+# Unfreeze the "Monitor Cluster Deployment" button in "Add OSCAR Clients" widget
+sub stop_monitor_deployment {
+    my ($window, $pipe) = @_;
+    our $pid;
+    our $monitor_button;
+
+    waitpid($pid, WNOHANG);
+
+    $monitor_button->configure(-state => 'normal') if Tk::Exists($monitor_button);
+    $window->fileevent($pipe, 'readable', "");
 }
 
 1;
