62c3a0054085ca720a530742b4b994b26a6bc27d
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-07-19 04:55:50 +0000

    Include a generic mechanism: system sanity check. This mechanism checks if the system is ready for the initialization of OSCAR. We currently check if the user is logged as root and if selinux is deactivated. Long term all system checking done before the actual initialization of OSCAR will have to be done with this mechanism. See scripts/system-sanity and scripts/system-sanity.d/ for more details.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@5185 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 54386a82282abc9f58ae265e01911143b2e27d67 [formerly 083effe87a27089aa9d268690cacb0825154fddf] [formerly d829138d47628c02badc72a61ab068b3200d7c5d [formerly 5a6691a1d8c3f113e656cf9690cf3503ca55aa50]] [formerly 9f29dafc77e12bc18b262b96a140a36d6b7217e7 [formerly 056b86059046636b2db64775e1595c65d90f1ff5] [formerly db4009de81a67d1764ecb7d591aa5c0eced8109c [formerly 08a483f3ebf2b673afbe10788616a586067fa550]]]
    Former-commit-id: 23985887aa1027ab841eeb87c4dfc2825af2e41b [formerly cca9e60e6726106e038220081961f09d29f1caf8] [formerly c3d1e0648e41b7a3c8f6abba9eab73f2f5d7ed8b [formerly 173be7b5518671c10017c32422f4bdcedaf9e22b]]
    Former-commit-id: d280076266f4aef09e19691a89d1755b57123a06 [formerly 3b96d11bb58a1d913333533353b7d63b7b9ce979]
    Former-commit-id: 3d6afb6603325bb870210ede36b6166dd04f821d

---
+++ scripts/system-sanity.d/selinux-check.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+# $Id$
+#
+# Copyright (c) 2006 Oak Ridge National Laboratory.
+#                    All rights reserved.
+
+use warnings;
+use English '-no_match_vars';
+use lib "$ENV{OSCAR_HOME}/lib";
+
+# NOTE: Use the predefined constants for consistency!
+use OSCAR::SystemSanity;
+
+my $rc = FAILURE;
+
+if (! -f "/usr/sbin/selinuxenabled" ) {
+        # We cannot find the file to test selinux, we assume selinux is 
+        # not installed
+        $rc = SUCCESS;
+} else {
+        if ( system("/usr/sbin/selinuxenabled") ) {
+	        $rc = SUCCESS;
+
+        } else {
+	        print " ----------------------------------------------\n";
+        	print "  $0 \n";
+        	print "  SELinux is enabled, please deactivate it editing\n";
+        	print "  /etc/selinux/config\n";
+        	print " ----------------------------------------------\n";
+
+        	$rc = FAILURE;  
+        }
+}
+
+exit($rc);
