dbf7b76877ffe3ee64a54982ed1c33701b59c377
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-07-21 13:46:17 +0000

    [Debian Port] Add a check of the /etc/apt/sources.list file.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@5201 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 0c6ca9833d9c5c192c673cc21bc922d17c5371b0 [formerly 96b1e6514afe8d1e613786bcb2bc606ad029d4f7] [formerly fdb60b96648d5374e327d0d76ad5c1e2782c705e [formerly 6766c847ac33b11288d194a590174fed6ffd9ad0]] [formerly 5f8be7f26a6c568bc1ff639b964f50a11147a0b0 [formerly 48eb61a855035591506a6b5fdc78503599f9ca2d] [formerly e2e4ecc0e5beb69ffa75286419d4a1d0a5510506 [formerly c55659a810ee5c640ecec3b6602550bdfb6ee79c]]]
    Former-commit-id: eb22c650f0eb4803e949d2a588706a8747213a7b [formerly ed232888ef0713f5e1055bafd9531629626c6ee5] [formerly 11cf8727a71efc2be72939eac0e5d541ddf774aa [formerly 9a870838564bf72355dc1a9cfb179269e88c6045]]
    Former-commit-id: b601b45e29f26249612014cf8ec593efbba6716e [formerly 8ee16b72bc28c911e704a6bf0442db528c085293]
    Former-commit-id: 4d2012f25a7f5a969074960e87f7fc656ca86222

---
+++ scripts/system-sanity.d/debrepo-check.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/perl
+#
+# Copyright (c) 2006 Oak Ridge National Laboratory.
+#                    All rights reserved.
+#
+# This script check if the /etc/apt/sources.list on Debian is a minimum correct
+# (with at least one entry.
+
+use warnings;
+use English '-no_match_vars';
+use lib "$ENV{OSCAR_HOME}/lib";
+
+# NOTE: Use the predefined constants for consistency!
+use OSCAR::SystemSanity;
+
+my $rc = WARNING;
+
+sub check_apt_entry {
+        my $cmd = "cat /etc/apt/sources.list | sed -e '/^deb /!d'";
+        my $ret = `$cmd`;
+        if (length($ret) > 0 ) {
+                return 0;
+        }
+        return -1;
+}
+
+if (! -f "/etc/apt/sources.list" ) {
+	# the file /etc/debian_version does not exist, it is not a Debian based
+	# distro, we stop here.
+	$rc = SUCCESS;
+} elsif (check_apt_entry() == 0) {
+	
+	$rc = SUCCESS; 
+}
+
+if ($rc eq WARNING) {
+	print " ----------------------------------------------\n";
+        print " $0 \n";
+        print " Impossible to find a valid entry in your \
+		/etc/apt/sources.list file.\n";
+        print " Please update it to point to a valid Debian repository.\n";
+        print " ----------------------------------------------\n";
+}
+
+exit ($rc);
+
+
