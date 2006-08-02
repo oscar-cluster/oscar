403a3eec7b3c133ca07afb8215d6a5fe23f88479
wesbland <wesbland@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-08-02 16:41:51 +0000

    Rather than always waiting for the user to type continue to see if the clients are booted, the main_cli now calls a script which will determine when the nodes are done.  This script can be changed with a flag on the install_cluster command line.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@5270 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 6a0415e3765167ca87bec79e8d8d0238c16e8c3d [formerly 1331ee62024ba4b9bb3dea51cf844194d270ead3] [formerly 102c062d847c102af5b8a85c8c7dc58cff774b4b [formerly 5e96307fef14f07579a0b2e511a89dd051989d1c]] [formerly 2835b7e964f592bd65d190c70ae3026e95daa611 [formerly 573e6c828a45ec517885cf3189035168d1f377cf] [formerly ad49e88561d4bff63b0b98201f68ebe7c7830546 [formerly e271e032c6d67edeb4a677a1192c872fbdb2df21]]]
    Former-commit-id: 5aed506ed27601fa4f0bbdc32d82423a32286b08 [formerly 4006963507fc6d80226f895cd3c6bcad5598936b] [formerly 52130dc336e184bff2b9ad29b23d2ae02167d7c7 [formerly 38f04f4f98ebce4190d9efbc1f5d27df2d11f8b4]]
    Former-commit-id: b17c9f3844a5e04f5941374277af39cf1838abf0 [formerly 90c18ce55aa744ffb420668f937bcabd982f0893]
    Former-commit-id: d788045dfd1b0d51f32aed5ad46ded250fa318f4

---
+++ src/cli/simple_complete.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w
+
+# $Id$
+#
+# Copyright (c) 2006 Oak Ridge National Laboratory.
+#                  All rights reserved.
+#
+# This script is the simple case to wait for the user to 
+# type continue to quit.  The script is made to be used
+# in conjunction with the main_cli script.  When the CLI
+# needs to wait for the nodes to boot up and load their
+# image, this script is one of the options to use to
+# determine when that has completed.
+
+$|++;
+
+my $input = " ";
+while ($input ne "continue") {
+    print "*************************************************************\n" .
+    "* Before continuing, network boot all of your nodes.        *\n" .
+    "* Once they have completed installation, reboot them from   *\n" .
+    "* the hard drive. Once all the machines and their ethernet  *\n" .
+    "* adaptors are up, type \'continue\' and press Enter.         *\n" .
+    "*************************************************************\n";
+    $input = <STDIN>;
+chomp $input;
+}
+
+exit 0;
