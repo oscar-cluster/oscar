5e029e802dfa763d53d41cdb6586c65614aebefd
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-08-20 21:36:09 +0000

    This check-in fixes #142, we check now much more stuff regarding the hostname and network configuration. It should check everything described in the bug report. If not please reopen the bug with the details of what you want to be done.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@5307 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: e9e900deaa430b35dc59f431885cbb07641aa235 [formerly 96946493fec73fdafc3ba12b3c40596355da1020] [formerly fb4a4aa35d211ee7e0e64103120ecc97069bf433 [formerly b1e3ea9e44df8270b00befd4e5ee7242a0716647]] [formerly 4da4c0835f325956629c9042a7e68918a1932999 [formerly c8927f23b6581b5599bcc4543c17a11ce5727640] [formerly ee72da7851ef6ed3ac7a6b8ab2832f72f8999fd8 [formerly 4c52ee5a584d523ce4ba463a7f76f0d53ab94aa7]]]
    Former-commit-id: a88839066b32ee048ded4ec63269c6852b34509a [formerly 19ff8abb8b5d3ee589fb19bcfd9a3262025d992f] [formerly ac542aba402ddc680d6776b96b6eee2309702c45 [formerly df2c642fb2531626676a1e6787ad65f0903d0fd7]]
    Former-commit-id: fc49d005bcb2fc701e2060056e17f152516125b5 [formerly 7f6c791932bd5b40500907e9078967604c213e8d]
    Former-commit-id: bcf9af57219cda4ebf5593fcde366bf91bc25989

--- scripts/system-sanity.d/hostname-check.pl
+++ scripts/system-sanity.d/hostname-check.pl
@@ -14,28 +14,99 @@ use OSCAR::SystemSanity;
 
 my $rc = SUCCESS;
 
-my $hostname = (uname)[1];
-if ($hostname eq "") {
-    $rc = FAILURE;
-} else {
+# We check if the hostname is valid, i.e. does not return localhost or
+# something similar. We also check if the host name is assigned to the
+# loopback interface or not.
+# @return: FAILURE if a problem is detected, SUCCESS else.
+sub check_hostname {
+    my $hostname = (uname)[1];
+    if ($hostname eq "") {
+        print " ---------------------------------------\n";
+        print " ERROR: Localhost name seems to be empty\n";
+        print " ---------------------------------------\n";
+        return FAILURE;
+    }
     my ($shorthostname) = split(/\./,$hostname,2);
+    if ($shorthostname eq "") {
+        print " --------------------------------------\n";
+        print " ERROR: shorthostname seems to be empty\n";
+        print " --------------------------------------\n";
+        return FAILURE;
+    }
     my $dnsdomainname = `dnsdomainname`;
-    chomp($dnsdomainname);
-    chomp ($hostname);
-
+    chomp ($dnsdomainname);
     if ($shorthostname eq "localhost") {
-        $rc = FAILURE;
+       return FAILURE;
     }
     if ($hostname eq "localhost.localdomain") {
-        $rc = FAILURE;
+       return FAILURE;
     }
+
+    # the value of hostname should not to be assigned to the loopback
+    # interface
+    my $hostname_ip = `grep $shorthostname /etc/hosts | awk ' { print \$1 } '`;
+    chomp ($hostname_ip);
+    if ($hostname_ip eq "127.0.0.1") {
+        print " -------------------------------------------------\n";
+        print " ERROR: your hostname is assigned to the loopback \n";
+        print " interface\n";
+        print " Please assign it to your public network interface\n";
+        print " updating /etc/hosts\n";
+        print " -------------------------------------------------\n";
+        return FAILURE;
+    }
+    
+    return SUCCESS;
 }
 
-if ( $rc eq FAILURE ) {
-    print " ----------------------------------------------\n";
+# Check the configuration of the interface used by OSCAR
+# @return: FAILURE if a problem is detected, SUCCESS else.
+sub check_oscar_interface {
+    my $oscar_if = $ENV{OSCAR_HEAD_INTERNAL_INTERFACE};
+    my %nics;
+    open IN, "/sbin/ifconfig |" || die "ERROR: Unable to query NICs\n";
+    while( <IN> ) {
+        next if /^\s/ || /^lo\W/;
+        chomp;
+        s/\s.*$//;
+        $nics{$_} = 1;
+    }
+    close IN;
+
+    if (! ($oscar_if && exists $nics{$oscar_if}) ) {
+        print " ----------------------------------------------------\n";
+        print " ERROR: A valid NIC must be specified for the cluster\n";
+        print " private network.\n";
+        print " Valid NICs: ".join( ", ", sort keys %nics )."\n\n";
+        print " You tru to use: " . $oscar_if . ".\n";
+        print " ----------------------------------------------------\n";
+        return FAILURE;
+    }
+
+    # we check now the IP assgned to the interface used by OSCAR
+    my $oscar_ip = `grep oscar_server /etc/hosts | awk ' { print \$1 } '`;
+    chomp ($oscar_ip);
+    my $oscar_if_ip = `ifconfig $oscar_if | grep "addr:" | awk '{ print \$2 }' | sed -e 's/addr://'`;
+    chomp ($oscar_if_ip);
+    # the first time we execute OSCAR, /etc/hosts is not updated, it is 
+    # normal
+    if ($oscar_ip ne "" && ($oscar_ip ne $oscar_if_ip)) {
+        print " --------------------------------------------------\n";
+        print " ERROR: it seems the interface used is not the one \n";
+        print " assigned to OSCAR in /etc/hosts \n";
+        print " --------------------------------------------------\n";
+        return FAILURE;
+    }
+
+    return SUCCESS;
+}
+
+if (check_hostname () eq FAILURE || check_oscar_interface () eq FAILURE) { 
+    $rc = FAILURE;
+    print " -----------------------------------\n";
     print "  $0 \n";
-    print "  Hostname not correct \n";
-    print " ----------------------------------------------\n";
+    print "  Network configuration not correct \n";
+    print " -----------------------------------\n";
 }
 
 exit($rc);
