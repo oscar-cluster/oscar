3a5c756f56a515128be1f187bea348e1125879bf
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-09-28 15:06:29 +0000

    [BUGFIX] Do not assume any more sshd is installed by default by the Linux distribution.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@5378 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: b4b81064ed107be9368784eb2db9a2d8b8eb0bf7 [formerly 7fb07263731c4b938e8521a75b17635a62512891] [formerly d518839619a4d584bbb938ec8272872abd34c505 [formerly 7159a214c513d9ce4d9e85360660b0cc8de0129e]] [formerly d56d64541bb461d4a0ff80944ae2409affbe2c43 [formerly df391d14e9327f39517336e8bdf67c29b462a661] [formerly 4801a16e5275949a70a883c6c340957f8a229181 [formerly 0233227928e1d6524d0b52e5784f2321319800e0]]]
    Former-commit-id: 2a3445f48b65d474a34eb81684eaa75154a02ac7 [formerly 6064c6f072813431ce4aae835a2fb3acb7ee0d84] [formerly 47a3d1a87902753864af4f1f34b6389ede899373 [formerly dbefa3dac7a9ad587728d6d3e07e78138b8de910]]
    Former-commit-id: 5a992b25ceb22fc2c5928b15631022012f790a90 [formerly 9931f4d1b2471bcbfc8c9324ac8e5cf4543c9d43]
    Former-commit-id: 4d76f8b640a2cf0512d3d1857d257e9384789f03

--- scripts/system-sanity.d/ssh-check.pl
+++ scripts/system-sanity.d/ssh-check.pl
@@ -12,18 +12,32 @@ use lib "$ENV{OSCAR_HOME}/lib";
 use OSCAR::SystemSanity;
 
 my $rc = FAILURE;
-my $ssh_config = `grep \"PermitRootLogin\" /etc/ssh/sshd_config | grep -v \"^\\s\*\#\" | awk ' { print \$2} '`;
-chomp ($ssh_config);
-if ( $ssh_config eq "yes" ) {
-	$rc = SUCCESS;
-} else {
-	print " ----------------------------------------------\n";
-	print "  $0 \n";
-	print "  Option PermitRootLogin in /etc/ssh/sshd_config should be \'yes\'\n";
-	print "  Current value is \'$ssh_config\'\n";
-	print " ----------------------------------------------\n";
 
-	$rc = FAILURE;  
+# We first test if the file /etc/ssh/sshd_config exists or not.
+# If the file does not exist, it should be because it is the first time
+# OSCAR runs, so we skip the sanity check
+# If the file exists, we check the configuration.
+if (!-e "/etc/ssh/sshd_config") {
+    print "FYI the file does /etc/ssh/sshd_config not exists, it is not \
+possible to check the sshd configuration. It is normal, most probably \
+your Linux distribution does not install sshd by default, OSCAR will do it \
+for you.";
+    $rc = SUCCESS;
+    }
+else {
+    my $ssh_config = `grep \"PermitRootLogin\" /etc/ssh/sshd_config | grep -v \"^\\s\*\#\" | awk ' { print \$2} '`;
+    chomp ($ssh_config);
+    if ( $ssh_config eq "yes" ) {
+    	$rc = SUCCESS;
+    } else {
+    	print " ----------------------------------------------\n";
+    	print "  $0 \n";
+    	print "  Option PermitRootLogin in /etc/ssh/sshd_config should be \'yes\'\n";
+    	print "  Current value is \'$ssh_config\'\n";
+    	print " ----------------------------------------------\n";
+
+    	$rc = FAILURE;  
+    }
 }
 
 exit($rc);
