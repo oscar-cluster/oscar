a110f465ed24b66dea50989be6b9276ddd916de3
efocht <efocht@18f9d06c-6ae2-0310-8438-938a554feff9>
2006-10-10 14:00:43 +0000

    Adding new detection module for SLES.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@5397 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: de5b26edd7f88e777b091920fdb7d1b1d6f31275 [formerly bf41675c78cf73712f4db65f9383895cec1ff897] [formerly 959e9e1b89b1455cddb37efdd42269a107e7341a [formerly 6dbb3f7f11ff3726603bdf13d12795d52bb08c5f]] [formerly d41f378a3d2909835c186e60c7be814081ccb467 [formerly 6a0a5606c24344aa5ba2313266e674d3fcdb478d] [formerly 71353a92a757d9374a6e3ad75d812fbcf9aba2d8 [formerly 53240bba3355f4167c890a7954203babb8b9752d]]]
    Former-commit-id: a2a8078f6832bf10fad1ca6d710aea1d2916ca57 [formerly 284560f49612cabf81648e5a4c8694a2c1337dbf] [formerly 505518815730d5885cad594fa9f9ca48333c014b [formerly aad357013fda9c75df857bf26ae69fa5f230d5f2]]
    Former-commit-id: e3ed220fe546be6db7fd68c2d1a0085317d41daf [formerly a158e572ea98d4fc0494864b89cfef0cd4abda4c]
    Former-commit-id: 50f9f198af9fef9d5a54c0b96836c15f337f64b1

---
+++ lib/OSCAR/OCA/OS_Detect/SLES.pm
@@ -0,0 +1,83 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) Erich Focht <efocht@hpce.nec.com>
+#      - complete rewrite to enable use on top of images
+#      - enabled use on top of package pools
+# 
+# This file is part of the OSCAR software package.  For license
+# information, see the COPYING file in the top level directory of the
+# OSCAR source distribution.
+#
+# $Id$
+#
+
+package OCA::OS_Detect::SLES;
+
+use strict;
+
+my $distro = "sles";
+my $compat_distro = "sles";
+my $pkg = "rpm";
+my $detect_package = "sles-release";
+my $detect_file = "/bin/bash";
+
+sub detect_dir {
+    my ($root) = @_;
+    my $release_string;
+
+    if (-f "$root/etc/SuSE-release") {
+	$release_string = `cat $root/etc/SuSE-release`;
+    } else {
+	return undef;
+    }
+
+    # this hash contains all info necessary for identifying the OS
+    my $id = {
+	os => "linux",
+	chroot => $root,
+    };
+
+    # match strings for SLES
+    if ($release_string =~ m/SUSE Linux Enterprise Server (\d+) \((\S+)\)/) {
+	my $os_release = $1;
+	$id->{distro} = $distro;
+	$id->{distro_version} = $os_release;
+	#$id->{distro_update} = $os_update;
+	$id->{compat_distro} = $compat_distro;
+	$id->{compat_distrover} = $os_release;
+	$id->{pkg} = $pkg;
+    } else {
+	return undef;
+    }
+
+    # determine architecture
+    my $arch = main::OSCAR::OCA::OS_Detect::detect_arch_file($root,$detect_file);
+    $id->{arch} = $arch;
+
+    # Make final string
+    $id->{ident} = "$id->{os}-$id->{arch}-$id->{distro}-$id->{distro_version}-$id->{distro_update}";
+    return $id;
+}
+
+sub detect_pool {
+    my ($pool) = @_;
+
+    my $id = main::OSCAR::OCA::OS_Detect::detect_pool_rpm($pool,
+							  $detect_package,
+							  $distro,
+							  $compat_distro);
+
+    return $id;
+}
+
+sub detect_fake {
+    my ($fake) = @_;
+    my $id = main::OSCAR::OCA::OS_Detect::detect_fake_common($fake,
+							     $distro,
+							     $compat_distro,
+							     $pkg);
+    return $id;
+}
+
+# If we got here, we're happy
+1;
