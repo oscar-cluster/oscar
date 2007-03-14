b40a7b2bd01180c68275011f4ecf9a99ff7d1c42
bli <bli@18f9d06c-6ae2-0310-8438-938a554feff9>
2007-03-14 07:27:12 +0000

    Checking in RPMs built by Allan Menezes for Fedora Core 7 x86_64 support (based on Test 2)
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@5736 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 2f98163b98b53a57813ff2e76c934a0082bff6fa [formerly 6b613da58621b74321891ddebc6ab55fdc2a8b67] [formerly f80cde006186888c3bf374688b2ba692f8a6e8c1 [formerly 02ced79f706cbbb1f51b68bdd57aaea75803684f]] [formerly 60f1007044f1c907bba890b1da07d370460b93cf [formerly 1333fd17f29670404a3a8349fb991dc4c02fb6aa] [formerly f563c2c8704e2d78ed8067a5ba75423a21acd878 [formerly f71cbc550f6d98a1b026f859e046f102cd451380]]]
    Former-commit-id: 15456bc264933e7ac7f53d12585993051a7e73dd [formerly fd6b2243e91cf3d38a696ff59a3b04aec663fb64] [formerly f564d819e57890e7ca5bb84b4b04c68975ce95c6 [formerly 9b549e36cf38186b7da6cd91684217c7553378d9]]
    Former-commit-id: 07f281d62275a6100934891a4966cca774a08c4e [formerly 0e57edfe8763d45a29583c51945c631f10d42c03]
    Former-commit-id: 1616a302c71d3763e61e66c0dc5029dfb0c7b00f

--- dist/newmake.sh
+++ dist/newmake.sh
@@ -352,7 +352,7 @@ if [ -n "$DISTROS" -o -n "$ALL_REPOS" ]; then
     fi
     if [ -n "$ALL_REPOS" ]; then
 	DISTROS="common-rpms \
-            fc-4-i386 fc-4-x86_64 fc-5-i386 fc-5-x86_64 fc-6-i386 fc-6-x86_64 fc-7-i386 \
+            fc-4-i386 fc-4-x86_64 fc-5-i386 fc-5-x86_64 fc-6-i386 fc-6-x86_64 fc-7-i386 fc-7-x86_64 \
             mdv-2006-i386 \
             rhel-4-i386 rhel-4-x86_64 \
             suse-10.0-i386"
