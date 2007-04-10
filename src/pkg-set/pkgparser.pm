3def2c2cf2f9835f389288202d5471a60ec04e7c
wesbland <wesbland@18f9d06c-6ae2-0310-8438-938a554feff9>
2007-04-10 17:09:58 +0000

    Some simple utilities to parse a package sets xml file.  Currently only gets the names of the package sets and the packages in a specified set.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@5799 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: d228f65bd8c57c9bc8afdf09a46dbca0ab76d238 [formerly ee271b02f48bb95950ab71f2e2a67fce33f17b57] [formerly 4acb1a299d40080caa53a95b77d7f9d279614141 [formerly 55476f4673ff4a1ad2601c7b430cd240678834ef]] [formerly 171724904968fe94281dfa2a1a78e135e5fe0bf7 [formerly 9a90211951521c633475ac2182bbf927b2b2dbc4] [formerly 55bc554f08a932d2cce53e707f082beabcac95e7 [formerly 24569fec3fc2473fa2b453f57c7cf07dba5fe512]]]
    Former-commit-id: 0f5a98ba53d04ed6990dc360f5f69e2d138f142c [formerly a0222a4e015d895fe35392301e513dee2e75c481] [formerly 37bf9d62428977d2df12caf8ffc7d07eb9a40cb4 [formerly 09006a559d546e5c2b5de94f59dd20bb9a633279]]
    Former-commit-id: 1d9dcb8241193b06827d0aea41e2ffa9ae509186 [formerly 89b95f77e9467ca1ec51b36f41fe3f8bc653919e]
    Former-commit-id: d22c730766030f50cc74d6ee924dcfec141fac53

---
+++ src/pkg-set/pkgparser.pm
@@ -0,0 +1,77 @@
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+#   Copyright (c) 2007 Oak Ridge National Laboratory
+#                      All rights reserved
+#
+#   $Id$
+
+use strict;
+
+use vars qw(@EXPORT);
+use base qw(Exporter);
+our @EXPORT = qw(read_file get_package_sets get_packages);
+
+use XML::Simple;	# Read the XML package set files
+use Data::Dumper;
+
+our $xml = new XML::Simple;  # XML parser object
+our $filename = "pkgsets.xml";
+
+our $data;
+
+#########################################################################
+# Subroutine : read_file                                                #
+# Opens the file and reads in the package list and version information  #
+# Parameters : None                                                     #
+# Returns    : None                                                     #
+#########################################################################
+sub read_file {
+	our $data;
+
+	$data = $xml->XMLin($filename, ForceArray => 1);
+}
+
+#########################################################################
+# Subroutine : get_package_sets                                         #
+# Gets the names of all the package sets stored in the package sets file#
+# Parameters : None                                                     #
+# Returns    : An array of the package set names                        #
+#########################################################################
+sub get_package_sets {
+	our $data;
+
+	read_file();
+
+	my @sets = keys(%{$data->{packageSet}});
+
+	return @sets;
+}
+
+#########################################################################
+# Subroutine : get_packages                                             #
+# Gets the names of all the packages in the package set                 #
+# Parameters : Name of the package set                                  #
+# Returns    : An hash of the package information                       #
+#########################################################################
+sub get_packages {
+	our $data;
+	my $packageSet = shift;
+
+	my @packages = keys(%{$data->{packageSet}->{$packageSet}->{package}});
+
+	return @packages;
+}
+
+1;
