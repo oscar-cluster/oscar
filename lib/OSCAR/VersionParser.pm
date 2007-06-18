26dcb5155c839a3a56f509daecbbccd21282437c
wesbland <wesbland@18f9d06c-6ae2-0310-8438-938a554feff9>
2007-06-18 14:49:50 +0000

    Library to parse and compare OPKG versions as specified by the wiki. [wiki:http://svn.oscar.openclustergroup.org/trac/oscar/wiki/OPKGVersioning Information]
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@5919 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 259679a29f6623068d78bc39f5da91611925e543 [formerly cb74c04757a70f6a6768fb6617df1b5a1031c049] [formerly 132a058f1a844f9823c48a7bbbf48b0e3b49778d [formerly 0b5457e9cb9312cc5465680e29712d660e8ee7d2]] [formerly ac485a4fa7c1b6b8f1805358b10f52c23a493a2d [formerly 77aa04061f78725b7f41583823d69e85274c7f5f] [formerly df6c40a82a3bbc422fa998a54703a0df1f8042bb [formerly 71a70f22789e4fec235264476b93fdb2cfb83976]]]
    Former-commit-id: 99b78328c972b7f401c0256ce247a7b36f481a06 [formerly 29740a135d5400d60c53c008241beabaec1faa68] [formerly dbbf1d6d73131261667fe5e261bc3c282c525b9e [formerly 2d11474d621afff310520bd2a3442c79b93b8111]]
    Former-commit-id: 0e046ba5b417046d92458856339564b5adecd827 [formerly f37f48a1d09bb8b259b67d2d0aab5776ae869ea5]
    Former-commit-id: 9ad775d4e9c38857cb9a03b57c7db33fa1547f36

---
+++ lib/OSCAR/VersionParser.pm
@@ -0,0 +1,289 @@
+package OSCAR::VersionParser;
+
+#   $Id$
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+#   Copyright (c) 2007 Oak Ridge National Laboratory
+#                      All rights reserved
+#
+#   Code translated from the dpkg version parsing and comparison tool
+#
+#   This module tests two OPKG version numbers according to a comparison
+
+use strict;
+
+use vars qw(@EXPORT);
+use base qw(Exporter);
+our @EXPORT = qw( version_satisfied parse_version);
+
+our $debug;
+	
+use Data::Dumper;
+
+sub parse_version {
+	my $version_string = shift;
+	
+	if ($version_string eq '') {return "Version string is empty"};
+	
+	# The position in the version string
+	my $verpos = 0;
+	
+	# The next character to be parsed
+	my $ver_char = substr($version_string, $verpos, 1);
+	
+	# Trim leading and trailing space
+	while($verpos < length($version_string) && ($ver_char eq ' ' || $ver_char eq "\t" || $ver_char eq "\n") ) {
+		print $ver_char . "\n";
+		$ver_char = substr($version_string, ++$verpos, 1);
+	}
+	
+	# Save the position of the first "real character"
+	my $firstpos = $verpos;
+	
+	while($verpos < length($version_string) && $ver_char ne ' ' && $ver_char ne "\t" && $ver_char ne "\n") {
+		$ver_char = substr($version_string, ++$verpos, 1);
+	}
+	
+	# Save the position of the last "real character"
+	my $endpos = $verpos;
+	
+	# Check for extra chars after trailing spaces
+	while ($verpos < length($version_string) && ( $ver_char eq ' ' || $ver_char eq "\t" || $ver_char eq "\n") ) {
+		$ver_char = substr($version_string, ++$verpos, 1);
+	}
+	
+	if($ver_char) {return "Version string has embedded spaces";}
+	
+	my $version = "";
+	my $revision = "";
+	
+	$verpos = $firstpos;
+	$ver_char = substr($version_string, $verpos, 1);
+	
+	# Search for the first hyphen
+	while ($verpos < length($version_string) && ($ver_char ne '-' && $ver_char ne ' ' && $ver_char ne "\t" && $ver_char ne "\n") ) {
+		$version = $version . $ver_char;
+		$ver_char = substr($version_string, ++$verpos, 1);
+	}
+	
+	# Move the verpos variable
+	$verpos++;
+	
+	# Everything after the hyphen goes under revision
+	if($verpos < $endpos) {
+		$revision = substr($version_string, $verpos, $endpos - $verpos);
+	}
+	
+	return $version, $revision;
+}
+
+#################################################################
+# Tests two versions to see if it meets a specified comparison  #
+# Parameters:                                                   #
+# first_version  - The version part (major, minor, subversion)  #
+#                  of the entire version number for the first   #
+# first_release  - The release part of the first                #
+# second_version - The version part (major, minor, subversion)  #
+#                  of the entire version number for the second  #
+# second_release - The release part of the second               #
+# comparison     - The test to use for the comparison           #
+#                  Valid inputs - gt, lt, gte, lte, eq          #
+# debug          - If there is something other than 0 here it   #
+#                     will print debugging information          #
+#                                                               #
+# Returns:                                                      #
+#   Less than 0     - If there is an error                      #
+#   0               - If the comparison is false                #
+#   Greater than 0  - If the comparison is true                 #
+#################################################################
+sub version_satisfied {
+	my $first_version = shift;
+	my $first_release = shift;
+	my $second_version = shift;
+	my $second_release = shift;
+	my $comparison = shift;
+	our $debug = shift;
+	
+	my $r;
+	
+	if($comparison ne 'gt' && $comparison ne 'lt' && $comparison ne 'gte' && 
+		$comparison ne 'lte' && $comparison ne 'eq') {
+		return -1;
+	}
+	
+	$r = version_compare($first_version, $first_release, $second_version,
+		$second_release);
+		
+	print "R: $r\n" if $debug;
+	
+	if ($comparison eq 'gt') {
+		return 1 if ($r > 0);
+	} elsif ($comparison eq 'lt') {
+		return 1 if ($r < 0);
+	} elsif ($comparison eq 'gte') {
+		return 1 if ($r >= 0);
+	} elsif ($comparison eq 'lte') {
+		return 1 if ($r <= 0);
+	} elsif ($comparison eq 'eq') {
+		return 1 if ($r == 0);
+	}
+	
+	return 0;
+}
+
+# Compares two versions according to the scheme specified in the wiki 
+# (http://svn.oscar.openclustergroup.org/trac/oscar/wiki/OPKGVersioning)
+sub version_compare {
+	my $first_version = shift;
+	my $first_release = shift;
+	my $second_version = shift;
+	my $second_release = shift;
+	
+	my $r;
+	
+	$r = verrevcmp($first_version, $second_version);
+	if ($r) {return $r;}
+	
+	else {
+		return verrevcmp($first_release, $second_release);
+	}
+}
+
+# Returns the ordering number of a character
+# Digits are given an order of 0 (when it's time to compare digits, they don't
+# 	need this subroutine)
+# Non-digits are ordered by their ASCII value
+sub order {
+	my $x = shift;
+	
+	if ($x eq '~') {return -1;}
+	elsif ($x =~ /\d/) {return 0;}
+	elsif ($x !~ /\d/) {
+		return ord($x);
+	} else {
+		return ord($x + 256);
+	}
+}
+
+# Does the actual comparison between two parts of the entire version
+# This only compares version to version or release to release, not a combination
+sub verrevcmp {
+	our $debug;
+
+	print "verrevcmp\n" if $debug;
+	my $val = shift;
+	my $ref = shift;
+	
+	print "VAL: $val\n" if $debug;
+	print "REF: $ref\n" if $debug;
+	
+	# The position in the version variables
+	my $valpos = 0;
+	my $refpos = 0;
+	
+	if (!$val) {$val = "";}
+	if (!$ref) {$ref = "";}
+	
+	while ( $valpos < length($val) || $refpos < length($ref) ) {
+		print "OUTER LOOP\n" if $debug;
+	
+		# Keeps track of the first difference in so that if we need to go back
+		# to it we can.  Useful for cases such as v2 vs. v10.  The second should
+		# evaluate as greater, but there is no way to know it when we are at the
+		# first character
+		my $first_diff = 0;
+		
+		# The next character to be evaluated
+		my $val_char = substr($val, $valpos, 1);
+		my $ref_char = substr($ref, $refpos, 1);
+		
+		print "VAL_CHAR: $val_char\n" if $debug;
+		print "REF_CHAR: $ref_char\n" if $debug;
+		
+		# While there is something in $val and the next character in val is not
+		# a digit, or there is something in $ref and the next character in ref
+		# is not a digit
+		while ( ($val && $val_char !~ /\d/) || ($ref && $ref_char !~ /\d/) ) {
+			print "INNER LOOP\n" if $debug;
+			my $vc = order(substr($val, $valpos, 1));
+			my $rc = order(substr($ref, $refpos, 1));
+			
+			print "VC: $vc\n" if $debug;
+			print "RC: $rc\n" if $debug;
+			
+			# If one has a nondigit and the other doesn't or the other's
+			# nondigit is smaller, return that the larger won
+			if($vc != $rc) {
+				return $vc - $rc;
+			}
+			
+			# Increment the counters
+			$valpos++;
+			$refpos++;
+			
+			# Move the position of the next character
+			$val_char = substr($val, $valpos, 1);
+			$ref_char = substr($ref, $refpos, 1);
+			
+			print "VAL_CHAR: $val_char\n" if $debug;
+			print "REF_CHAR: $ref_char\n" if $debug;
+		}
+		
+		#$valpos = 0;
+		#$refpos = 0;
+		
+		$val_char = substr($val, $valpos, 1);
+		$ref_char = substr($ref, $refpos, 1);
+		
+		print "VAL_CHAR: $val_char\n" if $debug;
+		print "REF_CHAR: $ref_char\n" if $debug;
+		
+		# While the next character in val is a digit and the next character in 
+		# ref is a digit
+		while( $val_char =~ /\d/ && $ref_char =~ /\d/) {
+			print "INNER LOOP 2\n" if $debug;
+			
+			# If one has a bigger digit, remember it so we can come back to it
+			# later
+			if ($first_diff == 0) { $first_diff = $val_char - $ref_char; }
+			
+			print "FIRST_DIFF: $first_diff\n" if $debug;
+			
+			$valpos++;
+			$refpos++;
+			
+			$val_char = substr($val, $valpos, 1);
+			$ref_char = substr($ref, $refpos, 1);
+			
+			print "VAL_CHAR: $val_char\n" if $debug;
+			print "REF_CHAR: $ref_char\n" if $debug;
+		}
+		
+		# If one has more digits that haven't been evaluated, it wins
+		if ($val_char =~ /\d/) {return 1;}
+		elsif ($ref_char =~ /\d/) {return -1;}
+		
+		# If they have the same number of digits, the one with the bigger
+		# digits wins
+		if($first_diff != 0) {return $first_diff};
+		
+	}
+	
+	# It's a tie, give up
+	return 0;
+}
+
+1;
