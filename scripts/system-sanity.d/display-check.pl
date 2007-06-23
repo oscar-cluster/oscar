141b4b61298f4043945d246e6a62bd41177bb15a
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2007-06-23 05:15:51 +0000

    Creates a new system sanity script to check the DISPLAY environment variable. This check-in fixes #312.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@5948 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 7fa9a79b459e213c3efd597a6da3f7bc0d3117a6 [formerly 5ccdaf0a0e713cfd63ae660298a6218570f970b2] [formerly d026889a61ab4313a5b9145cec918a61d001ad60 [formerly ea48e2be4ef53cf81f0b0c2b249439ccc0d9ab62]] [formerly 8fe332fde1318f49f283a3208f0cfa9fcd1abe6d [formerly e784a7b89d8e4a908831455bc6e185ecd96bd920] [formerly 0fe86f0d257ec0da8210b707234c1695a4ce93a9 [formerly 7cc41ed793f7c3bb055e3d437bb71aab636b3504]]]
    Former-commit-id: d679969b2d95fba2f565528cc4721b9d455f0237 [formerly 4442a35547ccbebae59141c42d661fd06f5849f4] [formerly 6388d9925e859bdc067650d84c0765f09df31223 [formerly c0e67ae43dc70aeafc288a5183b7736d5e51923e]]
    Former-commit-id: b28915f0da8d8c45e2e01ff495bf94f9dfd0320a [formerly 75bd8ab22e36ffd760761ff900d7c6bcae90a742]
    Former-commit-id: 41559687fa052ee3ce59836d22dba2f2728e2da7

---
+++ scripts/system-sanity.d/display-check.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+# $Id$
+#
+# Copyright (c) 2007 Oak Ridge National Laboratory.
+#                    Geoffroy Vallee <valleegr@ornl.gov>
+#                    All rights reserved.
+
+use warnings;
+use English '-no_match_vars';
+use lib "$ENV{OSCAR_HOME}/lib";
+use POSIX;
+
+# NOTE: Use the predefined constants for consistency!
+use OSCAR::SystemSanity;
+
+my $rc = SUCCESS;
+
+if ( not defined $ENV{DISPLAY} || 
+     (!$ENV{DISPLAY} && ($ENV{OSCAR_UI} eq "gui"))) {
+    print " ------------------------------------------------------------\n";
+    print " ERROR: Your \"DISPLAY\" environment variable is not set,\n";
+    print " probably indicating that you are not running in an X windows\n";
+    print " environment.\n";
+    print " OSCAR requires that you run the installer in an X windows\n";
+    print " environment.\n";
+    print " ------------------------------------------------------------\n";
+    $rc = FAILURE;
+}
+
+exit ($rc);
