b3c8960a72e86d04f0e13d8fce9623daed27efa1
ngraham <ngraham@18f9d06c-6ae2-0310-8438-938a554feff9>
2007-07-20 20:02:13 +0000

    Identification of the pool's binary package format: fixes distributions' IDs.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@6043 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: d3b0703634c7e1f7e8eb6e909ccbcbcd46a947b5 [formerly b8b09d30e65ccef77ffaea8f502d18465e1bf5ac] [formerly ca91a709fb2218302df060460a8072d54ed66dd6 [formerly cf068350f2f9aa5c40f41e86f840dbefcf307e5d]] [formerly 3d8436cf3eeeec497daf701eb828787b8906a961 [formerly 4749a042589651771039435523db0cf000c777b5] [formerly 54cbeb73007254197f9cee57b97f4477346808a5 [formerly 1adb832f0507b5f0a097b4cd3e371f18056d8e3f]]]
    Former-commit-id: b1b39c1ff239fe3f30e802422f4729d7a350c8b4 [formerly 8360eafb02a85f9031f27958e71b22ab12e3fb6e] [formerly c96c1c1fa60abeac3122820f6397765a3cd9ba4f [formerly 35d883b1a8d935f206c0b6056bb7fa2db319cb9f]]
    Former-commit-id: cc02ac45893dd4520e8c4ad3c064a83efb8ae4cb [formerly e1b736a57dc8a65dcbeb4ac4680c051239f44958]
    Former-commit-id: 29dbd4eba985e7423e793b6602b55d480594179c

--- lib/OSCAR/PackageSmart.pm
+++ lib/OSCAR/PackageSmart.pm
@@ -71,8 +71,7 @@ sub prepare_pools {
     my $archs = "i386|x86_64|ia64";
     # List of all supported distros. May be nice if we can get this list 
     # from OS_Detect.
-    my $distros =
-        "debian|centos|fedora|mandriva|redhat-el|scientific_linux|sles|suse";
+    my $distros = "debian|fc|mdv|rhel|suse";
     my $format = "";
     # Before to prepare a pool, we try to detect the binary package format
     # associated Not that for a specific pool or set of pools, it is not
