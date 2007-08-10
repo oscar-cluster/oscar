3cfa47f72ca02fac0b630c86c93e015fc49404d9
wesbland <wesbland@18f9d06c-6ae2-0310-8438-938a554feff9>
2007-08-10 16:46:41 +0000

    A small module to convert the hash from OSM into ODA format for OPM.  Still needs to be modified to handle versioning.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@6115 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: c09501a6cfcc36fa2802d23b8f554b3d1cad186f [formerly 5f4d78387ca3c72ef1c463dddb0d43f7d836310e] [formerly 01c4cb494f990df7602c04a418b4c0cac817d971 [formerly b86c5d5c50430a6c6184a411061ab9f4b35ea2cf]] [formerly f570dc5737076022ee98e08b543c5392048615d1 [formerly 91f0999a29785c7ffdc5b9835f72c11d56fe61ae] [formerly 504ba3fbaa66da9cc796b441aeb9a0ca66addf01 [formerly 2263c36b84ec0c823d656fa6b04eda867e166482]]]
    Former-commit-id: 41aa9eea04f9bff84eb39edc1a16d75916cd4bbd [formerly 702bd834f73db57c69089401790d23bf5877ae93] [formerly d659548bd475144426087d35714a17a28cde3be7 [formerly 65c82aab2d31af1f8921226312e20cf22c9e898b]]
    Former-commit-id: efd768fd2e90baaf0cef5585322795a239ba254c [formerly 6d1cb0d0b3747815fc7d675683f15cf6cae1de6a]
    Former-commit-id: 39f7d4697250c20aa81061985e89e587126f6fae

---
+++ src/opm/osmToOpm.pm
@@ -0,0 +1,60 @@
+#!/usr/bin/perl
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
+#
+#   Copyright (c) 2007 Oak Ridge National Laboratory
+#                      All rights reserved
+#
+#	This module is for converting the hash that comes out of OSM into database
+#	form for OPM.
+#
+#	TODO - 	Use version restrictions stored in the hash
+#			There are also other pieces of info not used (distro, arch, lists
+#				of packages and sets selected). These can be saved or discarded.
+#
+#   $Id$
+
+use strict;
+
+use lib "/usr/lib/perl5/OSCAR", "$ENV{OSCAR_HOME}/lib";
+use OSCAR::Database;
+	
+use Data::Dumper;
+
+use vars qw(@EXPORT);
+use base qw(Exporter);
+our @EXPORT = qw(
+	convert_hash_to_oda
+	);
+	
+sub convert_hash_to_oda {
+	my %data = %{$_[0]};
+	
+	# Go through each node in the hash
+	for my $nodename (keys(%data)) {
+		
+		# Go through each package for the node
+		for my $package (keys( $data{$nodename}{package} )) {
+			
+			# Set the package's requested values to 'finished'
+			my %options;
+			my @errors;
+			my %field_values;
+			$field_values{requested} = 'finished';
+			update_node_package_status_hash(\%options, $nodename, $package, 
+					\%field_values, \@errors);
+		}
+	}
+}
