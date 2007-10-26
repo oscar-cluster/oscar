ebd6191fa07b2937c3dfeda15b94de03ad0d6031
dikim <dikim@18f9d06c-6ae2-0310-8438-938a554feff9>
2007-10-26 16:15:59 +0000

    Add YDL component for OS_Detect
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@6355 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 5d828d65d55de91ea8e9262c3dd14c44d05bead7 [formerly 0cf34e177356520f66f98631fc25b7c25d7843a0] [formerly ece50438f439aaf80b141d1f1ec9ccfae4f17de9 [formerly 739aa74774df48d9e6ccfcbc72fefa052d8dfae1]] [formerly 9b0df0f93358c296c3d6f8d192b958b107bb6b2d [formerly 5e7a880512139651a4d5915390da867c6dadc96d] [formerly 7d2a00bbda0668e3fcc14d621c0654035497497e [formerly cc5ad09736024f0d1ccc82682211c5412709e46c]]]
    Former-commit-id: 19d3c2b064eaac69632daffbe6183d3712cc9d7f [formerly 47b2c9e0391636e9083b70e90ba8864b3c55cc9a] [formerly b38985ba7316791d44ba86d3aa4d8b370cec4d1f [formerly 251c870abde5309b15bb4f2acef74ea9543e6722]]
    Former-commit-id: f77f119f2206ec413ea5979bc70b430fd3655c07 [formerly 268a1c2fbe9dc373635908abd77367f25718cdc4]
    Former-commit-id: 5aa1ffb27cea852d967a52c035475b20cf0ce361

---
+++ lib/OSCAR/OCA/OS_Detect/YDL.pm
@@ -0,0 +1,87 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) 2007 The Trustees of Indiana University.  
+#                    All rights reserved.
+# 
+# This file is part of the OSCAR software package.  For license
+# information, see the COPYING file in the top level directory of the
+# OSCAR source distribution.
+#
+# $Id$
+#
+
+package OCA::OS_Detect::YDL;
+
+use strict;
+
+my $distro = "yellowdog";
+my $compat_distro = "ydl";
+my $pkg = "rpm";
+my $detect_package = "yellowdog-release";
+my $detect_file = "/bin/bash";
+
+# This routine is cheap and called very rarely, so don't care for
+# unnecessary buffering. Simply recalculate $id each time this is
+# called.
+sub detect_dir {
+    my ($root) = @_;
+    my ($release_string, $ydl_release);
+    my $dfile = "/etc/yellowdog-release";
+
+    # If /etc/yellowdog-release exists, continue, otherwise, quit.
+    if (-f "$root$dfile") {
+	$release_string = `cat $root$dfile`;
+    } else {
+	return undef;
+    }
+
+    # this hash contains all info necessary for identifying the OS
+    my $id = {
+	os => "linux",
+	chroot => $root,
+    };
+
+    if ($release_string =~ /Yellow Dog Linux release (\d+)/) {
+	$ydl_release = $1;
+    } else {
+	return undef;
+    }
+
+    $id->{distro} = $distro;
+    $id->{distro_version} = $ydl_release;
+    $id->{compat_distro} = $compat_distro;
+    $id->{compat_distrover} = $ydl_release;
+    $id->{pkg} = $pkg;
+
+    # determine architecture
+    my $arch = main::OSCAR::OCA::OS_Detect::detect_arch_file($root,$detect_file);
+    $id->{arch} = $arch;
+
+    # Make final string
+    $id->{ident} = "$id->{os}-$id->{arch}-$id->{distro}-$id->{distro_version}";
+
+    return $id;
+}
+
+sub detect_pool {
+    my ($pool) = @_;
+
+    my $id = main::OSCAR::OCA::OS_Detect::detect_pool_rpm($pool,
+							  $detect_package,
+							  $distro,
+							  $compat_distro);
+
+    return $id;
+}
+
+sub detect_fake {
+    my ($fake) = @_;
+    my $id = main::OSCAR::OCA::OS_Detect::detect_fake_common($fake,
+							     $distro,
+							     $compat_distro,
+							     $pkg);
+    return $id;
+}
+
+# If we got here, we're happy
+1;
