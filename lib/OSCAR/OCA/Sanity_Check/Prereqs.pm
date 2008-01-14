144b71cbe31c626e37efed009a9dc92dbcc0a27e
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2008-01-14 15:08:25 +0000

    Include a sanity check scripts that checks the prereqs status (installed/not installed).
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@6852 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 9dd3ba4f33d368edd1f0ba0fb11e06b4be99e92d [formerly 47c32b18ec5ff79bca964c4cbd6bedb3ba086b3a] [formerly 7e748825e30b3ebcc212ab20204be8e8de48c1b3 [formerly e2452fa690c09a30599b9a5373ea6212b7aba817]] [formerly 6d3edca102378c2d0982c31011f9673f39075707 [formerly da477c49b4838d7354b22b516d5ab2aec4346524] [formerly 25a459d6a52919855d9305a494a1e16d8537ded9 [formerly f11ff60d5ec20d8612fe55941c5a2e39482c6181]]]
    Former-commit-id: 87d5ada489e2b3986e3d2600c7d597452fb0c1f6 [formerly 9befeaf33ddcddd1a46ecfb8003fcd4b10db0af1] [formerly 5cdbeccfc2ac78a6f4ea6902b95539dd31458f6b [formerly 68ef555c42bcf605f6fc960c74bfcffe4e0d854a]]
    Former-commit-id: 89ebb7fb10a190ef00cc323d813eb9a818c86cde [formerly 4e654176bf23b6173baff40dd59cc8c3c848041c]
    Former-commit-id: 3735a7c22baae695f044943306093866060c6185

---
+++ lib/OSCAR/OCA/Sanity_Check/Prereqs.pm
@@ -0,0 +1,50 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) 2007 Oak Ridge National Laboratory.
+#                    Geoffroy Vallee <valleegr@ornl.gov>
+#                    All rights reserved.
+#
+# This file is part of the OSCAR software package.  For license
+# information, see the COPYING file in the top level directory of the
+# OSCAR source distribution.
+#
+# $Id: OPKG.pm 5913 2007-06-15 02:45:52Z valleegr $
+#
+# This file check the status of prereqs (installed or not).
+
+package OCA::Sanity_Check::Prereqs;
+
+use strict;
+use lib "$ENV{OSCAR_HOME}/lib";
+use OSCAR::ConfigManager;
+use OSCAR::FileUtils;
+use OSCAR::Utils;
+my $oscar_configurator = OSCAR::ConfigManager->new();
+
+my $prereqs_path = $oscar_configurator->get_prereqs_path();
+my $scripts_path = $oscar_configurator->get_scripts_path();
+
+print "Prereqs available at: $prereqs_path\n";
+print "Scripts available at: $scripts_path\n";
+
+my $ipscript = $scripts_path . "/install_prereqs ";
+
+my @entries = OSCAR::FileUtils::get_directory_content ($prereqs_path);
+if (!defined (@entries)) {
+    print "ERROR: Impossible to find the prereqs dir ($prereqs_path)\n";
+    exit 0;
+}
+
+foreach my $e (@entries) {
+    my $path = "$prereqs_path/$e";
+    if ( -d  "$path") {
+        print "Checking prereqs status $e ($path)\n";
+        my $cmd = "$scripts_path/install_prereq $path --status";
+        if (system ($cmd)) {
+            print "ERROR: impossible to get the status of the prereqs $e\n";
+            exit 0;
+        }
+    }
+}
+
+1;
