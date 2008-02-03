112fa5b4c391dd3f6619733c87e034b8b5c132f0
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2008-02-03 02:56:41 +0000

    Inlcude the $Id keyword.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@6911 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: d4c756adfbe93326a82c8df74ed0fc5a5df863cc [formerly 4f4b3c8fb9dfef4bf936d1249d7a9f624c2e12df] [formerly 8f6a8f159277518888c9353ca53965d7c201a882 [formerly fbdb1d3304745e58c2c48cc00aafb1b1d2f62860]] [formerly 121d6dd41e5588efa92321d9638064114c8a787f [formerly d2cfcf2347e60b5ecb263cb4e6995e7d8392c02d] [formerly 1588fc9776a0b3cc8dd9abba5d913839411cfbe0 [formerly 62bbde455f03e7e23a564cbb15419b19f54d1f39]]]
    Former-commit-id: 1cf97f6b5839f77d21cd727bd0e23752926aa011 [formerly b9c5d4b44fa7396544541bf6b2ec34b07e2b3bdd] [formerly e141d64e90bba96ce301d4acd093c649aeccb931 [formerly b41a6b0a7b997b7495da1677f37e86165e8d33f8]]
    Former-commit-id: 21bb47aa21d7c7f592710ee45373e163a5d9a447 [formerly 5e0f581feb5e2a2e540191312767b00e0ce7f2ac]
    Former-commit-id: 04a86fbc95abac6814500c5c144982da3fb106a3

--- lib/OSCAR/FileUtils.pm
+++ lib/OSCAR/FileUtils.pm
@@ -18,6 +18,10 @@ package OSCAR::FileUtils;
 #   along with this program; if not, write to the Free Software
 #   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  US
 
+#
+# $Id$
+#
+
 use strict;
 use vars qw(@EXPORT);
 use base qw(Exporter);
