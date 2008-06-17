b13fb999116b720d303a24b71828ad1d506e87e1
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2008-06-17 20:18:48 +0000

    Update the module that extend AppConfig for the management of configuration files in read/write mode.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@7042 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: d35d2091406fe826883ea55b6a857b6a5ce513a9 [formerly 7f4b45ab576df53316ae3ca7e8dbcce7fa258eb0] [formerly 274c7b08c20c4ceb11d4a3f824747ff488488840 [formerly a8b0d2bd062aa9cc0866774fa05f8e147cd2561f]] [formerly 20f48e61e6c93ead2a7404c2d8638f9fab6d9e5a [formerly e77104773bb3247b30e2fddb36ce1386bc622bc6] [formerly d3b004def334639ad4351ff4a1b9285c1509d029 [formerly ace444a805315d51403ea9d5e3f5053b8da390ac]]]
    Former-commit-id: 63a08f5a6db702977d2a2cf5047d263dbe9d5b3e [formerly f8543cc04199eb8590eadd17193dc860d8a4a7dd] [formerly 14b6b3914780917527a3b91a89e990805786a2a3 [formerly f9c1558731bae8e6d54adb0a44694a40d57fb8a9]]
    Former-commit-id: f0560be79dbc42039f3aa6c2d703d21fd0d55071 [formerly 8192e5cee1a0f337bc5d1058d1f0ab2882eba655]
    Former-commit-id: 03f9957f197e66965962587b267deaad41bbc56d

--- lib/OSCAR/ConfigFile.pm
+++ lib/OSCAR/ConfigFile.pm
@@ -21,13 +21,15 @@ package OSCAR::ConfigFile;
 #   along with this program; if not, write to the Free Software
 #   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-# This Perl module is a simple abstraction to handle configuration files. The
-# goal is typically to be able to easily get and set a given key value in a 
+# This Perl module is a simple abstraction to handle configuration files,
+# extending the AppConfig Perl module (typically adding write operations). The
+# goal is typically to be able to easily get and set a given key value in a
 # given configuration file.
 
 use strict;
 use lib "$ENV{OSCAR_HOME}/lib";
 use OSCAR::Logger;
+use OSCAR::Utils;
 use vars qw(@EXPORT);
 use base qw(Exporter);
 use Carp;
@@ -37,6 +39,8 @@ use warnings "all";
 
 @EXPORT = qw(
             get_value
+            set_value
+            get_all_values
             );
 
 ################################################################################
@@ -45,17 +49,96 @@ use warnings "all";
 # from the /etc/yum.conf configuration file.                                   #
 #                                                                              #
 # Input: config_file, full path to the configuration file we want to analyse.  #
-#        key, key we want the value from. When you want the value of a key,    #
-#        the key name has to follow the AppConfig syntax. It means that to     #
-#        access the key gpgcheck under the main section of the configuration   #
-#        file, the key name is "main_gpgcheck". For our example, the           #
-#        configuration file looks like:                                        #
-#        [main]                                                                #
-#           gpgcheck = 1                                                       #
+#        block, configuration files may be arranged in blocks, in that case,   #
+#               if you want to access a given key under a given block, specify #
+#               the block name here; otherwise, use undef or ""/               #
+#               [main]                                                           #
+#                  gpgcheck = 1
+#        key, key we want the value from. 
 # Return: the key value is the key exists, undef if the key does not exist.    #
 ################################################################################
-sub get_value ($$) {
-    my ($config_file, $key) = @_;
+sub get_value ($$$) {
+    my ($config_file, $block, $key) = @_;
+
+    if (!defined($config_file) || ! -f $config_file) {
+        carp "ERROR: the configuration file does not exist ($config_file)\n";
+        return undef;
+    }
+
+    use vars qw($config);
+    $config = AppConfig->new({
+            CREATE => '^*',
+        },
+        $key            => { ARGCOUNT => 1 },
+        );
+    if (!defined ($config)) {
+        carp "ERROR: Impossible to parse configuration file ($config_file)";
+        return undef;
+    }
+    $config->file($config_file);
+
+    if (defined ($block) && $block ne "") {
+        $key = $block . "_" .$key;
+    }
+
+    return $config->get($key);
+}
+
+sub get_block_list ($) {
+    my $config_file = @_;
+
+    my $list_blocks = `grep '\\[' $config_file`;
+    print "List blocks ($config_file): $list_blocks";    
+}
+
+sub set_value ($$$$) {
+    my ($config_file, $block, $key, $value) = @_;
+
+    if (!defined($config_file) || ! -f $config_file) {
+        carp "ERROR: the configuration file does not exist ($config_file)\n";
+        return -1;
+    }
+
+    use vars qw($config);
+    $config = AppConfig->new({
+            CREATE => '^*',
+        },
+        $key            => { ARGCOUNT => 1 },
+        );
+    if (!defined ($config)) {
+        carp "ERROR: Impossible to parse configuration file ($config_file)";
+        return -1;
+    }
+    $config->file($config_file);
+
+    if (defined ($block) && $block ne "") {
+        $key = $block . "_" .$key;
+    }
+
+    get_block_list ($config_file);
+
+    $config->set($key, $value);
+
+    return 0;
+}
+
+################################################################################
+# Get the value of all keys from a given configuration file. This function is  #
+# based on the get_value function, therefore it means we do not deal with the  #
+# key namespace. In other terms, you have to explicitely expand the key name   #
+# if the key is part of a section (see example in the get_value function       #
+# description).                                                                #
+#                                                                              #
+# Input: config_file, full path to the configuration file we want to analyse.  #
+# Return: a hash with all keys and values, undef if we cannot parse the        #
+#         configuration file. For instance, if the configuration file looks    #
+#         like:                                                                #
+#           var1 = value1                                                      #
+#           var2 = value2                                                      #
+#         The hash will look like: ( "var1", "value1", "var2", "value2" ).     #
+################################################################################
+sub get_all_values ($) {
+    my ($config_file) = @_;
 
     if (!defined($config_file) || ! -f $config_file) {
         print "ERROR: the configuration file does not exist ($config_file)\n";
@@ -66,11 +149,13 @@ sub get_value ($$) {
     $config = AppConfig->new({
             CREATE => '^*',
         },
-        $key            => { ARGCOUNT => 1 },
         );
     $config->file ($config_file);
-
-    return $config->get ($key);
+    my %vars = $config->varlist("^*");
+    while ( my ($key, $value) = each(%vars) ) {
+        $vars{$key} = get_value ($config_file, undef, $key);
+    }
+    return %vars;
 }
 
 1;
