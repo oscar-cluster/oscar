ce100cc3794124d20a30d5abd42b2c2572c75133
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2008-10-31 14:32:28 +0000

    Add a basic Perl module to get information about the local system.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@7524 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 07aa3cc0e3910a578f2610097c7764e1a7ef6d97 [formerly 40f247bef7591011f51b7c591326d531a47a40d0] [formerly cb3b505e42ba43998ad8693f8437f742ec2f5121 [formerly 0f4c6fc949365f941cd7b162e329f0594d6b3ae3]] [formerly b6ae0c27754a92f65442b4be0337fcce283436c0 [formerly 0dee40ece11bc4571b51a19c09fbc8f73d567799] [formerly f669b9e3fcb2d7834c7beb9501ded5bdc174203a [formerly 5498a3a780f98fb9cae1c236ae11dba97e2e81ee]]]
    Former-commit-id: fbfbb5229915607afcc752ea319041322f1700e2 [formerly b63a248b79024f68adc5d59c0f2c4246a5a91120] [formerly 262586bf1e262840b876c203bff31a5cbe1074b7 [formerly 0a2f15c9109e75bf7f041d06392fc2833e316114]]
    Former-commit-id: f1c40a244cd2f59de423fb3d8ad6623c9b769880 [formerly 50944a6c3445e429153a456fe3f92276ac9a84b7]
    Former-commit-id: 3cb07444136d54b58070918d8e9aa6ce432fd0e6

---
+++ lib/OSCAR/SystemInfo.pm
@@ -0,0 +1,117 @@
+package OSCAR::SystemInfo;
+
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+# Copyright (c) 2008, Geoffroy Vallee <valleegr@ornl.gov>
+#                     Oak Ridge National Laboratory.
+#                     All rights reserved.
+#
+
+#
+# $Id$
+#
+
+use strict;
+use lib "$ENV{OSCAR_HOME}/lib";
+use OSCAR::Utils;
+use OSCAR::FileUtils;
+use Carp;
+use vars qw(@EXPORT $VERSION);
+use base qw(Exporter);
+
+sub new {
+    my $invocant = shift;
+    my $class = ref($invocant) || $invocant;
+    my $self = { 
+        output_file => "",
+        @_,
+    };
+    bless ($self, $class);
+    return $self;
+}
+
+sub read_proc_entry ($$) {
+    my $self = shift;
+    my $entry = shift;
+
+    my %data;
+    open(DAT, $entry) || die("Could not open file!");
+    foreach my $line (<DAT>) {
+        my ($key, $value) = split(":", $line);
+        $key = OSCAR::Utils::trim ($key);
+        $value = OSCAR::Utils::trim ($value);
+    $data{$key} = $value;
+    }
+    close (DAT);
+
+    return %data;
+}
+
+sub get_proc_info ($) {
+    my $self = shift;
+
+    return read_proc_entry ($self, "/proc/cpuinfo");
+}
+
+
+sub proc_info ($) {
+    my $self = shift;
+
+    my %data = get_proc_info ($self);
+    print "Number of CPU(s): $data{'processor'}\n";
+    print "Number of cores: $data{'cpu cores'}\n";
+    print "Clock speed: $data{'cpu MHz'} MHz\n";
+}
+
+sub get_number_of_cache_levels ($) {
+    my $self = shift;
+    my $dir = "/sys/devices/system/cpu/cpu0/cache";
+    my @dirs = OSCAR::FileUtils::get_dirs_in_path ($dir);
+    return scalar @dirs;
+}
+
+sub cache_info ($) {
+    my $self = shift;
+
+    my %cpu_data = get_proc_info ($self);
+    for (my $ncpu = 0; 
+         $ncpu < ($cpu_data{'processor'} * $cpu_data{'cpu cores'});
+         $ncpu++) {
+        my $dir = "/sys/devices/system/cpu/cpu$ncpu/cache";
+        print "\nCPU/Core ($ncpu) cache info:\n";
+        for (my $i=0; $i<get_number_of_cache_levels($self); $i++) {
+            my $path = "$dir/index$i";
+            open(FILE, "$path/level") || die("Could not open file!");
+            my $level = <FILE>;
+            chomp $level;
+            close (FILE);
+            open(FILE, "$path/type") || die("Could not open file!");
+            my $type = <FILE>;
+            chomp $type;
+            $type = "Shared" if ($type eq "Unified");
+            close (FILE);
+            open(FILE, "$path/size") || die("Could not open file!");
+            my $size = <FILE>;
+            chomp $size;
+            close (FILE);
+            print "\tL".$level." cache ($type): $size\n";
+        }
+        #my %data = read_proc_entry
+    }
+}
+1;
+
+__END__
+
