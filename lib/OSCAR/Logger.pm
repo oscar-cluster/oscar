b6b352a10085e0b2f916ecf0db24bca926124b4a
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2008-12-12 18:47:15 +0000

    Create a new function to set log files based on the output of a running process.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@7707 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 18df48b007a2e7a7dd545d046e6d5c943506dc15 [formerly 9514e7c0c146050f0324faac120e705e77f7866c] [formerly a75b6392c34b4bead3f5a1892f385ade472db2cc [formerly be6b8eb3d8d48a004e1a18a7477c665764f1159e]] [formerly 62c9963a7e78ab9015f74a88932358afba0acc51 [formerly a7981cbc6e579b40f2733dc086156566af738b9b] [formerly 65c3a25f873d961d7f843e04f84ec4b6cbca677f [formerly d308e10779f2060ec3aa251b6b14f89c9b56dcad]]]
    Former-commit-id: 4465cd865a17c2c97b7a1e4062c6d0ac85ea7bcc [formerly 888853ac39eb0c2a1cafe1f63f5afc98a65138fa] [formerly ed8b0ec8f0f186fcab7b5e3d8396969d4961401d [formerly 3559e13456f00ed2a32578bb477d8f4b9949fc10]]
    Former-commit-id: 7324d3cc99c79c7ca917a20cfd20a8d4e5671243 [formerly cf36d8f51228e68a2e366b5c61597c861b9c0189]
    Former-commit-id: 6d80031adf2b684bcfb210799a1a8755c788b62a

--- lib/OSCAR/Logger.pm
+++ lib/OSCAR/Logger.pm
@@ -22,6 +22,7 @@ package OSCAR::Logger;
 #                           All rights reserved.
 
 use strict;
+use Carp;
 use vars qw($VERSION @EXPORT);
 use base qw(Exporter);
 
@@ -88,5 +89,61 @@ sub print_error_strings ($) {
     $error_strings_ref = \@error_strings;
 }
 
+################################################################################
+# Setup a log file for a given process.
+#
+# Input: Absolute path of the log file.
+# Return: 0 if the log file can be set correctly, -1 else.
+################################################################################
+sub init_log_file ($) {
+    my $log_file = shift;
+
+    # Setup to capture all stdout/stderr
+    require File::Basename;
+    my $oscar_log_dir = File::Basename::dirname ($log_file);
+    if (! -d $oscar_log_dir ) {
+        print "$oscar_log_dir does not exist, we create it\n";
+        mkdir ($oscar_log_dir);
+    }
+
+    # EF: Fix for bug #244: multiple oscarinstall.log files
+    # The current (and latest) log file is oscarinstall.log. Old log files
+    # get a number appended (eg. oscarinstall.log_27), starting with _1
+    # and increasing with each new invocation of install_cluster.
+    # Date stamp added to output.
+    if (-e $log_file) {
+        my $indx = 1;
+        # more old logs around?
+        my @ologs = glob("$log_file"."_*");
+        @ologs = map { if (/_(\d+)$/) { $1 } } @ologs;
+        @ologs = sort { $a <=> $b; } @ologs;
+        if (@ologs) {
+        $indx = $ologs[$#ologs] + 1;
+        }
+        !system("mv $log_file $log_file"."_$indx")
+        or (carp "Could not rename $log_file : $!", return -1);
+    }
+
+    if (!open (STDOUT,"| tee $log_file") || !open(STDERR,">&STDOUT")) {
+        (carp("ERROR: Cannot tee stdout/stderr into the OSCAR logfile: ".
+        "$log_file\n\nAborting the install.\n\n"), return -1);
+    }
+
+    return 0;
+}
 
 1;
+
+__END__
+
+Exported functions:
+
+=over 4
+
+=item init_log_file
+
+Setup a log file for a given process.
+
+=back
+
+=cut
