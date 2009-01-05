3283dedf031e7fe7d9afef7047ec05beb11cfa3c
valleegr <valleegr@18f9d06c-6ae2-0310-8438-938a554feff9>
2009-01-05 21:43:45 +0000

    Add a file for the definition of common OSCAR macros.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@7779 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 6e31de90285ebf5e9994d6b080e74c13f1b080fc [formerly 4c06420f8efd8615426f9a838ae1e5a40f234497] [formerly c91abbbfa3049b2089c40f1a3a872cfa05ab4b6b [formerly 924236741abd3425d893db135d212f526099b817]] [formerly c5e0c450d3aaf37fdad9eb992ca2661dbc65c2b3 [formerly 305e9053be8921fa4802a143772a6806136ba784] [formerly 5b57f9ba7ed065c461532c5029310a0208d2c03a [formerly db059415fd6196439d6da36ddde7ed2537c738f8]]]
    Former-commit-id: 7bbd48720c64b04be9a8f3c4f47dc113e3478871 [formerly 63f4af1a7718f8672a09dc70f9db63416c894e78] [formerly 751ec241e6932403356d8d07c94bcca16a5ee4ac [formerly 5b72d722c3e4aba4af91f7d1358e2b9516018e36]]
    Former-commit-id: 7be4b82ccff2a1645d7d7c88a7548420a7f06f54 [formerly 6caa0e7ebe99a107cade07f1556473c34f887645]
    Former-commit-id: 9ee7da5600b9287aec3467814ab9ea9d17571f2f

---
+++ lib/OSCAR/Defs.pm
@@ -0,0 +1,30 @@
+package OSCAR::Defs;
+
+# $Id$
+#
+# Copyright (c) 2009 Geoffroy Vallee <valleegr@ornl.gov>
+#                    Oak Ridge National Laboratory.
+#                    All rights reserved.
+
+use strict;
+use base qw(Exporter);
+
+# Definition of some options commonly used by OSCAR functions
+use constant NO_OVERWRITE       => 0;
+use constant OVERWRITE          => 1;
+
+# Some macros specific to file management: the different file types are support
+use constant TARBALL            => 0;
+use constant SRPM               => 1;
+
+my @ISA = qw(Exporter);
+
+our @EXPORT = qw(
+                NO_OVERWRITE
+                OVERWRITE
+
+                TARBALL
+                SRPM
+                );
+
+1;
