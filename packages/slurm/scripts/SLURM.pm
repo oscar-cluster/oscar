6e26e363c46c9ba7ca8b145635e90e7c1288f80f
olahaye74 <olahaye74@18f9d06c-6ae2-0310-8438-938a554feff9>
2013-09-20 16:42:50 +0000

    [slurm:opkg] Add opkg for slurm. (not yet finished).
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@10036 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 35517d2c337ce30ae0b45c3830f5ac6aeb3a1821 [formerly fbee24275d67177bd895a1c74846c26cee79eacb] [formerly 780c9fb009f00ef0260caf52d9c4cfa0d87d04e0 [formerly c6bba7bbefbabc3d87df5baba565df5aceba611c]] [formerly c04cf6921581d90078509e391e51857e4bf7eb71 [formerly d2bd91674ada2e341a164b027e0cb0e0c4cb6731] [formerly f3ee71dfb439a0e8b2bcf81d37ce3425f3a2bb86 [formerly 3a4767c647284a0c9f85e7c925af53f74eb7ac18]]]
    Former-commit-id: 6272df8e16030898a3c522fd9ba9bf9a11be380f [formerly 0778e07b298d2d5e707943ab66d52fad5fd0ea77] [formerly 1ab23349e4e9c4767dce6bf2689172db2d48e0ab [formerly 5b49899264ab7f56af9f272728d554a554b1d139]]
    Former-commit-id: 66a45b0f6d0773f990f5ebe9f0ba6af137a2fc1f [formerly 5c36d60fc26da81f245e39911f8fe7fd9b0a6bc4]
    Former-commit-id: 10fa170b3e8492b2387fb8b2e57a448398b48d63

---
+++ packages/slurm/scripts/SLURM.pm
@@ -0,0 +1,56 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) 2005 Bernard Li <bli@bcgsc.ca>.
+#                    All rights reserved.
+# 
+# This file is part of the OSCAR software package.  For license
+# information, see the COPYING file in the top level directory of the
+# OSCAR source distribution.
+#
+# $HEADER$
+#
+
+package OCA::RM_Detect::SLURM;
+
+use strict;
+use XML::Simple;
+
+my $pkg = "slurm";
+my $pkg_dir = "$ENV{OSCAR_HOME}/packages/$pkg";
+my $pkg_config = "$pkg_dir/config.xml";
+
+my $xml_ref = undef;
+my $xs = new XML::Simple();
+$xml_ref = eval { $xs->XMLin( $pkg_config ); };
+
+my $displayname = "SLURM";
+my $test = "slurm_test";
+my $jobscript = "slurm_script";
+
+# First set of data
+
+our $id = {
+    name => $displayname,
+    pkg => $pkg,
+    major => $xml_ref->{version}->{major},
+    minor => $xml_ref->{version}->{minor},
+    subversion => $xml_ref->{version}->{subversion},
+    test => "/var/lib/oscar/testing/$pkg/$test",
+    jobscript => $jobscript,
+};
+
+# Make final string
+
+$id->{ident} = "$id->{pkg}-$id->{major}.$id->{minor}.$id->{subversion}";
+
+# Once all this has been setup, whenever someone invokes the "query"
+# method on this component, we just return the pre-setup data.
+
+sub query {
+    our $id;
+    return $id;
+}
+
+# If we got here, we're happy
+
+1;
