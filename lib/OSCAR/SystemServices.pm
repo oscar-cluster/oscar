33d378acd42d2d6f24271e18cca34e67dd44400f
olahaye74 <olahaye74@18f9d06c-6ae2-0310-8438-938a554feff9>
2013-10-16 09:26:43 +0000

    [[OSCAR::SystemServices] Add support for performing an action on a service hosted on a remote host. Write full Pod documentation.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@10082 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: df2ab4c248325929ce7d76020ba8649a9d320a5d [formerly 3388916d1969c2416b1ac04972fcc568cddac1a4] [formerly 2db6cf49e6b4d0bb3f8f638490e7e5ad9c2dc5a2 [formerly 33a317292bab572fa28ddcf64122a0486319593a]] [formerly fb6ee74262f9caffd1a0d800f35906d72e98121d [formerly dc69ce49df9379aae8c1369a543f0d4470ba8e91] [formerly ad0e3382ef7123eb4f7ac1abd2a8dc00ca8a4a9f [formerly 9055dee9383c0586b4bc04e0099aca7b82b40b3e]]]
    Former-commit-id: 0d80a56d364824854201de6dd32192fd9d00f9ef [formerly efd6af2bacd2bd2dd3daa448cfae9a90995e2459] [formerly 4ae4ba07945f7b02c656f336b269b8dfbc9331d2 [formerly 3d0db3f5fde97f809e02bcc219dfed463048faac]]
    Former-commit-id: adc5bafd11dfe4aac0219f0de2f1d939334602c5 [formerly 27958bd4a6408fae7aece1a85c65d90ef7a0ff2c]
    Former-commit-id: 2433cb6d64772e21c9a7d4a38ae699b76161899f

--- lib/OSCAR/SystemServices.pm
+++ lib/OSCAR/SystemServices.pm
@@ -52,16 +52,50 @@ use base qw(Exporter);
              enable_system_services
              disable_system_services
              system_service
+             remote_system_services
+             system_service_status
              get_system_services
              );
 
-#sub system_service ($$);
+=encoding utf8
 
-# Parse the output of the chkconfig --list <service_id> command.
-#
-# Input: the output of the chkconfig command.
-# Return: an array with the status for each run-level (n elt of the array =
-#         level-n of execution.
+=head1 NAME
+
+OSCAR::SystemServices -- System services management abstraction module
+
+=head1 SYNOPSIS
+
+use OSCAR::SystemServices;
+use OSCAR::SystemServicesDefs;
+
+=head1 DESCRIPTION
+
+This module provides a collection of fuctions to provide an abstraction
+layer to system services management.
+
+Depending on Linux distro, services are managed by systemd, initscripts
+commands (chkconfig, service) or manually (run the /etc/init.d script
+directly or enabling it manually as well). This module allows to forget
+those differences when managing services.
+
+=head2 Functions
+
+=over 4
+
+=cut
+###############################################################################
+=item parse_chkconfig_output( $output )
+
+Parse the output of the chkconfig --list <service_id> command.
+
+Input:  the output of the chkconfig command.
+Return: an array with the status for each run-level (n elt of the array =
+        level-n of execution.
+Exported: NO
+
+=cut 
+###############################################################################
+
 sub parse_chkconfig_output ($) {
     my $output = shift;
 
@@ -76,11 +110,28 @@ sub parse_chkconfig_output ($) {
     return @status;
 }
 
-# Check the status of a single system service.
-#
-# Input: service name (either generic (ssh) or real (sshd))
-# Return: SERVICE_ENABLED or SERVICE_DISABLED; see the SystemServiceDefs file.
-#         undef if error.
+###############################################################################
+=item system_service_status( $service )
+
+Check the status of a single system service.
+
+ Input: service name (either MACRO, generic name or real name)
+Return: SERVICE_ENABLED or SERVICE_DISABLED; see the SystemServiceDefs file.
+        undef if error.
+
+Examples: system_service_status(HTTP())   => Prefered method
+          system_service_status("http")
+          system_service_status("httpd")  => Avoid if name is not the same on
+                                            all distros (httpd, apache2, ...).
+         => Note: HTTP() points to "http" (SystemServiceDefs.pm) which in turn
+            allow to point to the real service name when appending _service
+            (variable http_service from OS_Settings/{default,...})
+
+Exported: YES
+
+=cut
+###############################################################################
+
 sub system_service_status ($) {
     my $service = shift;
 
@@ -136,20 +187,64 @@ sub system_service_status ($) {
     return undef;
 }
 
-# Abstraction of the underlying tool for system service management (such as 
-# chkconfig).
-#
-# Input: a list of services (either generic (dns) or real (bind))
-#
-# Return: 0 if success, -1 else.
+###############################################################################
+=item enable_system_services( $service )
+
+Enable multiple system services to automatically start at boot.
+
+ Input: a list of services (either MACROS, generic names or real names)
+Return:  0: Success.
+        -1: Error.
+
+Example: enable_system_services((HTTP(),GANGLIA_GMOND(),GANGLIA_GMETAD()))
+
+Exported: YES
+
+=cut
+###############################################################################
+
 sub enable_system_services (@) {
     OSCAR::Logger::oscar_log_subsection("Unabling services:");
     return set_system_services(OSCAR::SystemServicesDefs::SERVICE_ENABLED(),@_);
 }
+
+###############################################################################
+=item disable_system_services( $service )
+
+Disable multiple system services to automatically start at boot.
+
+ Input: a list of services (either MACROS, generic names or real names)
+Return:  0: Success.
+        -1: Error.
+
+Example: enable_system_services((SI_BITTORRENT(),SI_FLAMETHROWER()))
+
+Exported: YES
+
+=cut
+###############################################################################
+
 sub disable_system_services (@) {
     OSCAR::Logger::oscar_log_subsection("Disabling services:");
     return set_system_services(OSCAR::SystemServicesDefs::SERVICE_DISABLED(),@_);
 }
+
+###############################################################################
+=item set_system_services( $service )
+
+Configure multiple system services start behavior at boot.
+
+ Input: - START() or STOP()
+        - a list of services (either MACROS, generic names or real names)
+Return:  0: Success.
+        -1: Error.
+
+Example: set_system_services(SERVICE_ENABLED(),(SI_BITTORRENT(),SI_FLAMETHROWER()))
+
+Exported: YES
+
+=cut
+###############################################################################
 
 sub set_system_services ($@) {
     my $config = shift;
@@ -267,18 +362,63 @@ sub set_system_services ($@) {
     return 0;
 }
 
-################################################################################
-# is_system_service_running: tells if a service is running                     #
-#                                                                              #
-# input: service name (either generic (dns) or real name (bind)                #
-#        action                                                                #
-# output: 0 Success                                                            #
-#         non 0: Error                                                         #
-################################################################################
+###############################################################################
+=item system_service( $service , $action )
+
+Perform an action on a service: START, STOP, RESTART, STATUS
+
+ Input: - service (either MACRO, generic name or real name)
+        - action (START(), STOP(),RESTART(),STATUS())
+
+Return:     0: Success.
+        non 0: Error.
+
+Examples: system_service(HTTP(),RESTART()) # Prefered method
+          system_service("http","restart")
+          system_service("httpd","restart")
+          => HTTP() points to "http" (SystemServiceDefs.pm) which in turn
+             allow to point to the real service name when appending _service
+             (http_service from OS_Settings/{default,...})
+
+Exported: YES
+
+=cut
+###############################################################################
 
 sub system_service ($$) {
-    my ($service, $action) = @_;
+    my $service = shift;
+    my $action = shift;
+    return remote_system_service($service,$action,undef);
+}
 
+###############################################################################
+=item remote_system_service( $service , $action , $remote_cmd)
+
+Perform an action on a remote service: START, STOP, RESTART, STATUS
+
+ Input: - service (either MACRO, generic name or real name)
+        - action (START(), STOP(),RESTART(),STATUS())
+        - remote command (typically: /usr/bin/ssh $host or /usr/bin/cexec)
+
+Return:     0: Success.
+        non 0: Error.
+
+Examples: remote_system_service(GANGLIA_GMOND(),RESTART(),"/usr/bin/cexec")
+          remote_system_service(GANGLIA_GMOND(),RESTART(),"/usr/bin/ssh $node")
+
+Exported: YES
+
+=cut
+###############################################################################
+
+sub remote_system_service($$$) {
+    my ($service, $action, $remote_cmd) = @_;
+
+    # If remote command is defined but is not executable or is a directory => Problem.
+    if (defined $remote_cmd and ( not -x $remote_cmd or -d $remote_cmd )) {
+        carp "ERROR: remote command ($remote_cmd) not an executable.\n Can't perform remote action $action on remote service $service";
+        return -1;
+    }
     my $os = OSCAR::OCA::OS_Detect::open();
     my $service_mgt = $os->{service_mgt}; # (systemd, initscripts, manual)
 
@@ -312,11 +452,11 @@ sub system_service ($$) {
     OSCAR::Logger::oscar_log_subsection ("Performing '$cmd_action' on service $service_name ($service)... ");
     given ($service_mgt) {
         when "systemd" {
-            $cmd = "LC_ALL=C  /bin/systemctl ".$cmd_action." ".$service_name;
+            $cmd = "/bin/systemctl ".$cmd_action." ".$service_name;
             last
         }
         when "initscripts" {
-            $cmd = "LC_ALL=C /sbin/service ".$service_name." ".$cmd_action;
+            $cmd = "/sbin/service ".$service_name." ".$cmd_action;
             last
         }
         when "manual" {
@@ -334,19 +474,29 @@ sub system_service ($$) {
     # command not found: 127
     # unknown service: 1
 
-    # my $ret_code = system ($cmd) / 256;
-    my $ret_code = system ($cmd);
+    my $ret_code = system ("LC_ALL=C $remote_cmd $cmd");
     $ret_code = $ret_code/256 if ($ret_code > 255);
     OSCAR::Logger::oscar_log_subsection ("[SystemService] Return code: $ret_code");
 
     return $ret_code;
 }
 
-# Give the list of system services OSCAR knows how to deal with.
-#
-# Input: None.
-# Return: a hash where the key is the id the service if the value the actual
-#         service name eg. http => apache2.
+###############################################################################
+=item get_system_services()
+
+Give the list of system services OSCAR knows how to deal with.
+
+ Input: none
+Output: a hash where the key is the id the service if the value the actual
+        service name eg. http => apache2.
+
+Examples: my %services = list_system_services();
+
+Exported: YES
+
+=cut
+###############################################################################
+
 sub get_system_services () {
     # We get the list of all entries in OS_Settings
     my $config = OSCAR::OCA::OS_Settings::getconf ();
@@ -367,25 +517,43 @@ sub get_system_services () {
     return %services;
 }
 
+=back
+
+=head1 TO DO
+
+ * Add support for remotely enable/disable services.
+
+=head1 SEE ALSO
+
+L<OSCAR::SystemServicesDefs>
+
+=head1 AUTHORS
+Origially written by:
+    (c) 2008-2009 Geoffroy Vallee C<< <valleegr@ornl.gov> >>
+                  Oak Ridge National Laboratory
+                  All rights reserved.
+Mostly rewritten and documented by:
+    (c) 2009-2013 Olivier Lahaye C<< <olivier.lahaye@cea.fr> >>
+                  CEA (Commissariat à l'Énergie Atomique)
+                  All rights reserved
+
+=head1 LICENSE
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+=cut
+
 1;
 
 __END__
-
-=head1 Exported Functions
-
-=over 4
-
-=item enable_system_services
-
-=item system_service
-
-Peform an action on a given system service. For example: my $rc = SystemServices (OSCAR::SystemServicesDefs::DHCP(), OSCAR::SystemServicesDefs::STOP()); stops the dhcp deamon. Note that we use macros to be sure we correctly identify the service and the action.
-
-=item list_system_services
-
-Give the list of system services OSCAR knows how to deal with. Example:
-my %services = list_system_services();
-
-=back
-
-=cut
