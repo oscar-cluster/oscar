246a891d6864204b1bd75f69a58741cde877aef2
olahaye74 <olahaye74@18f9d06c-6ae2-0310-8438-938a554feff9>
2014-03-07 16:27:13 +0000

    [opkg-torque:tests.d/torque_nodes_up.pl] full rewrite using pbsnodes -x xml output. Test is hardened and check for nodes seens that are not in ODA, and also check for bad status (offline, down, ...)
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@10619 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: 05dc60c257f356892bc1f95fe91fb87817a7bfb6 [formerly b97b8db6c99a7b1a2c20160fe38305f18bd965ca] [formerly 8252fe3889de2d9beb0ae2e03a3a8334e4e19237 [formerly 653a70795c7938efff678d7b30adccf65bb4112b]] [formerly da935525d19ec157c0ee13204423387d39aa8098 [formerly 2a1231d674246b26534904d6df78f91f39572502] [formerly 77c628a796afd5178760f12e7ebc57d0bb7a26ac [formerly 43a3ed346ac6f17e6ac688bfd8261c24b4362e87]]]
    Former-commit-id: e37abb6384c34d0c1668ffa988ce588e682e7269 [formerly b24790056c510c7c1140b06cda10a41d616a2a7d] [formerly 2285dc7328c61ea75c8292c9b44d6b1b56260178 [formerly c8c1668e365fa84495e97c50bb8197ba0980ba79]]
    Former-commit-id: e42b8f31e5029695c9a697382259829025bab8d4 [formerly a1884b91d1df4f8f605be5f1de0cc0c765fa45d2]
    Former-commit-id: f732968323ae77dae84c811102a0f0e46544c9e0

--- packages/torque/testing/tests.d/torque_nodes_up.pl
+++ packages/torque/testing/tests.d/torque_nodes_up.pl
@@ -1,90 +1,145 @@
-#!/usr/bin/perl -w
+#!/usr/bin/perl 
 #############################################################################
-###
-###   This program is free software; you can redistribute it and/or modify
-###   it under the terms of the GNU General Public License as published by
-###   the Free Software Foundation; either version 2 of the License, or
-###   (at your option) any later version.
-###
-###   This program is distributed in the hope that it will be useful,
-###   but WITHOUT ANY WARRANTY; without even the implied warranty of
-###   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-###   GNU General Public License for more details.
-###
-###   You should have received a copy of the GNU General Public License
-###   along with this program; if not, write to the Free Software
-###   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-###
-###   Copyright (c) 2013-2014 CEA - Commissariat a l'Energie Atomique et
-###                            aux Energies Alternatives
-###                            All rights reserved.
-###   Copyright (C) 2013-2014  Olivier LAHAYE <olivier.lahaye@cea.fr>
-###                            All rights reserved.
-###
-### $Id: $
-###
-###############################################################################
+####
+####   This program is free software; you can redistribute it and/or modify
+####   it under the terms of the GNU General Public License as published by
+####   the Free Software Foundation; either version 2 of the License, or
+####   (at your option) any later version.
+####
+####   This program is distributed in the hope that it will be useful,
+####   but WITHOUT ANY WARRANTY; without even the implied warranty of
+####   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+####   GNU General Public License for more details.
+####
+####   You should have received a copy of the GNU General Public License
+####   along with this program; if not, write to the Free Software
+####   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+####
+####   Copyright (c) 2013-2014 CEA - Commissariat a l'Energie Atomique et
+####                            aux Energies Alternatives
+####                            All rights reserved.
+####   Copyright (C) 2013-2014  Olivier LAHAYE <olivier.lahaye@cea.fr>
+####                            All rights reserved.
+####
+#### $Id: $
+####
+################################################################################
 
 use strict;
 use warnings;
-use Carp;
-use Data::Dumper;
-use OSCAR::Package;
+
 use OSCAR::Database;
+use OSCAR::Package;
+use XML::Simple;
+use OSCAR::Logger;
+use OSCAR::LoggerDefs;
 
-BEGIN {
-    if (defined $ENV{OSCAR_HOME}) {
-        unshift @INC, "$ENV{OSCAR_HOME}/lib";
-    }
+# Up to now, everything is OK.
+my $rc=0;
+
+# First of all, enforce that the user running this script is 'root'
+if ($< != 0) {
+    oscar_log(1, ERROR, "You must be 'root' to run this script.  Aborting");
+    exit 1;
 }
 
-my $pbsnodes_bin;
+my $pbsnodes_cmd;
 if (-x '/usr/bin/pbsnodes') {
-   $pbsnodes_bin='/usr/bin/pbsnodes';
+   $pbsnodes_cmd='/usr/bin/pbsnodes -x';
 } else {
-   $pbsnodes_bin='/opt/pbs/bin/pbsnodes';
+   $pbsnodes_cmd='/opt/pbs/bin/pbsnodes -x';
 }
+
+my $xml = new XML::Simple;
+open CMD, "$pbsnodes_cmd |" or die "Error: $!";
+my $pbsnodes_hash = $xml->XMLin(<CMD>, ForceArray => [ 'Node' ]);
+close CMD;
+
+# Get nodes seen by PBS.
+my @pbs_seen_nodes = keys %{$pbsnodes_hash->{Node}};
 
 my %options = ();
 my @errors = ();
-my @sis_nodes = ();
-my @client_nodes = ();
+my @oda_nodes = ();
 
 # Prepare to check if head must be counted as a compute node.
 my $configvalues = getConfigurationValues('torque');
 my $compute_on_head = ($configvalues->{compute_on_head}[0]);
-
-my $rc = 0;
-
-open PBSNODES, "${pbsnodes_bin} |"
-    or (carp "ERROR: Could not run: $!", return 255);
-
-# Loop on sis nodes and check that pbs_knows it.
-# FIXME: need to handle the $compute_on_head.
-
-my @pbs_nodes = ();
-while (my $line = <PBSNODES>) {
-    chomp $line;
-    next if ($line =~ /^\s*$/);
-    # FIXME: need better regexp for hostname match.
-    if ( $line =~ /^[a-zA-Z0-9]+$/) {
-        push(@pbs_nodes , $line);
+if($compute_on_head) {
+    # Compute on head: we get ALL nodes.
+    if(!get_nodes(\@oda_nodes,\%options,\@errors)) {
+        oscar_log(5, ERROR, "Can't get OSCAR defined nodes from database.");
+        $rc++;
+    }
+} else {
+   # Don't compute on head: we get only client nodes.
+    if(!get_client_nodes(\@oda_nodes,\%options,\@errors)) {
+        oscar_log(5, ERROR, "Can't get OSCAR defined nodes from database.");
+        $rc++;
     }
 }
-close PBSNODES;
 
-get_client_nodes(\@client_nodes,\%options,\@errors);
-# FIXME: Need to check that the above command succeed. (it can fail if db is down).
+my $check_server_priv_nodes=0;
+my @oda_seen_nodes = ();
+my @pbs_unknown_nodes = ();
+foreach my $client_ref (@oda_nodes){
+    my $node_name = $$client_ref{hostname};
 
-foreach my $client_ref (@client_nodes){
-    my $node_name = $$client_ref{name};
-    push @sis_nodes, $node_name;
+    # Check that ODA node is seen by PBS
+    if (! defined($pbsnodes_hash->{Node}->{$node_name})) {
+        oscar_log(5, ERROR, "Node: $node_name defined in OSCAR database but unknown to PBS scheduler");
+        $rc++;
+        push(@pbs_unknown_nodes, $node_name);
+        next;
+    }
+
+    # If $$client_ref{gpu_num} is not defined, it means gpus=0;
+    $$client_ref{gpu_num} = 0 if (! defined($$client_ref{gpu_num}));
+
+    # Fake head gpu count test on head. (We disable gpus on head).
+    $pbsnodes_hash->{Node}->{$node_name}->{gpus} = $$client_ref{gpu_num} if ($$client_ref{group_name} eq "oscar_server");
+
+    # Check that ODA node options are the same as in PBD (np= gpus=)
+    if (($pbsnodes_hash->{Node}->{$node_name}->{np} != $$client_ref{cpu_num}) ||
+        ($pbsnodes_hash->{Node}->{$node_name}->{gpus} != $$client_ref{gpu_num})) {
+            oscar_log(5, ERROR, "Node: $node_name PBS config differs from ODA: PBS(np=$pbsnodes_hash->{Node}->{$node_name}->{np},gpus=$pbsnodes_hash->{Node}->{$node_name}->{gpus}) ODA(np=$$client_ref{cpu_num},gpus=$$client_ref{gpu_num})");
+            $rc++;
+            $check_server_priv_nodes=1;
+     }
+
+    # Check that the nodes are ok. (no errors)
+    for my $state (split ',', $pbsnodes_hash->{Node}->{$node_name}->{state}) {
+        if ($state ~~ {"down","unknown","offline","buzy"}) {
+            oscar_log(5, ERROR, "Node: $node_name bad state: $state");
+            $rc++;
+        }
+    }
+
+    # Keep in mind that we saw this node.
+    push @oda_seen_nodes, $node_name;
 }
 
-for my $node (@sis_nodes) {
-    next if ($node ~~ @pbs_nodes);
-    print("ERROR: $node is not seen by torque server.\n");
-    exit 1;
+# If there were differences between ODA and what PBS sees, display a tip.
+if ($check_server_priv_nodes) {
+    oscar_log(5, ERROR, "PBS config differs from ODA: Check server_priv/nodes");
 }
 
-exit 0;
+# If Some nodes are in ODA but not seen by PBS, just report that as well.
+if (scalar(@pbs_unknown_nodes) > 0) {
+    oscar_log(5, ERROR, "Some ODA nodes are not seen by PBS: ".join(' ',@pbs_unknown_nodes));
+    $rc++;
+}
+
+# Check that all nodes from PBS are in the database.
+PBS_NODES: for my $pbs_node (@pbs_seen_nodes) {
+    foreach my $client_ref (@oda_nodes) {
+        if ($$client_ref{hostname} eq $pbs_node) {
+            next PBS_NODES; # Found. move to next one.
+        }
+    }
+    # Not found!
+    oscar_log(5, ERROR, "PBS node: $pbs_node not defined in ODA.");
+    $rc++;
+}
+
+exit $rc;
