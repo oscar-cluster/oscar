023cfb06208790aaa829bac8d7fd6c2c690b152d
olahaye74 <olahaye74@18f9d06c-6ae2-0310-8438-938a554feff9>
2014-03-13 12:08:01 +0000

    [opkg-torque: torque_job_test.pl] Write a simple job submission test. Check that job runs and gives the correct result.
    git-svn-id: http://svn.oscar.openclustergroup.org/oscar/trunk@10635 18f9d06c-6ae2-0310-8438-938a554feff9
    
    Former-commit-id: ab844a7b4c0e3f26db06f0399ee83aa6b3852ee3 [formerly b5106a588bf350a4b465cfd0fa4144e4dfbe3821] [formerly b4ae9b2ed989567be71147b7127e8fea58167616 [formerly 97dcf3488287121f118660b82bb1afd811bf1c68]] [formerly dab362286db5bd1ef0881a76c57f228d4ea5ba73 [formerly 1435a804a39c588864dd31724ec19df6891f0924] [formerly 20615ca7d6d95c9d1848bfac1ed024ed623d4989 [formerly 7b7533deca41c441c8cd36f87d83d713ff20334b]]]
    Former-commit-id: 1033f18448b8fd9693b8803367785a3feda8b6c8 [formerly e397b02f5ae4463dc90f690678e6d8dfa1f812f1] [formerly 6d212b23b75da69bedf76572799ff7b591e957f7 [formerly a472ce88288a48d22c32effcea263e077c151783]]
    Former-commit-id: fc3eba70019519373c31e9297e85f4dfa669c4e9 [formerly 7c7aa707718056643023d70ea0f410e63223abce]
    Former-commit-id: 2c70f3393490a2df3d181dadec470d1b6b78cced

--- packages/torque/testing/tests.d/torque_job_test.pl
+++ packages/torque/testing/tests.d/torque_job_test.pl
@@ -28,11 +28,84 @@
 use strict;
 use warnings;
 use Carp;
+use OSCAR::Package;
+use OSCAR::Testing;
+use OSCAR::Utils;
+use OSCAR::Logger;
+use OSCAR::LoggerDefs;
 
 BEGIN {
     if (defined $ENV{OSCAR_HOME}) {
         unshift @INC, "$ENV{OSCAR_HOME}/lib";
     }
 }
+
+my $datadir = get_data_dir('torque');
+my $testuser = get_test_user();
+my $i_am = getpwuid($<) || "unknown";
+
+# Test that we are running as a normal user.
+if ( "$testuser" ne "$i_am" ) {
+	oscar_log(1, ERROR, "Test must run under the OSCAR test user");
+    oscar_log(1, ERROR, "I am: $i_am and should be: $testuser");
+    exit 1;
+}
+
+# Try to chdir to HOMEDIR
+return 1 unless chdir;
+
+# cleanup old stuffs if any.
+unlink glob "shelltest.*";
+
+# Get the PBS test script
+my $pbs_script = "$datadir/pbs_script.shell";
+if ( ! -r "$pbs_script" ) {
+    oscar_log(1, ERROR, "Can't read $pbs_script");
+    exit 1;
+}
+
+my $torque_bin="/usr/bin";
+$torque_bin="/opt/pbs/bin" if (-x '/opt/pbs/bin/qsub');
+
+my $cmd = "$torque_bin/qsub $pbs_script";
+
+!oscar_system($cmd)
+    or exit 1;
+
+# 10 seconds to get the answer should be far sufficient.
+for( my $retry=0; $retry <= 10; $retry++) {
+    last if ( -f "shelltest.out" );
+    sleep 1;
+}
+
+# Check that we got some output.
+if (! -f "shelltest.out") {
+    oscar_log(1, ERROR, "Timout: no result after 10 seconds.");
+    oscar_log(1, ERROR, "Content of the queue:");
+    oscar_system("$torque_bin/qstat -a");
+    exit 1;
+}
+
+# Check that shelltest.err is an empty file.
+my $err_output_size = -s "shelltest.err";
+if ( -s "shelltest.err" ) {
+    oscar_log(1, ERROR, "shelltest.err is not empty, there was a problem. Check ~$testuser/shelltest.*");
+    exit 1;
+}
+
+# Check that the output contains the Hello word.
+if (! open FH, "<", "shelltest.out") {
+    oscar_log(1, ERROR, "Failed to read the shelltest.out file");
+    exit 1;
+}
+
+my @seen = grep {/Hello/} <FH>;
+if (scalar (@seen) == 0) {
+    oscar_log(1, ERROR, "shelltest.out doesn't contain the Hello word. something is wrong.");
+    exit 1;
+}
+
+# Cleanup (no errors, thus useless output files).
+unlink glob "shelltest.*";
 
 exit 0;
