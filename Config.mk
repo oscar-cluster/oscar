954e83153b6f4e8a7aa35e872933f0445de84002
DongInn Kim <dikim@indiana.edu>
2016-10-20 16:49:45 -0400

    Remove all the OSCAR files from the github pages branch (gh-pages)

--- Config.mk
+++
@@ -1,4 +0,0 @@
-# -*- mode: Makefile; -*-
-
-LIBDIR	?= $(shell perl -V:vendorlib | cut -d"'" -f2)
-MANDIR  ?= /usr/share/man
--- packages/blcr/testing/tests.d/foo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo dummy test to ease opkgc developpement (migration to new testing tree)
-
-exit 0
